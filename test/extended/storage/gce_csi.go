374efc3befc5eee2e851bf8d20f38385e4c8cb13
chaoyang <chaoyang@redhat.com>
2022-05-19 09:40:07 +0800

    Add test case for gcp csi driver supported volume type (#3552)

--- test/extended/storage/gce_csi.go
+++ test/extended/storage/gce_csi.go
@@ -12,8 +12,15 @@ import (
 var _ = g.Describe("[sig-storage] STORAGE", func() {
 	defer g.GinkgoRecover()
 
-	var oc = exutil.NewCLI("storage-gcp-csi", exutil.KubeConfigPath())
+	var (
+		oc = exutil.NewCLI("storage-gcp-csi", exutil.KubeConfigPath())
 
+		storageTeamBaseDir   string
+		storageClassTemplate string
+		pvcTemplate          string
+		deploymentTemplate   string
+		podTemplate          string
+	)
 	// gcp-csi test suite cloud provider support check
 
 	g.BeforeEach(func() {
@@ -21,17 +28,18 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 		if !strings.Contains(cloudProvider, "gcp") {
 			g.Skip("Skip for non-supported cloud provider!!!")
 		}
+		storageTeamBaseDir = exutil.FixturePath("testdata", "storage")
+		storageClassTemplate = filepath.Join(storageTeamBaseDir, "storageclass-template.yaml")
+		pvcTemplate = filepath.Join(storageTeamBaseDir, "pvc-template.yaml")
+		deploymentTemplate = filepath.Join(storageTeamBaseDir, "dep-template.yaml")
+		podTemplate = filepath.Join(storageTeamBaseDir, "pod-template.yaml")
 	})
 
 	// author: chaoyang@redhat.com
 	// [GKE-PD-CSI] [Dynamic Regional PV]Check provisioned region pv and drain node function
 	g.It("Author:chaoyang-Critical-37490-[GKE-PD-CSI] Check provisioned region pv and drain node function [Disruptive]", func() {
 		var (
-			storageTeamBaseDir   = exutil.FixturePath("testdata", "storage")
-			storageClassTemplate = filepath.Join(storageTeamBaseDir, "storageclass-template.yaml")
-			pvcTemplate          = filepath.Join(storageTeamBaseDir, "pvc-template.yaml")
-			deploymentTemplate   = filepath.Join(storageTeamBaseDir, "dep-template.yaml")
-			storageClass         = newStorageClass(setStorageClassTemplate(storageClassTemplate), setStorageClassProvisioner("pd.csi.storage.gke.io"))
+			storageClass = newStorageClass(setStorageClassTemplate(storageClassTemplate), setStorageClassProvisioner("pd.csi.storage.gke.io"))
 
 			pvc = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimStorageClassName(storageClass.name),
 				setPersistentVolumeClaimCapacity("200Gi"))
@@ -95,11 +103,7 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 			g.Skip("Have less than 2 zones - skipping test ... ")
 		}
 		var (
-			storageTeamBaseDir   = exutil.FixturePath("testdata", "storage")
-			storageClassTemplate = filepath.Join(storageTeamBaseDir, "storageclass-template.yaml")
-			pvcTemplate          = filepath.Join(storageTeamBaseDir, "pvc-template.yaml")
-			deploymentTemplate   = filepath.Join(storageTeamBaseDir, "dep-template.yaml")
-			storageClass         = newStorageClass(setStorageClassTemplate(storageClassTemplate), setStorageClassProvisioner("pd.csi.storage.gke.io"))
+			storageClass = newStorageClass(setStorageClassTemplate(storageClassTemplate), setStorageClassProvisioner("pd.csi.storage.gke.io"))
 
 			pvc = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimStorageClassName(storageClass.name),
 				setPersistentVolumeClaimCapacity("200Gi"))
@@ -147,4 +151,52 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 		o.Expect(outPut).To(o.ContainSubstring(zones[1]))
 
 	})
+	// author: chaoyang@redhat.com
+	// Author:chaoyang-[GCP-PD-CSI] [VOLUME-TYPES] support scenarios testsuit
+	// https://cloud.google.com/compute/docs/disks
+	gcpPDvolTypeTestSuit := map[string]string{
+		"51150": "pd-ssd",      // High-51150-[GCP-PD-CSI] [Dynamic PV] pd-ssd type volumes should store data and allow exec of file
+		"51151": "pd-standard", // High-51151-[GCP-PD-CSI] [Dynamic PV] pd-standard type volumes should store data and allow exec of file
+		"51152": "pd-balanced", // High-51152-[GCP-PD-CSI] [Dynamic PV] pd-balanced type volumes should store data and allow exec of file
+		"51153": "pd-extreme",  // High-51153-[GCP-PD-CSI] [Dynamic PV] pd-extreme type volumes should store data and allow exec of file
+	}
+	caseIds := []string{"51150", "51151", "51152", "51153"}
+	for i := 0; i < len(caseIds); i++ {
+		volumeType := gcpPDvolTypeTestSuit[caseIds[i]]
+		g.It("Author:chaoyang-High-"+caseIds[i]+"-[GCP-PD-CSI] [VOLUME-TYPES] dynamic "+volumeType+" type pd volume should store data and allow exec of files", func() {
+			var (
+				storageClassParameters = map[string]string{
+					"replication-type": "none",
+					"type":             volumeType,
+				}
+				extraParameters = map[string]interface{}{
+					"parameters": storageClassParameters,
+				}
+				storageClass = newStorageClass(setStorageClassTemplate(storageClassTemplate), setStorageClassProvisioner("pd.csi.storage.gke.io"))
+				pvc          = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimStorageClassName(storageClass.name))
+				pod          = newPod(setPodTemplate(podTemplate), setPodPersistentVolumeClaim(pvc.name))
+			)
+			g.By("# Create new project for the scenario")
+			oc.SetupProject()
+
+			g.By("# Create \"" + volumeType + "\" type gcp-pd-csi storageclass")
+			storageClass.createWithExtraParameters(oc, extraParameters)
+			defer storageClass.deleteAsAdmin(oc) // ensure the storageclass is deleted whether the case exist normally or not
+
+			g.By("# Create a pvc with the gcp-pd-csi storageclass")
+			pvc.create(oc)
+			defer pvc.deleteAsAdmin(oc)
+
+			g.By("# Create pod with the created pvc and wait for the pod ready")
+			pod.create(oc)
+			defer pod.deleteAsAdmin(oc)
+			waitPodReady(oc, pod.namespace, pod.name)
+
+			g.By("# Check the pod volume can be read and write")
+			pod.checkMountedVolumeCouldRW(oc)
+
+			g.By("# Check the pod volume have the exec right")
+			pod.checkMountedVolumeHaveExecRight(oc)
+		})
+	}
 })
