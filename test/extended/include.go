bf0705817eb9f83a3b305f15aef7492226ffebad
Walid Abouhamad <wabouham@redhat.com>
2022-05-20 01:08:13 -0400

    Updated seventh revision code for PSAP GPU Operator Testcase OCP-48452 (#3368)

--- test/extended/include.go
+++ test/extended/include.go
@@ -25,6 +25,7 @@ import (
 	_ "github.com/openshift/openshift-tests-private/test/extended/ota/cvo"
 	_ "github.com/openshift/openshift-tests-private/test/extended/ota/osus"
 	_ "github.com/openshift/openshift-tests-private/test/extended/perfscale"
+	_ "github.com/openshift/openshift-tests-private/test/extended/psap/gpu"
 	_ "github.com/openshift/openshift-tests-private/test/extended/psap/nfd"
 	_ "github.com/openshift/openshift-tests-private/test/extended/psap/nto"
 	_ "github.com/openshift/openshift-tests-private/test/extended/psap/sro"
