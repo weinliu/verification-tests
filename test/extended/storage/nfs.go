cd359d0c06c5e9e252d3c929d5515a3b418fe273
Rahul Deore <rdeore@redhat.com>
2022-06-15 21:04:45 -0400

    Automation NFS test case OCP-52071 (#3747)

--- test/extended/storage/nfs.go
+++ test/extended/storage/nfs.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"path/filepath"
+	"strconv"
 
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
@@ -18,6 +19,7 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 		pvTemplate         string
 		pvcTemplate        string
 		dsTemplate         string
+		stsTemplate        string
 	)
 	// setup NFS server before each test case
 	g.BeforeEach(func() {
@@ -26,6 +28,7 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 		pvTemplate = filepath.Join(storageTeamBaseDir, "csi-pv-template.yaml")
 		pvcTemplate = filepath.Join(storageTeamBaseDir, "pvc-template.yaml")
 		dsTemplate = filepath.Join(storageTeamBaseDir, "ds-template.yaml")
+		stsTemplate = filepath.Join(storageTeamBaseDir, "sts-template.yaml")
 		svcNfsServer = setupNfsServer(oc, storageTeamBaseDir)
 	})
 
@@ -39,8 +42,8 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 		// Set the resource objects definition for the scenario
 		var (
 			scName = "nfs-sc-" + getRandomString()
-			pvc    = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimStorageClassName(svcNfsServer.deploy.name),
-				setPersistentVolumeClaimCapacity("5Gi"), setPersistentVolumeClaimAccessmode("ReadWriteMany"), setPersistentVolumeClaimStorageClassName(scName))
+			pvc    = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimStorageClassName(scName),
+				setPersistentVolumeClaimCapacity("5Gi"), setPersistentVolumeClaimAccessmode("ReadWriteMany"))
 			ds    = newDaemonSet(setDsTemplate(dsTemplate))
 			nfsPV = newPersistentVolume(setPersistentVolumeTemplate(pvTemplate), setPersistentVolumeAccessMode("ReadWriteMany"), setPersistentVolumeKind("nfs"),
 				setPersistentVolumeCapacity(pvc.capacity), setPersistentVolumeStorageClassName(scName), setPersistentVolumeReclaimPolicy("Delete"), setPersistentVolumeCapacity("5Gi"))
@@ -81,6 +84,73 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 
 		g.By("#. Delete the  Resources: pvc from namespace")
 		deleteSpecifiedResource(oc, "pvc", pvc.name, pvc.namespace)
+	})
+
+	// author: rdeore@redhat.com
+	// OCP-52071 [NFS] [StatefulSet] volumes should store data and allow exec of files on the volume
+	g.It("Author:rdeore-High-52071-[NFS] [StatefulSet] volumes should store data and allow exec of files on the volume", func() {
+		// Set the resource objects definition for the scenario
+		var (
+			scName     = "nfs-sc-" + getRandomString()
+			stsName    = "nfs-sts-" + getRandomString()
+			stsVolName = "vol-" + getRandomString()
+			replicaNum = 2
+			pvc        = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimCapacity("2Gi"),
+				setPersistentVolumeClaimAccessmode("ReadWriteOnce"), setPersistentVolumeClaimStorageClassName(scName))
+			pvc2 = newPersistentVolumeClaim(setPersistentVolumeClaimTemplate(pvcTemplate), setPersistentVolumeClaimCapacity("2Gi"),
+				setPersistentVolumeClaimAccessmode("ReadWriteOnce"), setPersistentVolumeClaimStorageClassName(scName))
+			sts = newSts(setStsTemplate(stsTemplate), setStsName(stsName), setStsReplicasNumber(strconv.Itoa(replicaNum)), setStsVolName(stsVolName), setStsSCName(scName))
+			pv  = newPersistentVolume(setPersistentVolumeTemplate(pvTemplate), setPersistentVolumeAccessMode("ReadWriteOnce"), setPersistentVolumeKind("nfs"),
+				setPersistentVolumeStorageClassName(scName), setPersistentVolumeReclaimPolicy("Delete"), setPersistentVolumeCapacity("2Gi"))
+			pv2 = newPersistentVolume(setPersistentVolumeTemplate(pvTemplate), setPersistentVolumeAccessMode("ReadWriteOnce"), setPersistentVolumeKind("nfs"),
+				setPersistentVolumeStorageClassName(scName), setPersistentVolumeReclaimPolicy("Delete"), setPersistentVolumeCapacity("2Gi"))
+			uniqueNodeNames = make(map[string]bool)
+		)
+
+		g.By("#. Create new project for the scenario")
+		oc.SetupProject()
+
+		g.By("#. Create a pv with the storageclass")
+		pv.nfsServerIP = svcNfsServer.svc.clusterIP
+		pv.create(oc)
+		defer pv.deleteAsAdmin(oc)
+		pv2.nfsServerIP = svcNfsServer.svc.clusterIP
+		pv2.create(oc)
+		defer pv2.deleteAsAdmin(oc)
+
+		g.By("#. Create a pvc with the storageclass")
+		pvc.name = stsVolName + "-" + stsName + "-0"
+		pvc.create(oc)
+		defer pvc.deleteAsAdmin(oc)
+		pvc2.name = stsVolName + "-" + stsName + "-1"
+		pvc2.create(oc)
+		defer pvc2.deleteAsAdmin(oc)
+
+		g.By("#. Create statefulSet pod with the created pvc and wait for the pod ready")
+		sts.scname = scName
+		sts.create(oc)
+		defer sts.deleteAsAdmin(oc)
+		sts.waitReady(oc)
+		podList, err := getPodsListByLabel(oc, sts.namespace, "app="+sts.applabel)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		for i := 0; i < replicaNum; i++ {
+			uniqueNodeNames[getNodeNameByPod(oc, sts.namespace, podList[i])] = true
+		}
+
+		g.By("#. Check the pods can read/write data inside volume")
+		sts.checkMountedVolumeCouldRW(oc)
+
+		g.By("# Check the pod volume have the exec right")
+		sts.checkMountedVolumeHaveExecRight(oc)
+
+		g.By("#. Delete the  Resources: statefulSet from namespace")
+		deleteSpecifiedResource(oc, "statefulset", sts.name, sts.namespace)
+
+		g.By("#. Check the volume umount from the node")
+		volName := sts.pvcname
+		for nodeName := range uniqueNodeNames {
+			checkVolumeNotMountOnNode(oc, volName, nodeName)
+		}
 	})
 })
 
