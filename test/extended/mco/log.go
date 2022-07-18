9b52999777e963f5c9676ff38204df65795014b3
Xiaozheng Liu (Rio) <rioliu@redhat.com>
2022-07-18 11:17:47 +0800

    wrappered logger impl, support debug logging. (#4137)
    * wrappered logger impl, support debug logging.
    
    * refine interface name
    
    * add comments for var and funcs
    
    * refine log format for timestamp, level fields and disable colorful output
    
    * refine level field format
    add contact info
    
    * remove unnecessary type conversion
    
    * remove enable debug func
    
    * move log impl from exutil to mco pkg
    
    * change pkg name to mco

---
+++ test/extended/mco/log.go
@@ -0,0 +1,67 @@
+package mco
+
+/*
+  author: rioliu@redhat.com
+*/
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/onsi/ginkgo"
+	"github.com/rs/zerolog"
+)
+
+const (
+	// EnableDebugLog env variable to enable debug logging
+	EnableDebugLog = "GINKGO_TEST_ENABLE_DEBUG_LOG"
+)
+
+// LogWrapper wrapper interface for zerolog
+type LogWrapper struct {
+	log zerolog.Logger
+}
+
+// NewLogger initialize log wrapper with zerolog logger
+// default log level is INFO, user can enable debug logging by env variable GINKGO_TEST_ENABLE_DEBUG_LOG
+func NewLogger() *LogWrapper {
+
+	// customize time field format to sync with e2e framework
+	zerolog.TimeFieldFormat = time.StampMilli
+	// initialize customized output to integrate with GinkgoWriter
+	output := zerolog.ConsoleWriter{Out: ginkgo.GinkgoWriter, TimeFormat: time.StampMilli}
+	// customize level format e.g. INFO, DEBUG, ERROR
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("%s:", i))
+	}
+	// disable colorful output for timestamp field
+	output.FormatTimestamp = func(i interface{}) string {
+		return fmt.Sprintf("%s:", i)
+	}
+	logger := &LogWrapper{log: zerolog.New(output).With().Timestamp().Logger()}
+	// set default log level to INFO
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	// if system env var is defined, enable debug logging
+	if _, enabled := os.LookupEnv(EnableDebugLog); enabled {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	return logger
+}
+
+// Infof log info level message
+func (lw *LogWrapper) Infof(format string, v ...interface{}) {
+	lw.log.Info().Msgf(format, v...)
+}
+
+// Debugf log debug level message
+func (lw *LogWrapper) Debugf(format string, v ...interface{}) {
+	lw.log.Debug().Msgf(format, v...)
+}
+
+// Errorf log error level message
+func (lw *LogWrapper) Errorf(format string, v ...interface{}) {
+	lw.log.Error().Msgf(format, v...)
+}
