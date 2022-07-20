67a53280a227a3fc34f5333ae832df5502360a22
Zhanqi Zhao <zzhao@redhat.com>
2022-07-20 09:17:17 +0800

    add x710 adapter (#4224)
    Co-authored-by: zzhao <zzhao@localhost.localdomain>

--- test/extended/networking/sriov.go
+++ test/extended/networking/sriov.go
@@ -374,4 +374,132 @@ var _ = g.Describe("[sig-networking] SDN sriov", func() {
 		o.Expect(testpmdOutput).Should(o.MatchRegexp("10.56.217"))
 
 	})
+
+	g.It("Author:zzhao-Medium-Longduration-41145-[xl710] sriov pod can be worked well with netdevice deviceType for xl710 adapter [Disruptive]", func() {
+		var (
+			buildPruningBaseDir            = exutil.FixturePath("testdata", "networking/sriov")
+			sriovNetworkNodePolicyTemplate = filepath.Join(buildPruningBaseDir, "sriovnetworkpolicy-template.yaml")
+			sriovNeworkTemplate            = filepath.Join(buildPruningBaseDir, "sriovnetwork-hostlocal-template.yaml")
+			sriovTestPodTemplate           = filepath.Join(buildPruningBaseDir, "sriov-netdevice-template.yaml")
+			sriovOpNs                      = "openshift-sriov-network-operator"
+		)
+		sriovPolicy := sriovNetworkNodePolicy{
+			policyName:   "xl710",
+			deviceType:   "netdevice",
+			deviceID:     "1583",
+			pfName:       "ens2f0",
+			vondor:       "8086",
+			numVfs:       3,
+			resourceName: "xl710netdevice",
+			template:     sriovNetworkNodePolicyTemplate,
+			namespace:    sriovOpNs,
+		}
+
+		g.By("check the sriov operator is running")
+		chkSriovOperatorStatus(oc, sriovOpNs)
+
+		g.By("Check the deviceID if exist on the cluster worker")
+		if !checkDeviceIDExist(oc, sriovOpNs, sriovPolicy.deviceID) {
+			g.Skip("the cluster do not contain the sriov card. skip this testing!")
+		}
+
+		g.By("Create sriovnetworkpolicy to init VF and check they are inited successfully")
+		defer rmSriovNetworkPolicy(oc, sriovPolicy.policyName, sriovOpNs)
+		sriovPolicy.createPolicy(oc)
+		waitForSriovPolicyReady(oc, sriovOpNs)
+
+		g.By("Create sriovNetwork to generate net-attach-def on the target namespace")
+		sriovnetwork := sriovNetwork{
+			name:             sriovPolicy.policyName,
+			resourceName:     sriovPolicy.resourceName,
+			networkNamespace: oc.Namespace(),
+			template:         sriovNeworkTemplate,
+			namespace:        sriovOpNs,
+		}
+		defer rmSriovNetwork(oc, sriovnetwork.name, sriovOpNs)
+		sriovnetwork.createSriovNetwork(oc)
+
+		g.By("Create test pod on the target namespace")
+
+		sriovTestPod := sriovTestPod{
+			name:        "xl710netdevice",
+			namespace:   oc.Namespace(),
+			networkName: sriovnetwork.name,
+			template:    sriovTestPodTemplate,
+		}
+		sriovTestPod.createSriovTestPod(oc)
+		err := waitForPodWithLabelReady(oc, oc.Namespace(), "name=sriov-netdevice")
+		exutil.AssertWaitPollNoErr(err, "this pod with label name=sriov-netdevice not ready")
+
+		g.By("Check test pod have second interface with assigned ip")
+		command := "ip a show net1"
+		testpmdOutput, err := e2e.RunHostCmd(sriovTestPod.namespace, sriovTestPod.name, command)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(testpmdOutput).Should(o.MatchRegexp("10.56.217"))
+
+	})
+	g.It("Author:zzhao-Medium-Longduration-41144-[xl710] DPDK works well in pod with vfio-pci for xl710 adapter [Disruptive]", func() {
+		var (
+			buildPruningBaseDir            = exutil.FixturePath("testdata", "networking/sriov")
+			sriovNetworkNodePolicyTemplate = filepath.Join(buildPruningBaseDir, "sriovnetworkpolicy-template.yaml")
+			sriovNeworkTemplate            = filepath.Join(buildPruningBaseDir, "sriovnetwork-template.yaml")
+			sriovTestPodTemplate           = filepath.Join(buildPruningBaseDir, "sriov-dpdk-template.yaml")
+			sriovOpNs                      = "openshift-sriov-network-operator"
+		)
+		sriovPolicy := sriovNetworkNodePolicy{
+			policyName:   "xl710",
+			deviceType:   "vfio-pci",
+			deviceID:     "1583",
+			pfName:       "ens2f0",
+			vondor:       "8086",
+			numVfs:       2,
+			resourceName: "xl710",
+			template:     sriovNetworkNodePolicyTemplate,
+			namespace:    sriovOpNs,
+		}
+
+		g.By("check the sriov operator is running")
+		chkSriovOperatorStatus(oc, sriovOpNs)
+
+		g.By("Check the deviceID if exist on the cluster worker")
+		if !checkDeviceIDExist(oc, sriovOpNs, sriovPolicy.deviceID) {
+			g.Skip("the cluster do not contain the sriov card. skip this testing!")
+		}
+
+		g.By("Create sriovnetworkpolicy to init VF and check they are inited successfully")
+		defer rmSriovNetworkPolicy(oc, sriovPolicy.policyName, sriovOpNs)
+		sriovPolicy.createPolicy(oc)
+		waitForSriovPolicyReady(oc, sriovOpNs)
+
+		g.By("Create sriovNetwork to generate net-attach-def on the target namespace")
+		sriovnetwork := sriovNetwork{
+			name:             sriovPolicy.policyName,
+			resourceName:     sriovPolicy.resourceName,
+			networkNamespace: oc.Namespace(),
+			template:         sriovNeworkTemplate,
+			namespace:        sriovOpNs,
+		}
+		defer rmSriovNetwork(oc, sriovnetwork.name, sriovOpNs)
+		sriovnetwork.createSriovNetwork(oc)
+
+		g.By("Create test pod on the target namespace")
+
+		sriovTestPod := sriovTestPod{
+			name:        "sriovdpdk",
+			namespace:   oc.Namespace(),
+			networkName: sriovnetwork.name,
+			template:    sriovTestPodTemplate,
+		}
+		sriovTestPod.createSriovTestPod(oc)
+		err := waitForPodWithLabelReady(oc, oc.Namespace(), "name=sriov-dpdk")
+		exutil.AssertWaitPollNoErr(err, "this pod with label name=sriov-dpdk not ready")
+
+		g.By("Check testpmd running well")
+		pciAddress := getPciAddress(sriovTestPod.namespace, sriovTestPod.name)
+		command := "testpmd -l 2-3 --in-memory -w " + pciAddress + " --socket-mem 1024 -n 4 --proc-type auto --file-prefix pg -- --disable-rss --nb-cores=1 --rxq=1 --txq=1 --auto-start --forward-mode=mac"
+		testpmdOutput, err := e2e.RunHostCmd(sriovTestPod.namespace, sriovTestPod.name, command)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(testpmdOutput).Should(o.MatchRegexp("forwards packets on 1 streams"))
+
+	})
 })
