1aaf9b1063174b60b812db6751a74736e4d2747b
Kasturi Narra <knarra@redhat.com>
2022-07-21 12:21:18 +0530

    Add disruptive tag for the case (#4364)

--- test/extended/workloads/ks_operator.go
+++ test/extended/workloads/ks_operator.go
@@ -107,7 +107,7 @@ var _ = g.Describe("[sig-apps] Workloads", func() {
 		}
 	})
 
-	g.It("Author:knarra-High-44049-DefaultPodTopologySpread doesn't work in non-CloudProvider env in OpenShift 4.7 [Flaky]", func() {
+	g.It("Author:knarra-High-44049-DefaultPodTopologySpread doesn't work in non-CloudProvider env in OpenShift 4.7 [Disruptive][Flaky]", func() {
 		nodeList, err := e2enode.GetReadySchedulableNodes(oc.KubeFramework().ClientSet)
 		// Create test project
 		g.By("Create test project")
