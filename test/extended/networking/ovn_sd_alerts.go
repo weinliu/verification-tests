9c92596762fb2fdfcf13d518e4e51b9860d6b673
Weibin Liang <weliang@redhat.com>
2022-08-07 22:54:14 -0400

    Automate OCP-51722 (#4632)

--- test/extended/networking/ovn_sd_alerts.go
+++ test/extended/networking/ovn_sd_alerts.go
@@ -1,11 +1,12 @@
 package networking
 
 import (
+	"strings"
+
 	g "github.com/onsi/ginkgo"
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
-	"strings"
 )
 
 var _ = g.Describe("[sig-networking] SDN", func() {
@@ -55,5 +56,27 @@ var _ = g.Describe("[sig-networking] SDN", func() {
 		o.Expect(runbookErr).NotTo(o.HaveOccurred())
 		e2e.Logf("The alertRunbook is %v", alertRunbook)
 		o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/NoOvnMasterLeader.md"))
+	})
+
+	g.It("Author:weliang-Medium-51722-Create runbook and link SOP for SouthboundStale alert", func() {
+		networkType := checkNetworkType(oc)
+		if !strings.Contains(networkType, "ovn") {
+			g.Skip("Skip testing on non-ovn cluster!!!")
+		}
+
+		alertName, NameErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[*].alert}").Output()
+		o.Expect(NameErr).NotTo(o.HaveOccurred())
+		e2e.Logf("The alertName is %v", alertName)
+		o.Expect(alertName).To(o.ContainSubstring("SouthboundStale"))
+
+		alertSeverity, severityErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NoOvnMasterLeader\")].labels.severity}").Output()
+		o.Expect(severityErr).NotTo(o.HaveOccurred())
+		e2e.Logf("alertSeverity is %v", alertSeverity)
+		o.Expect(alertSeverity).To(o.ContainSubstring("critical"))
+
+		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"SouthboundStale\")].annotations.runbook_url}").Output()
+		o.Expect(runbookErr).NotTo(o.HaveOccurred())
+		e2e.Logf("The alertRunbook is %v", alertRunbook)
+		o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/SouthboundStaleAlert.md"))
 	})
 })
