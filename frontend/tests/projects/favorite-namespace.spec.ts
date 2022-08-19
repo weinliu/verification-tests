3368b8a98c562c14f11567dce737c727e6aeec5e
Xiangyi Li <venusli370306@gmail.com>
2022-08-19 16:00:29 +0800

    Auto OCP-45319 (#4652)
    Update check logic for namespaces
    
    OCP-45319 revision
    
    Use more stable selector for favoriting projects.
    
    ocp45319 final revision
    
    Remove trailing lines
    
    Auto OCP-45319
    
    Update check logic for namespaces
    
    OCP-45319 revision
    
    Use more stable selector for favoriting projects.
    
    ocp45319 final revision
    
    Remove trailing lines

---
+++ frontend/tests/projects/favorite-namespace.spec.ts
@@ -0,0 +1,43 @@
+import { namespaceDropdown } from "views/namespace-dropdown";
+
+describe("namespace dropdown favorite test", () => {
+  before(() => {
+    cy.exec(
+      `oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env(
+        "LOGIN_USERNAME"
+      )} --kubeconfig ${Cypress.env("KUBECONFIG_PATH")}`
+    );
+    cy.login(
+      Cypress.env("LOGIN_IDP"),
+      Cypress.env("LOGIN_USERNAME"),
+      Cypress.env("LOGIN_PASSWORD")
+    );
+  });
+
+  after(() => {
+    cy.logout();
+    cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+  });
+
+  it("(OCP-45319, xiangyli) - Check the system namespaces that are Favorited will list in the Favorited list even if 'show default projects' is unselectedt", () => {
+    const namespaces = [
+      'openshift-apiserver-operator',
+      'openshift-authentication-operator'
+    ];
+
+    cy.visit('/search/all-namespaces');
+    namespaceDropdown.clickTheDropdown();
+    namespaceDropdown.clickDefaultProjectToggle();
+    namespaceDropdown.filterNamespace('openshift');
+
+    namespaces.forEach(namespace => {
+      namespaceDropdown.favoriteNamespace(namespace);
+      cy.get(`li:contains(${namespace})`).should('have.length', 2);
+    });
+
+    namespaceDropdown.clickDefaultProjectToggle();
+    namespaces.forEach(namespace => {
+      cy.get(`li:contains(${namespace})`).should('have.length', 1);
+    });
+  });
+});
--- frontend/views/namespace-dropdown.ts
+++ frontend/views/namespace-dropdown.ts
@@ -6,12 +6,22 @@ export const namespaceDropdown = {
         cy.get(selector).click();
     },
     clickTheDropdown: () => {
-        cy.get('button.co-namespace-dropdown__menu-toggle').click()
+        cy.get('button.co-namespace-dropdown__menu-toggle').click();
     },
     getProjectsDisplayed: () => {
-        return cy.get('li.pf-c-menu__list-item')
+        return cy.get('li[data-test="dropdown-menu-item-link"]');
     },
     clickDefaultProjectToggle: () => {
         cy.get('span.pf-c-switch__toggle').click()
-    }
+    },
+
+    filterNamespace: (name: string) => {
+        cy.get('[data-test="dropdown-text-filter"]').clear()
+          .type(name, { force: true });
+    },
+    favoriteNamespace: (name: string) => {
+        cy.get('[data-test="dropdown-menu-item-link"]').contains(name)
+          .next('button.pf-m-favorite').click();
+    },
+
 }
