e688bb80790c92e3fb1d930c2d07e3c01c996c6d
Xiangyi Li <venusli370306@gmail.com>
2022-08-25 15:28:38 +0800

    OCP-50657 Auto (#4725)
    * parent f83302332561c9b4d5f4de738f6d07bcfb0cccd2
    author Xiangyi Li <venusli370306@gmail.com> 1660528722 +0800
    committer Xiangyi Li <venusli370306@gmail.com> 1661246956 +0800
    
    OCP-50657
    
    Delete automation-41.sh
    
    OCP 50657 first revision: Restructure feature file and add deployments.
    
    OCP-50657 revised. Local run passed.
    
    The warning part seems to be invalid in 4.11. Some discussions may be needed.
    
    ocp 50657
    
    Update pod-disruption-budgets.spec.ts
    
    Delete deployments.ts
    
    Update deployments.yaml
    
    Update pod-disruption-budgets.spec.ts
    
    Auto ocp50657
    
    Updating project names in the url
    
    OCP-50657
    
    Delete automation-41.sh
    
    OCP 50657 first revision: Restructure feature file and add deployments.
    
    OCP-50657 revised. Local run passed.
    
    The warning part seems to be invalid in 4.11. Some discussions may be needed.
    
    ocp 50657
    
    Update pod-disruption-budgets.spec.ts
    
    Delete deployments.ts
    
    Auto ocp50657
    
    Updating project names in the url
    
    * Delete redundant step for deleting the resource
    
    * Update table head check for PDB list page
    
    * Update column head selection and PDB edit checkpoint
    
    * Check real time sync between form view and yaml view
    
    * Make sure the bot in the form view when creating pdb

---
+++ frontend/tests/workloads/pod-disruption-budgets.spec.ts
@@ -0,0 +1,54 @@
+import { pdbListPage } from './../../views/pod-disruption-budgets';
+
+describe('PDB List Page and Detail Page Test', () => {
+  const deploymentParams = {
+    name: 'example-deployment'
+  }
+
+  const testParams = {
+    fileName: 'deployments',
+    projectName: 'ocp50657'
+  }
+
+  before(() => {
+    cy.exec(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+    cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
+    cy.exec(`oc new-project ${testParams.projectName} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
+    cy.exec(`oc create -f ./fixtures/${testParams.fileName}.yaml -n ${testParams.projectName} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+  })
+
+  after(() => {
+    cy.logout();
+    cy.exec(`oc delete project ${testParams.projectName} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
+    cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+  })
+
+  it('(OCP-50657, xiangyli) - Add support for PDB (Pod Disruption Budget)', () => {
+    const pdbParams = {
+      name: 'example-pdbd',
+      label: 'app=name',
+      value: '6',
+      editValue: '50657'
+    }
+    cy.visit('/k8s/all-namespaces/poddisruptionbudgets');
+    cy.get('[data-test-id="resource-title"]').contains('PodDisruptionBudgets');
+    cy.get('thead').contains(/^Name(.*)Namespace(.*)Selector(.*)Availability(.*)Allowed disruptions(.*)Created$/);
+
+    cy.visit(`/k8s/ns/${testParams.projectName}/deployments/${deploymentParams.name}`);
+    cy.byLegacyTestID('actions-menu-button').click({force: true});
+    cy.byButtonText('Add PodDisruptionBudget').click({force: true});
+    pdbListPage.createPDB(pdbParams);
+
+    cy.visit(`/k8s/ns/${testParams.projectName}/poddisruptionbudgets/${pdbParams.name}`);
+    cy.get('[data-test-selector="details-item-value__Name"]').contains(pdbParams.name);
+    cy.get('.co-m-horizontal-nav__menu').contains(/Details|YAML|Pods/);
+
+    cy.visit(`/k8s/ns/${testParams.projectName}/deployments/${deploymentParams.name}`);
+    cy.byLegacyTestID('actions-menu-button').click({force: true});
+    cy.byButtonText('Edit PodDisruptionBudget').click({force: true});
+    cy.get(`input[value=${pdbParams.value}]`).clear().type(pdbParams.editValue);
+    cy.byTestID('yaml-view-input').click();
+    cy.get('.mtk16').contains(pdbParams.editValue);
+    cy.get('[id="save-changes"]').click();
+  });
+})
