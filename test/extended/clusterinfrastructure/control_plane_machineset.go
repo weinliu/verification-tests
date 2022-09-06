09cbe8e6b07cdde7a6fc06a1c328c5856bde3a31
Zhaohua Sun <zhsun@redhat.com>
2022-09-06 14:10:23 +0800

    auto OCP-53081 and OCP-53610 (#5116)

--- test/extended/clusterinfrastructure/control_plane_machineset.go
+++ test/extended/clusterinfrastructure/control_plane_machineset.go
@@ -23,11 +23,11 @@ var _ = g.Describe("[sig-cluster-lifecycle] Cluster_Infrastructure", func() {
 		if !(iaasPlatform == "aws" || iaasPlatform == "azure") {
 			g.Skip("Skip this scenario because it is not supported on the " + iaasPlatform + " platform")
 		}
-		g.By("Check if controlplanemachineset is created")
+		g.By("Check if controlplanemachineset exists")
 		controlplanemachineset, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("controlplanemachineset/cluster", "-n", machineAPINamespace).Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
 		if len(controlplanemachineset) == 0 {
-			g.Skip("Skip for controlplanemachineset is not deployed!")
+			g.Skip("Skip for controlplanemachineset doesn't exist!")
 		}
 
 		g.By("Check ownerReferences is added to master machines")
@@ -51,5 +51,43 @@ var _ = g.Describe("[sig-cluster-lifecycle] Cluster_Infrastructure", func() {
 			o.Expect(err).NotTo(o.HaveOccurred())
 			o.Expect(ownerReferences).Should(o.BeEmpty())
 		}
+	})
+
+	// author: zhsun@redhat.com
+	g.It("Author:zhsun-Medium-53081-Finalizer should be added to control plan machineset [Disruptive]", func() {
+		exutil.SkipConditionally(oc)
+		if !(iaasPlatform == "aws" || iaasPlatform == "azure") {
+			g.Skip("Skip this scenario because it is not supported on the " + iaasPlatform + " platform")
+		}
+
+		g.By("Check if controlplanemachineset exists")
+		controlplanemachineset, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("controlplanemachineset/cluster", "-n", machineAPINamespace).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		if len(controlplanemachineset) == 0 {
+			g.Skip("Skip for controlplanemachineset doesn't exist!")
+		}
+
+		g.By("Check finalizer is added to controlplanemachineset")
+		finalizers, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("controlplanemachineset/cluster", "-o=jsonpath={.metadata.finalizers[0]}", "-n", machineAPINamespace).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(finalizers).To(o.ContainSubstring("controlplanemachineset.machine.openshift.io"))
+
+		g.By("Remove finalizer")
+		err = oc.AsAdmin().WithoutNamespace().Run("patch").Args("controlplanemachineset/cluster", "-p", `{"metadata":{"finalizers":null}}`, "--type=merge", "-n", machineAPINamespace).Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("Finalizer should be re-added to controlplanemachineset")
+		finalizers, err = oc.AsAdmin().WithoutNamespace().Run("get").Args("controlplanemachineset/cluster", "-o=jsonpath={.metadata.finalizers[0]}", "-n", machineAPINamespace).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(finalizers).To(o.ContainSubstring("controlplanemachineset.machine.openshift.io"))
+	})
+
+	// author: zhsun@redhat.com
+	g.It("Author:zhsun-High-53610-Operator control-plane-machine-set should be in Available state and report version information", func() {
+		state, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("clusteroperator/control-plane-machine-set", "-o=jsonpath={.status.conditions[?(@.type==\"Available\")].status}{.status.conditions[?(@.type==\"Progressing\")].status}{.status.conditions[?(@.type==\"Degraded\")].status}").Output()
+		version, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("clusteroperator/control-plane-machine-set", "-o=jsonpath={.status.versions[0].version}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(state).To(o.ContainSubstring("TrueFalseFalse"))
+		o.Expect(version).To(o.ContainSubstring("4."))
 	})
 })
