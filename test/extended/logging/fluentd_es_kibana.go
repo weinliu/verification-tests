43c6f05896c4a45b7d5332a30a16d6d05137edff
Anping Li <anpingli@users.noreply.github.com>
2022-09-13 10:01:24 +0800

    skip OCP-36368 as known issue https://issues.redhat.com/browse/LOG-2999 (#5256)

--- test/extended/logging/fluentd_es_kibana.go
+++ test/extended/logging/fluentd_es_kibana.go
@@ -46,6 +46,7 @@ var _ = g.Describe("[sig-openshift-logging] Logging NonPreRelease", func() {
 		// author ikanse@redhat.com
 		g.It("CPaasrunOnly-Author:ikanse-Medium-36368-Elasticsearch nodes can scale down[Serial][Slow]", func() {
 			// create clusterlogging instance with elasticsearch node count set to 3
+			g.Skip("Known issue: https://issues.redhat.com/browse/LOG-2999")
 			g.By("deploy EFK pods")
 			sc, err := getStorageClassName(oc)
 			o.Expect(err).NotTo(o.HaveOccurred())
