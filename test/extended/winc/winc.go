82174d69f58e83593d304748e9e4f85edde32c03
Jose Luis Franco Arza <jfrancoa@redhat.com>
2022-09-23 07:58:23 +0200

    Remove NonUnifyCI tag for WINC. (#5420)

--- test/extended/winc/winc.go
+++ test/extended/winc/winc.go
@@ -19,7 +19,7 @@ import (
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
 
-var _ = g.Describe("[sig-windows] Windows_Containers NonUnifyCI", func() {
+var _ = g.Describe("[sig-windows] Windows_Containers", func() {
 	defer g.GinkgoRecover()
 
 	var (
