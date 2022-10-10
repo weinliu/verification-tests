a3dbc949876fa4dd931818d28920dbbcc169699d
Xiangyi Li <venusli370306@gmail.com>
2022-10-10 11:01:40 +0800

    Auto ocp54014 (#5419)
    Update user policy
    
    Final version of OCP-54014
    
    Final version updated ocp54014. Merged cases into secrets.ts
    
    Delete temporary files
    
    Remove admin privileges

---
+++ frontend/tests/workloads/secrets.ts
@@ -0,0 +1,45 @@
+import { testName } from "../../upstream/support";
+import { Secrets } from "../../views/secrect";
+import { listPage } from "../../upstream/views/list-page";
+import { detailsPage } from "../../upstream/views/details-page";
+import { guidedTour } from '../../upstream/views/guided-tour';
+
+describe('Workload Secrets test', () => {
+  before(() => {
+    cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
+    guidedTour.close();
+    cy.switchPerspective('Administrator');
+    cy.createProject(testName);
+    cy.exec(`oc create -f ./fixtures/secret-tls.yaml -n ${testName} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);   
+    cy.exec(`oc create secret generic test1 -n ${testName} --from-file=data1=./fixtures/original.jks --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
+    cy.exec(`oc get secret -n ${testName} test1 -o yaml > ./fixtures/secret1.yaml --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
+  });
+
+  after(() => {
+    cy.exec(`oc delete project ${testName} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+    cy.logout();
+  });
+
+  it('OCP-47010 - Check Secrets is editable on console', () => {
+    Secrets.gotoSecretsPage(testName);
+    listPage.filter.byName('tlssecret');
+    listPage.rows.countShouldBe(1);
+
+    listPage.rows.clickKebabAction('tlssecret','Edit Secret')
+    cy.url().should('include',`/tlssecret/edit`);
+    
+    Secrets.addKeyValue("keyfortest", "valuefortest");
+    cy.get('#save-changes').click();
+    detailsPage.isLoaded();
+    Secrets.validKeyValueExist("keyfortest", "valuefortest");
+  });
+
+  it('(OCP-54014, xiangyli) Check Base64 data value for jave keystore secret would not change without changing anything', () => {
+    cy.visit(`/k8s/ns/${testName}/secrets/test1/edit`)
+    cy.byTestID('save-changes').click()
+    cy.exec(`oc get secret -n ${testName} test1 -o yaml --kubeconfig ${Cypress.env('KUBECONFIG_PATH')} > ./fixtures/secret2.yaml`)
+    cy.exec(`diff ./fixtures/secret1.yaml ./fixtures/secret2.yaml`)
+      .its('stdout')
+      .should('eq', '')
+  })
+})
