1e2919d9ce95fb3e3597c04d99f469c7a71f1976
Shudi Li <shudili@redhat.com>
2022-10-19 17:53:48 +0800

    Created two auto cases for NE-568 (#5664)
    * Created two auto cases for NE-568
    
    * Updated for NE-568
    
    * Removed two default words for NE-568
    
    * Replaced one word in the description of NE-568

--- test/extended/router/ingress-operator.go
+++ test/extended/router/ingress-operator.go
@@ -193,4 +193,146 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		o.Expect(findAnnotation).NotTo(o.ContainSubstring("nlb"))
 	})
 
+	// author: shudili@redhat.com
+	g.It("Author:shudili-High-54868-Configurable dns Management for LoadBalancerService Ingress Controllers on AWS", func() {
+		var (
+			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
+			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-external.yaml")
+			ingctrl1            = ingctrlNodePortDescription{
+				name:      "ocp54868cus1",
+				namespace: "openshift-ingress-operator",
+				domain:    "",
+				template:  customTemp,
+			}
+			ingctrl2 = ingctrlNodePortDescription{
+				name:      "ocp54868cus2",
+				namespace: "openshift-ingress-operator",
+				domain:    "ocp54868cus2.test.com",
+				template:  customTemp,
+			}
+			ingctrlResource1   = "ingresscontrollers/" + ingctrl1.name
+			dnsrecordResource1 = "dnsrecords/" + ingctrl1.name + "-wildcard"
+			ingctrlResource2   = "ingresscontrollers/" + ingctrl2.name
+			dnsrecordResource2 = "dnsrecords/" + ingctrl2.name + "-wildcard"
+		)
+
+		// skip if platform is not AWS
+		output, _ := oc.AsAdmin().WithoutNamespace().Run("get").Args("infrastructure", "cluster", "-o=jsonpath={.status.platformStatus.type}").Output()
+		if !strings.Contains(output, "AWS") {
+			g.Skip("Skip for non-supported platform, it requires AWS")
+		}
+
+		g.By("Create two custom ingresscontrollers, one matches the cluster's base domain, the other doesn't")
+		baseDomain := getBaseDomain(oc)
+		ingctrl1.domain = ingctrl1.name + "." + baseDomain
+		defer ingctrl1.delete(oc)
+		ingctrl1.create(oc)
+		defer ingctrl2.delete(oc)
+		ingctrl2.create(oc)
+		err := waitForCustomIngressControllerAvailable(oc, ingctrl1.name)
+		exutil.AssertWaitPollNoErr(err, fmt.Sprintf("ingresscontroller %s conditions not available", ingctrl1.name))
+		err = waitForCustomIngressControllerAvailable(oc, ingctrl2.name)
+		exutil.AssertWaitPollNoErr(err, fmt.Sprintf("ingresscontroller %s conditions not available", ingctrl2.name))
+
+		g.By("check the default dnsManagementPolicy value of ingress-controller1 matching the base domain, which should be Managed")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(ingctrlResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.spec.endpointPublishingStrategy.loadBalancer.dnsManagementPolicy}").Output()
+		o.Expect(output).To(o.ContainSubstring("Managed"))
+
+		g.By("check ingress-controller1's status")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(ingctrlResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.status.conditions[?(@.type==\"DNSManaged\")].status}{.status.conditions[?(@.type==\"DNSReady\")].status}").Output()
+		o.Expect(output).To(o.ContainSubstring("TrueTrue"))
+
+		g.By("check the default dnsManagementPolicy value of dnsrecord ocp54868cus1, which should be Managed, too")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(dnsrecordResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.spec.dnsManagementPolicy}").Output()
+		o.Expect(output).To(o.ContainSubstring("Managed"))
+
+		g.By("check dnsrecord ocp54868cus1's status")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(dnsrecordResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.status.zones[0].conditions[0].status}{.status.zones[0].conditions[0].reason}").Output()
+		o.Expect(output).To(o.ContainSubstring("TrueProviderSuccess"))
+
+		g.By("patch custom ingress-controller1 with dnsManagementPolicy Unmanaged")
+		patchResourceAsAdmin(oc, ingctrl1.namespace, ingctrlResource1, "{\"spec\":{\"endpointPublishingStrategy\":{\"loadBalancer\":{\"dnsManagementPolicy\":\"Unmanaged\"}}}}")
+
+		g.By("check the dnsManagementPolicy value of ingress-controller1, which should be Unmanaged")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(ingctrlResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.spec.endpointPublishingStrategy.loadBalancer.dnsManagementPolicy}").Output()
+		o.Expect(output).To(o.ContainSubstring("Unmanaged"))
+
+		g.By("check ingress-controller1's status")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(ingctrlResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.status.conditions[?(@.type==\"DNSManaged\")].status}{.status.conditions[?(@.type==\"DNSReady\")].status}").Output()
+		o.Expect(output).To(o.ContainSubstring("FalseUnknown"))
+
+		g.By("check the dnsManagementPolicy value of dnsrecord ocp54868cus1, which should be Unmanaged, too")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(dnsrecordResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.spec.dnsManagementPolicy}").Output()
+		o.Expect(output).To(o.ContainSubstring("Unmanaged"))
+
+		g.By("check dnsrecord ocp54868cus1's status")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(dnsrecordResource1, "-n", ingctrl1.namespace, "-o=jsonpath={.status.zones[0].conditions[0].status}{.status.zones[0].conditions[0].reason}").Output()
+		o.Expect(output).To(o.ContainSubstring("UnknownUnmanagedDNS"))
+
+		// there was a bug OCPBUGS-2247 in the below test step
+		// g.By("check the default dnsManagementPolicy value of ingress-controller2 not matching the base domain, which should be Unmanaged")
+		// output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(ingctrlResource2, "-n", ingctrl2.namespace, "-o=jsonpath={.spec.endpointPublishingStrategy.loadBalancer.dnsManagementPolicy}").Output()
+		//o.Expect(output).To(o.ContainSubstring("Unmanaged"))
+
+		g.By("check ingress-controller2's status")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(ingctrlResource2, "-n", ingctrl2.namespace, "-o=jsonpath={.status.conditions[?(@.type==\"DNSManaged\")].status}{.status.conditions[?(@.type==\"DNSReady\")].status}").Output()
+		o.Expect(output).To(o.ContainSubstring("FalseUnknown"))
+
+		// there was a bug OCPBUGS-2247 in the below test step
+		// g.By("check the default dnsManagementPolicy value of dnsrecord ocp54868cus2, which should be Unmanaged, too")
+		// output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(dnsrecordResource2, "-n", ingctrl2.namespace, "-o=jsonpath={.spec.dnsManagementPolicy}").Output()
+		// o.Expect(output).To(o.ContainSubstring("Unmanaged"))
+
+		g.By("check dnsrecord ocp54868cus2's status")
+		output, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(dnsrecordResource2, "-n", ingctrl2.namespace, "-o=jsonpath={.status.zones[0].conditions[0].status}{.status.zones[0].conditions[0].reason}").Output()
+		o.Expect(output).To(o.ContainSubstring("UnknownUnmanagedDNS"))
+
+	})
+
+	// author: shudili@redhat.com
+	g.It("Author:shudili-Low-54995-Negative Test of Configurable dns Management for LoadBalancerService Ingress Controllers on AWS", func() {
+		var (
+			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
+			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-external.yaml")
+			ingctrl             = ingctrlNodePortDescription{
+				name:      "ocp54995",
+				namespace: "openshift-ingress-operator",
+				domain:    "ocp54995.test.com",
+				template:  customTemp,
+			}
+			ingctrlResource = "ingresscontrollers/" + ingctrl.name
+		)
+
+		// skip if platform is not AWS
+		output, _ := oc.AsAdmin().WithoutNamespace().Run("get").Args("infrastructure", "cluster", "-o=jsonpath={.status.platformStatus.type}").Output()
+		if !strings.Contains(output, "AWS") {
+			g.Skip("Skip for non-supported platform, it requires AWS")
+		}
+
+		g.By("Create a custom ingresscontrollers")
+		defer ingctrl.delete(oc)
+		ingctrl.create(oc)
+		err := waitForCustomIngressControllerAvailable(oc, ingctrl.name)
+		exutil.AssertWaitPollNoErr(err, fmt.Sprintf("ingresscontroller %s conditions not available", ingctrl.name))
+
+		g.By("try to patch the custom ingress-controller with dnsManagementPolicy unmanaged")
+		patch := "{\"spec\":{\"endpointPublishingStrategy\":{\"loadBalancer\":{\"dnsManagementPolicy\":\"unmanaged\"}}}}"
+		output, err = oc.AsAdmin().WithoutNamespace().Run("patch").Args(ingctrlResource, "-p", patch, "--type=merge", "-n", ingctrl.namespace).Output()
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(output).To(o.ContainSubstring("dnsManagementPolicy: Unsupported value: \"unmanaged\": supported values: \"Managed\", \"Unmanaged\""))
+
+		g.By("try to patch the custom ingress-controller with dnsManagementPolicy abc")
+		patch = "{\"spec\":{\"endpointPublishingStrategy\":{\"loadBalancer\":{\"dnsManagementPolicy\":\"abc\"}}}}"
+		output, err = oc.AsAdmin().WithoutNamespace().Run("patch").Args(ingctrlResource, "-p", patch, "--type=merge", "-n", ingctrl.namespace).Output()
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(output).To(o.ContainSubstring("dnsManagementPolicy: Unsupported value: \"abc\": supported values: \"Managed\", \"Unmanaged\""))
+
+		g.By("try to patch the custom ingress-controller with dnsManagementPolicy 123")
+		patch = "{\"spec\":{\"endpointPublishingStrategy\":{\"loadBalancer\":{\"dnsManagementPolicy\":123}}}}"
+		output, err = oc.AsAdmin().WithoutNamespace().Run("patch").Args(ingctrlResource, "-p", patch, "--type=merge", "-n", ingctrl.namespace).Output()
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(output).To(o.ContainSubstring("dnsManagementPolicy: Unsupported value: 123: supported values: \"Managed\", \"Unmanaged\""))
+
+	})
+
 })
