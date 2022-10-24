da9964c916893b080db5ce5f79bf4ff0dfe378b2
Ya Dan Pei <yapei@redhat.com>
2022-10-24 11:29:59 +0800

    auto OCP-51743 (#5679)

--- frontend/tests/dynamic-plugins.ts
+++ frontend/tests/dynamic-plugins.ts
@@ -15,41 +15,72 @@ describe('Dynamic plugins features', () => {
       cy.exec(`oc create -f ./fixtures/${resource}.yaml -n ${demoPluginNamespace} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
         .then(result => { expect(result.stdout).contain("created")})
     });
-
-    // enable plugin
-    cy.exec(`oc patch console.operator cluster -p '{"spec":{"plugins":["console-demo-plugin"]}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+    cy.exec(`oc apply -f ./fixtures/console-customization-plugin-manifest.yaml --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
     
     // login via web
     cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
-
-    // set console to Unmanaged
-    cy.exec(`oc patch console.operator cluster -p '{"spec":{"managementState":"Unmanaged"}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`).then((result) => {
-      expect(result.stdout).contains('patched')
-    });
-    cy.exec(`oc get cm console-config -n openshift-console -o yaml --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, {timeout: 60000}).then((result) => {
-      expect(result.stdout).contains('console-demo-plugin')
-    });
+  });
+  beforeEach(() => {
+    guidedTour.close();
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/locales/resource.json?lng=en&ns=plugin__console-demo-plugin'
+      },
+      {}
+    ).as('getConsoleDemoPluginLocales');
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/locales/resource.json?lng=en&ns=plugin__console-customization'
+      },
+      {}
+    ).as('getConsoleCustomizaitonPluginLocales');    
   });
   after(() => {
-    cy.exec(`oc delete namespace console-demo-plugin --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
     cy.exec(`oc patch console.operator cluster -p '{"spec":{"managementState":"Managed"}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+    cy.exec(`oc delete namespace console-demo-plugin console-customization-plugin --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
     cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
     cy.exec(`oc patch console.operator cluster -p '{"spec":{"plugins":null}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
-    cy.exec(`oc delete consoleplugin console-demo-plugin --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+    cy.exec(`oc delete consoleplugin --all --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`); 
   });
-  it('(OCP-45629,yapei) dynamic plugins proxy to services on the cluster', {tags: ['e2e','admin']}, () => {
-    nav.sidenav.switcher.changePerspectiveTo('Developer');
-    guidedTour.close();
-    cy.wait(30000);
-    cy.get('body').then(($body) => {
-      if ($body.find(`[data-test="toast-action"]`).length) {
-        cy.contains('Refresh web console').click();
-      }
-    });
 
-    // demo plugin in Dev perspective
-    nav.sidenav.clickNavLink(['Demo Plugin']);
+  it('(OCP-51743,yapei) Preload - locale files are loaded once plugin is enabled', {tags: ['e2e','admin']},() => {
+    // enable console-customization plugin
+    cy.exec(`oc patch console.operator cluster -p '{"spec":{"plugins":["console-customization"]}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
+      .then(result => expect(result.stdout).contains('patched'));
+    cy.get('[data-test="toast-action"]', {timeout: 60000});
+    cy.contains('Refresh web console').click({force: true});
+    cy.wait('@getConsoleCustomizaitonPluginLocales',{timeout: 60000});
+  });
+  it('(OCP-51743,yapei) Lazy - do not load locale files during enablement', {tags: ['e2e','admin']},() => {
+    nav.sidenav.switcher.changePerspectiveTo('Developer');
+    // enable console-demo-plugin
+    cy.exec(`oc patch console.operator cluster -p '{"spec":{"plugins":["console-customization", "console-demo-plugin"]}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`)
+      .then(result => expect(result.stdout).contains('patched'));
+    cy.get('[data-test="toast-action"]', {timeout: 60000});
+    cy.contains('Refresh web console').click({force: true});
+    cy.wait('@getConsoleDemoPluginLocales', {timeout: 60000});
+    cy.on('fail', (e)=>{
+      console.log(e.message)
+      if (!e.message.includes('No request ever occurred')){
+        throw e;
+      }
+    })
+  });
+
+  it('(OCP-51743,yapei) Lazy - locale files are only loaded when visit plugin pages',{tags: ['e2e','admin']},() => {
+    cy.clickNavLink(['Demo Plugin', 'Test Consumer']);
+    cy.wait('@getConsoleDemoPluginLocales', {timeout: 30000})
+      .then((intercept)=>{
+        const { statusCode } = intercept.response
+        expect(statusCode).to.eq(200)
+      })
+  });
+
+  it('(OCP-45629,yapei) dynamic plugins proxy to services on the cluster', {tags: ['e2e','admin']},() => {
     nav.sidenav.shouldHaveNavSection(['Demo Plugin']);
+    // demo plugin in Dev perspective
     cy.get('.pf-c-nav__link').should('include.text', 'Dynamic Nav 1');
     cy.get('.pf-c-nav__link').should('include.text', 'Dynamic Nav 2');
     // demo plugin in Administrator perspective
@@ -89,11 +120,15 @@ describe('Dynamic plugins features', () => {
     })
     cy.get(".pf-c-popover__body").within(($div) => {
       cy.get('a:contains(View all)').should('have.attr', 'href', '/k8s/cluster/operator.openshift.io~v1~Console/cluster/console-plugins')
-      cy.contains(`${1}/${total} enabled`).should('exist')
+      cy.contains(`${2}/${total} enabled`).should('exist')
     })
   })
 
   it('(OCP-53234,yapei) Show alert when console operator is Unmanaged', {tags: ['e2e','admin']}, () => {
+    // set console to Unmanaged
+    cy.exec(`oc patch console.operator cluster -p '{"spec":{"managementState":"Unmanaged"}}' --type merge --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`).then((result) => {
+      expect(result.stdout).contains('patched')
+    });
     cy.visit('/k8s/cluster/operator.openshift.io~v1~Console/cluster/console-plugins');
     cy.get('a[data-test-id="console-demo-plugin"]').should('exist');
     cy.contains('unmanaged').should('exist');
