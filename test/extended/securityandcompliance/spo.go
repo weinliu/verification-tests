ce01a994a66ba1f51444b8aa813ba63301d82c66
Xiaojie Yuan <xiyuan@redhat.com>
2022-11-18 09:55:35 +0800

    update channel to release-0.5 for Security Profiles Operator (#6181)

--- test/extended/securityandcompliance/spo.go
+++ test/extended/securityandcompliance/spo.go
@@ -46,7 +46,7 @@ var _ = g.Describe("[sig-isc] Security_and_Compliance The Security Profiles Oper
 		subD = subscriptionDescription{
 			subName:                "security-profiles-operator-sub",
 			namespace:              "security-profiles-operator",
-			channel:                "release-0.4",
+			channel:                "release-0.5",
 			ipApproval:             "Automatic",
 			operatorPackage:        "security-profiles-operator",
 			catalogSourceName:      "qe-app-registry",
