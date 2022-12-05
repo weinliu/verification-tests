5aba3ff623d28a83d35a1800fade5bc5a268e8b9
Ke Wang <kewang@redhat.com>
2022-12-05 10:43:29 +0800

    skip case ocp-39601 automation execution (#6512)

--- test/extended/apiserverauth/apiserver.go
+++ test/extended/apiserverauth/apiserver.go
@@ -2279,6 +2279,7 @@ spec:
 
 	// author: zxiao@redhat.com
 	g.It("NonHyperShiftHOST-ROSA-ARO-OSD_CCS-Author:zxiao-High-39601-Examine critical errors in openshift-kube-apiserver related log files", func() {
+		g.Skip("This test always fails due to non-real critical errors and is not suitable for automated testing and will be tested manually instead, skip.")
 		g.By("1) Create log arrays.")
 		podAbnormalLogs := make([]string, 0)
 		masterNodeAbnormalLogs := make([]string, 0)
