262a7b307d5a580cdf8016442d01d26fd8c8e2ab
Xiyun Zhao <xiyuzhao@redhat.com>
2022-12-12 18:37:16 +0800

    Add new auto test case OCP-55684 (#6676)

--- frontend/tests/operators/operator-hub.ts
+++ frontend/tests/operators/operator-hub.ts
@@ -1,6 +1,15 @@
 import {operatorHubPage, OperatorHubSelector} from "../../views/operator-hub-page";
+import { listPage } from "upstream/views/list-page";
 
 describe('Operator Hub tests', () => {
+    const testParams = {
+        catalogName: 'custom-catalogsource',
+        catalogNamespace: 'openshift-marketplace',
+        suggestedNamespace: 'testxi3210',
+        suggestedNamespaceLabels: 'foo:testxi3120',
+        suggestedNamespaceannotations: 'baz:testxi3120'
+      }
+    
     before(() => {
         cy.exec(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
         cy.exec(`oc create -f ./fixtures/operators/custom-catalog-source.json --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
@@ -9,6 +18,7 @@ describe('Operator Hub tests', () => {
 
     after(() => {
         cy.exec(`oc delete CatalogSource custom-catalogsource -n openshift-marketplace --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+        cy.exec(`oc delete project ${testParams.suggestedNamespace} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
         cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
     });
 
@@ -51,5 +61,22 @@ describe('Operator Hub tests', () => {
                 cy.contains('No Results Match the Filter Criteria').should('exist');
             })
         });
+    });
+  
+    it('(OCP-55684, xiyuzhao) Allow operator to specitfy where to run with CSV suggested namespace template annotation', {tags: ['e2e','admin']}, () => {
+        cy.visit(`operatorhub/subscribe?pkg=flux-operator&catalog=${testParams.catalogName}&catalogNamespace=${testParams.catalogNamespace}&targetNamespace=undefined`)
+          .get('[data-test-id="resource-title"]')
+          .should('contain.text','Install Operator')
+        cy.contains(`${testParams.suggestedNamespace} (Operator recommended)`).should('exist')
+        cy.contains(`${testParams.suggestedNamespace} does not exist and will be created`).should('exist')
+        cy.get('[data-test="install-operator"]').click()
+
+        cy.visit('/k8s/cluster/projects')
+        listPage.filter.byName(`${testParams.suggestedNamespace}`)
+        listPage.rows.shouldExist(`${testParams.suggestedNamespace}`)
+        cy.exec(`oc get project ${testParams.suggestedNamespace} -o template --template={{.metadata}} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false })
+          .its('stdout')
+          .should('contain',`${testParams.suggestedNamespaceLabels}`)
+          .and('contain',`${testParams.suggestedNamespaceannotations}`)        
     })
 })
