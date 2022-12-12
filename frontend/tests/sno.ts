d0bcc54f44470b551027f9ae44b3cf91097ffa44
Yanping Zhang <yanpzhan@redhat.com>
2022-12-12 18:37:07 +0800

    auto ocp-39677 (#6626)

---
+++ frontend/tests/sno.ts
@@ -0,0 +1,27 @@
+describe('console feature on sno cluster', () => {
+  before(() => {
+    cy.exec(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+  });
+
+  after(() => {
+    cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`);
+  });
+
+  it('(OCP-39677,yanpzhan) Console supports on single-node cluster', {tags: ['e2e','admin']}, () => {
+    cy.exec(`oc get infrastructures.config.openshift.io cluster --template={{.status.controlPlaneTopology}} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false }).then((result) => {
+      if(result.stdout == 'SingleReplica') {
+        cy.log("Testing on SNO cluster.");
+        cy.exec(`oc get deployments -n openshift-console -o jsonpath='{.items[*].spec.replicas}' --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false }).then((output) => { 
+          expect(output.stdout).to.equal('1 1');
+        });
+        cy.exec(`oc get deployments -n openshift-console -o jsonpath='{.items[*].spec.template.spec.affinity}' --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false }).then((output) => {                                                                            
+          expect(output.stdout).to.equal('{} {}');
+        });
+      } else {
+        cy.log("Testing on none-SNO cluster.");
+      }
+    });
+  });
+
+})
+
