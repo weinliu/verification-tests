bf79e5d4d0a98129330ed0801740652ce5b15804
Hongan Li <hongli@redhat.com>
2023-01-17 10:12:16 +0800

    update haproxy version to 2.6.6 for 4.13 (#7261)

--- test/extended/router/haproxy-router.go
+++ test/extended/router/haproxy-router.go
@@ -479,7 +479,7 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 	// author: shudili@redhat.com
 	g.It("Author:shudili-Low-49131-check haproxy's version", func() {
-		var expVersion = "2.2.24"
+		var expVersion = "2.6.6"
 		g.By("rsh to a default router pod and get the HAProxy's version")
 		haproxyVer := getHAProxyVersion(oc)
 		g.By("show haproxy version(" + haproxyVer + "), and check if it is updated successfully")
