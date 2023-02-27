942af0f78f055e345bf8e76665fef801dfc0962c
Anurag Saxena <anusaxen@redhat.com>
2023-02-27 01:43:40 -0500

    MicroShift automation kickoff for SDN (#7805)
    * ufhift auto
    
    * usift auto
    
    * ushift auto
    
    * golint fix
    
    * uncomment defer step
    
    * addressed comments
    
    * addressed comments
    
    * yaml extension added  temp-ping-pod file
    
    * Added pod-selector allowto and allow from usecase as well
    
    * golint fixes
    
    * Addressed comments
    
    * golint
    
    * Addressed comments
    
    * upgrade go to 1.19.6 and make update
    
    ---------
    
    Co-authored-by: Anurag Saxena <anusaxen@anusaxen.remote.csb>

---
+++ test/extended/networking/microshift.go
@@ -0,0 +1,256 @@
+package networking
+
+import (
+	"fmt"
+	"net"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	g "github.com/onsi/ginkgo/v2"
+	o "github.com/onsi/gomega"
+
+	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+)
+
+var _ = g.Describe("[sig-networking] SDN", func() {
+	defer g.GinkgoRecover()
+
+	var oc = exutil.NewCLIWithoutNamespace("SDN-microshift")
+
+	// author: anusaxen@redhat.com
+	g.It("MicroShiftOnly-Author:anusaxen-Critical-60331-mixed ingress and egress policies can work well", func() {
+		var (
+			caseID              = "60331"
+			buildPruningBaseDir = exutil.FixturePath("testdata", "networking")
+			e2eTestNamespace1   = "e2e-ushift-sdn-" + caseID + "-" + getRandomString()
+			e2eTestNamespace2   = "e2e-ushift-sdn-" + caseID + "-" + getRandomString()
+			testPodFile         = filepath.Join(buildPruningBaseDir, "testpod.yaml")
+			helloSdnFile        = filepath.Join(buildPruningBaseDir, "hellosdn.yaml")
+			egressTypeFile      = filepath.Join(buildPruningBaseDir, "networkpolicy/egress_49696.yaml")
+			ingressTypeFile     = filepath.Join(buildPruningBaseDir, "networkpolicy/ingress_49696.yaml")
+		)
+		g.By("Create 1st namespace for the scenario")
+		oc.CreateSpecifiedNamespaceAsAdmin(e2eTestNamespace1)
+		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace1)
+
+		g.By("create test pods")
+		createResourceFromFile(oc, e2eTestNamespace1, testPodFile)
+		createResourceFromFile(oc, e2eTestNamespace1, helloSdnFile)
+		exutil.AssertWaitPollNoErr(waitForPodWithLabelReady(oc, e2eTestNamespace1, "name=test-pods"), fmt.Sprintf("this pod with label name=test-pods in ns/%s not ready", e2eTestNamespace1))
+		exutil.AssertWaitPollNoErr(waitForPodWithLabelReady(oc, e2eTestNamespace1, "name=hellosdn"), fmt.Sprintf("this pod with label name=hellosdn in ns/%s not ready", e2eTestNamespace1))
+		hellosdnPodNameNs1 := getPodName(oc, e2eTestNamespace1, "name=hellosdn")
+
+		g.By("create egress type networkpolicy in ns1")
+		createResourceFromFile(oc, e2eTestNamespace1, egressTypeFile)
+
+		g.By("create ingress type networkpolicy in ns1")
+		createResourceFromFile(oc, e2eTestNamespace1, ingressTypeFile)
+
+		g.By("#. Create 2nd namespace for the scenario")
+		oc.CreateSpecifiedNamespaceAsAdmin(e2eTestNamespace2)
+		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace2)
+
+		g.By("create test pods in second namespace")
+		createResourceFromFile(oc, e2eTestNamespace2, helloSdnFile)
+		exutil.AssertWaitPollNoErr(waitForPodWithLabelReady(oc, e2eTestNamespace2, "name=hellosdn"), fmt.Sprintf("this pod with label name=hellosdn in ns/%s not ready", e2eTestNamespace2))
+
+		g.By("Get IP of the test pods in second namespace.")
+		hellosdnPodNameNs2 := getPodName(oc, e2eTestNamespace2, "name=hellosdn")
+		hellosdnPodIP1Ns2 := getPodIPv4(oc, e2eTestNamespace2, hellosdnPodNameNs2[0])
+
+		g.By("curl from ns1 hellosdn pod to ns2 pod")
+		_, err := e2e.RunHostCmd(e2eTestNamespace1, hellosdnPodNameNs1[0], "curl --connect-timeout 5  -s "+net.JoinHostPort(hellosdnPodIP1Ns2, "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(err.Error()).Should(o.ContainSubstring("exit status 28"))
+
+	})
+
+	// author: anusaxen@redhat.com
+	g.It("MicroShiftOnly-Author:anusaxen-High-60332-Network Policies should work with OVNKubernetes when traffic hairpins back to the same source through a service", func() {
+		var (
+			caseID           = "60332"
+			baseDir          = exutil.FixturePath("testdata", "networking")
+			e2eTestNamespace = "e2e-ushift-sdn-" + caseID + "-" + getRandomString()
+			allowfromsameNS  = filepath.Join(baseDir, "networkpolicy/allow-from-same-namespace.yaml")
+		)
+
+		g.By("Create a namespace for the scenario")
+		oc.CreateSpecifiedNamespaceAsAdmin(e2eTestNamespace)
+		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace)
+
+		pod_pmtrs := map[string]string{
+			"$podname":   "hello-pod1",
+			"$namespace": e2eTestNamespace,
+			"$label":     "hello-pod",
+		}
+
+		g.By("create 1st hello pod in namespace")
+		createPingPodforUshift(oc, pod_pmtrs)
+		waitPodReady(oc, e2eTestNamespace, "hello-pod1")
+
+		pod_pmtrs = map[string]string{
+			"$podname":   "hello-pod2",
+			"$namespace": e2eTestNamespace,
+			"$label":     "hello-pod",
+		}
+		g.By("create 2nd hello pod in same namespace")
+		createPingPodforUshift(oc, pod_pmtrs)
+		waitPodReady(oc, e2eTestNamespace, "hello-pod2")
+
+		//ipFamilyPolicy and externalTrafficPolicy are left blank which would get default values
+		svc_pmtrs := map[string]string{
+			"$servicename":           "test-service",
+			"$namespace":             e2eTestNamespace,
+			"$label":                 "test-service",
+			"$internalTrafficPolicy": "Cluster",
+			"$externalTrafficPolicy": "",
+			"$ipFamilyPolicy":        "",
+			"$selector":              "hello-pod",
+			"$serviceType":           "ClusterIP",
+		}
+		createServiceforUshift(oc, svc_pmtrs)
+
+		g.By("create allow-from-same-namespace ingress networkpolicy in ns")
+		createResourceFromFile(oc, e2eTestNamespace, allowfromsameNS)
+
+		g.By("Get Pod IPs")
+		helloPod1IP := getPodIPv4(oc, e2eTestNamespace, "hello-pod1")
+		helloPod2IP := getPodIPv4(oc, e2eTestNamespace, "hello-pod2")
+
+		g.By("Get svc IP")
+		svcIP := getSvcIPv4(oc, e2eTestNamespace, "test-service")
+
+		g.By("curl hello-pod1 to hello-pod2")
+		output, err := e2e.RunHostCmd(e2eTestNamespace, "hello-pod1", "curl --connect-timeout 5 -s "+net.JoinHostPort(helloPod1IP, "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+
+		g.By("curl hello-pod2 to hello-pod1")
+		output, err = e2e.RunHostCmd(e2eTestNamespace, "hello-pod2", "curl --connect-timeout 5 -s "+net.JoinHostPort(helloPod2IP, "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+
+		for i := 0; i < 5; i++ {
+
+			g.By("curl hello-pod1 to service:port")
+			output, err = e2e.RunHostCmd(e2eTestNamespace, "hello-pod1", "curl --connect-timeout 5 -s "+net.JoinHostPort(svcIP, "27017"))
+			o.Expect(err).NotTo(o.HaveOccurred())
+			o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+
+			g.By("curl hello-pod2 to service:port")
+			output, err = e2e.RunHostCmd(e2eTestNamespace, "hello-pod2", "curl --connect-timeout 5 -s "+net.JoinHostPort(svcIP, "27017"))
+			o.Expect(err).NotTo(o.HaveOccurred())
+			o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+		}
+	})
+
+	// author: anusaxen@redhat.com
+	g.It("MicroShiftOnly-Author:anusaxen-High-60426-podSelector allow-to and allow-from can work together", func() {
+		var (
+			caseID            = "60426"
+			baseDir           = exutil.FixturePath("testdata", "networking")
+			e2eTestNamespace1 = "e2e-ushift-sdn-" + caseID + "-" + getRandomString()
+			e2eTestNamespace2 = "e2e-ushift-sdn-" + caseID + "-" + getRandomString()
+			ingressTypeFile   = filepath.Join(baseDir, "networkpolicy/default-deny-ingress.yaml")
+			allowfromRed      = filepath.Join(baseDir, "microshift/np-allow-from-red.yaml")
+			allowtoBlue       = filepath.Join(baseDir, "microshift/np-allow-to-blue.yaml")
+		)
+
+		g.By("Create a namespace for the scenario")
+		oc.CreateSpecifiedNamespaceAsAdmin(e2eTestNamespace1)
+		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace1)
+
+		g.By("create 4 test pods in e2enamespace1")
+		for i := 0; i < 4; i++ {
+			pod_pmtrs := map[string]string{
+				"$podname":   "test-pod" + strconv.Itoa(i),
+				"$namespace": e2eTestNamespace1,
+				"$label":     "test-pod" + strconv.Itoa(i),
+			}
+			createPingPodforUshift(oc, pod_pmtrs)
+			waitPodReady(oc, e2eTestNamespace1, "test-pod"+strconv.Itoa(i))
+		}
+
+		var testPodNS1 [4]string
+		var testPodIPNS1 [4]string
+		g.By("Get IP of the test pods in e2eTestNamespace1 namespace.")
+		for i := 0; i < 4; i++ {
+			testPodNS1[i] = strings.Join(getPodName(oc, e2eTestNamespace1, "name=test-pod"+strconv.Itoa(i)), "")
+			testPodIPNS1[i] = getPodIPv4(oc, e2eTestNamespace1, testPodNS1[i])
+		}
+
+		// label pod0 and pod1 with type=red and type=blue respectively in e2eTestNamespace1
+		err := exutil.LabelPod(oc, e2eTestNamespace1, testPodNS1[0], "type=red")
+		o.Expect(err).NotTo(o.HaveOccurred())
+		err = exutil.LabelPod(oc, e2eTestNamespace1, testPodNS1[1], "type=blue")
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("Create 2nd namespace for the scenario")
+		oc.CreateSpecifiedNamespaceAsAdmin(e2eTestNamespace2)
+		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace2)
+
+		g.By("create 2 test pods in e2enamespace1")
+		for i := 0; i < 2; i++ {
+			pod_pmtrs := map[string]string{
+				"$podname":   "test-pod" + strconv.Itoa(i),
+				"$namespace": e2eTestNamespace2,
+				"$label":     "test-pod" + strconv.Itoa(i),
+			}
+			createPingPodforUshift(oc, pod_pmtrs)
+			waitPodReady(oc, e2eTestNamespace2, "test-pod"+strconv.Itoa(i))
+		}
+		var testPodNS2 [2]string
+		var testPodIPNS2 [2]string
+		g.By("Get IP of the test pods in e2eTestNamespace2 namespace.")
+		for i := 0; i < 2; i++ {
+			testPodNS2[i] = strings.Join(getPodName(oc, e2eTestNamespace2, "name=test-pod"+strconv.Itoa(i)), "")
+			testPodIPNS2[i] = getPodIPv4(oc, e2eTestNamespace2, testPodNS2[i])
+		}
+
+		// label pod0 with type=red in e2eTestNamespace2
+		err = exutil.LabelPod(oc, e2eTestNamespace2, testPodNS2[0], "type=red")
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		g.By("create default deny ingress type networkpolicy in 1st namespace")
+		createResourceFromFile(oc, e2eTestNamespace1, ingressTypeFile)
+
+		g.By("create allow-from-red and allow-from-blue type networkpolicy in 1st namespace")
+		createResourceFromFile(oc, e2eTestNamespace1, allowfromRed)
+		createResourceFromFile(oc, e2eTestNamespace1, allowtoBlue)
+
+		g.By("Try to access the pod in e2eTestNamespace1 from each pod")
+		g.By("curl testPodNS10 to testPodNS13")
+		output, err := e2e.RunHostCmd(e2eTestNamespace1, testPodNS1[0], "curl --connect-timeout 5 -s "+net.JoinHostPort(testPodIPNS1[3], "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+
+		g.By("curl testPodNS12 to testPodNS11")
+		output, err = e2e.RunHostCmd(e2eTestNamespace1, testPodNS1[2], "curl --connect-timeout 5 -s "+net.JoinHostPort(testPodIPNS1[1], "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+
+		g.By("curl testPodNS12 to testPodNS13")
+		output, err = e2e.RunHostCmd(e2eTestNamespace1, testPodNS1[2], "curl --connect-timeout 5 -s "+net.JoinHostPort(testPodIPNS1[3], "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(output).ShouldNot(o.ContainSubstring("Hello OpenShift"))
+
+		g.By("Try to access the pod from e2eTestNamespace2 now")
+		g.By("curl testPodNS20 to testPodNS13")
+		output, err = e2e.RunHostCmd(e2eTestNamespace2, testPodNS2[1], "curl --connect-timeout 5 -s "+net.JoinHostPort(testPodIPNS1[3], "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(output).ShouldNot(o.ContainSubstring("Hello OpenShift"))
+
+		g.By("curl testPodNS21 to testPodNS11")
+		output, err = e2e.RunHostCmd(e2eTestNamespace2, testPodNS2[1], "curl --connect-timeout 5 -s "+net.JoinHostPort(testPodIPNS1[1], "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(output).Should(o.ContainSubstring("Hello OpenShift"))
+
+		g.By("curl testPodNS21 to testPodNS13")
+		output, err = e2e.RunHostCmd(e2eTestNamespace2, testPodNS2[1], "curl --connect-timeout 5 -s "+net.JoinHostPort(testPodIPNS1[3], "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+		o.Expect(output).ShouldNot(o.ContainSubstring("Hello OpenShift"))
+
+	})
+})
---
+++ test/extended/networking/microshift_utils.go
@@ -0,0 +1,57 @@
+package networking
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+	e2e "k8s.io/kubernetes/test/e2e/framework"
+)
+
+// get file contents to be modified for Ushift
+func getFileContentforUshift(baseDir string, name string) (fileContent string) {
+	filePath := filepath.Join(exutil.FixturePath("testdata", "networking", baseDir), name)
+	fileOpen, err := os.Open(filePath)
+	defer fileOpen.Close()
+	if err != nil {
+		e2e.Failf("Failed to open file: %s", filePath)
+	}
+	fileRead, _ := io.ReadAll(fileOpen)
+	if err != nil {
+		e2e.Failf("Failed to read file: %s", filePath)
+	}
+	return string(fileRead)
+}
+
+// get service yaml file, replace variables as per requirements in ushift and create service post that
+func createServiceforUshift(oc *exutil.CLI, svc_pmtrs map[string]string) (err error) {
+	e2e.Logf("Getting filecontent")
+	ServiceGenericYaml := getFileContentforUshift("microshift", "service-generic.yaml")
+	//replace all variables as per createServiceforUshift() arguements
+	for rep, value := range svc_pmtrs {
+		ServiceGenericYaml = strings.ReplaceAll(ServiceGenericYaml, rep, value)
+	}
+	svcFileName := "temp-service-" + getRandomString() + ".yaml"
+	defer os.Remove(svcFileName)
+	os.WriteFile(svcFileName, []byte(ServiceGenericYaml), 0644)
+	// create service for Microshift
+	_, err = oc.WithoutNamespace().Run("create").Args("-f", svcFileName).Output()
+	return err
+}
+
+// get generic pod yaml file, replace varibles as per requirements in ushift and create service post that
+func createPingPodforUshift(oc *exutil.CLI, pod_pmtrs map[string]string) (err error) {
+	PodGenericYaml := getFileContentforUshift("microshift", "ping-for-pod-generic.yaml")
+	//replace all variables as per createPodforUshift() arguements
+	for rep, value := range pod_pmtrs {
+		PodGenericYaml = strings.ReplaceAll(PodGenericYaml, rep, value)
+	}
+	podFileName := "temp-ping-pod-" + getRandomString() + ".yaml"
+	defer os.Remove(podFileName)
+	os.WriteFile(podFileName, []byte(PodGenericYaml), 0644)
+	// create ping pod for Microshift
+	_, err = oc.WithoutNamespace().Run("create").Args("-f", podFileName).Output()
+	return err
+}
