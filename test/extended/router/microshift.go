6e80eaa66f49599b73dc894cdae31316d02e4059
Melvin Joseph <mjoseph@redhat.com>
2023-03-08 08:07:14 +0530

    Automation for ocp60149 and modification of ocp60136 to use TLS certicate (#7962)
    * Automation for ocp60149 and modification of ocp60136 to use TLS certificate
    
    * addressed comment on bindata
    
    * Addressed review comments
    
    * address -k option of curl command

--- test/extended/router/microshift.go
+++ test/extended/router/microshift.go
@@ -15,12 +15,13 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 	var oc = exutil.NewCLIWithoutNamespace("router-microshift")
 
-	g.It("MicroShiftOnly-Author:mjoseph-High-60136-route using Ingress resource for Microshift using Opaque certificate for destination CA", func() {
-		e2eTestNamespace := "e2e-ne-ocp60136-" + getRandomString()
-		buildPruningBaseDir := exutil.FixturePath("testdata", "router")
-		testPodSvc := filepath.Join(buildPruningBaseDir, "web-server-rc.yaml")
-		ingressFile := filepath.Join(buildPruningBaseDir, "microshift-ingress-destca.yaml")
-		caCert := filepath.Join(buildPruningBaseDir, "ca-bundle.pem")
+	g.It("MicroShiftOnly-Author:mjoseph-High-60136-reencrypt route using Ingress resource for Microshift with destination CA certificate", func() {
+		var (
+			e2eTestNamespace    = "e2e-ne-ocp60136-" + getRandomString()
+			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
+			testPodSvc          = filepath.Join(buildPruningBaseDir, "web-server-signed-rc.yaml")
+			ingressFile         = filepath.Join(buildPruningBaseDir, "microshift-ingress-destca.yaml")
+		)
 
 		g.By("create a namespace for the scenario")
 		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace)
@@ -34,9 +35,6 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		podName := getPodName(oc, e2eTestNamespace, "name=web-server-rc")
 		ingressPod := getRouterPod(oc, "default")
 
-		g.By("create a secret with destination CA Opaque certificate")
-		createGenericSecret(oc, e2eTestNamespace, "service-secret", "tls.crt", caCert)
-
 		g.By("create ingress using the file and get the route details")
 		defer operateResourceFromFile(oc, "delete", e2eTestNamespace, ingressFile)
 		createResourceFromFile(oc, e2eTestNamespace, ingressFile)
@@ -45,21 +43,20 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		routeNames := getResourceName(oc, e2eTestNamespace, "route")
 
 		g.By("check whether route details are present")
-		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".metadata.annotations", `"route.openshift.io/destination-ca-certificate-secret":"service-secret"`)
-		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".spec.host", "service-secure-test.example.com")
+		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".status.ingress[0].conditions[0].type", "Admitted")
+		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".status.ingress[0].host", "service-secure1-test.example.com")
 		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".spec.tls.termination", "reencrypt")
 
 		g.By("check the reachability of the host in test pod")
 		routerPodIP := getPodv4Address(oc, ingressPod, "openshift-ingress")
-		curlCmd := fmt.Sprintf("curl --resolve  service-secure-test.example.com:443:%s https://service-secure-test.example.com -I -k", routerPodIP)
+		curlCmd := fmt.Sprintf("curl --resolve  service-secure1-test.example.com:443:%s https://service-secure1-test.example.com -I -k", routerPodIP)
 		statsOut, err := exutil.RemoteShPod(oc, e2eTestNamespace, podName[0], "sh", "-c", curlCmd)
 		o.Expect(err).NotTo(o.HaveOccurred())
 		o.Expect(statsOut).Should(o.ContainSubstring("HTTP/1.1 200 OK"))
 
 		g.By("check the router pod and ensure the routes are loaded in haproxy.config")
-		searchOutput := readRouterPodData(oc, ingressPod, "cat haproxy.config", "ingress-reen")
+		searchOutput := readRouterPodData(oc, ingressPod, "cat haproxy.config", "ingress-ms-reen")
 		o.Expect(searchOutput).To(o.ContainSubstring("backend be_secure:" + e2eTestNamespace + ":" + routeNames[0]))
-		o.Expect(searchOutput).To(o.ContainSubstring("/var/lib/haproxy/router/cacerts/" + e2eTestNamespace + ":" + routeNames[0] + ".pem"))
 	})
 
 	g.It("MicroShiftOnly-Author:mjoseph-Critical-60266-creation of edge and passthrough routes for Microshift", func() {
@@ -156,7 +153,7 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 		g.By("check the reachability of the host in test pod for http route")
 		routerPodIP := getPodv4Address(oc, ingressPod, "openshift-ingress")
-		curlCmd := fmt.Sprintf("curl --resolve  %s:80:%s http://%s -I -k", httpRoute, routerPodIP, httpRoute)
+		curlCmd := fmt.Sprintf("curl --resolve  %s:80:%s http://%s -I", httpRoute, routerPodIP, httpRoute)
 		statsOut, err := exutil.RemoteShPod(oc, e2eTestNamespace, podName[0], "sh", "-c", curlCmd)
 		o.Expect(err).NotTo(o.HaveOccurred())
 		o.Expect(statsOut).Should(o.ContainSubstring("HTTP/1.1 200 OK"))
@@ -183,5 +180,50 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		g.By("check the router pod and ensure the reen route is loaded in haproxy.config")
 		searchOutput1 := readRouterPodData(oc, ingressPod, "cat haproxy.config", "ms-reen")
 		o.Expect(searchOutput1).To(o.ContainSubstring("backend be_secure:" + e2eTestNamespace + ":ms-reen"))
+	})
+
+	g.It("MicroShiftOnly-Author:mjoseph-Critical-60149-http route using Ingress resource for Microshift", func() {
+		var (
+			e2eTestNamespace    = "e2e-ne-ocp60149-" + getRandomString()
+			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
+			testPodSvc          = filepath.Join(buildPruningBaseDir, "web-server-rc.yaml")
+			ingressFile         = filepath.Join(buildPruningBaseDir, "microshift-ingress-http.yaml")
+			httpRoute           = "service-unsecure-test.example.com"
+		)
+
+		g.By("create a namespace for the scenario")
+		defer oc.DeleteSpecifiedNamespaceAsAdmin(e2eTestNamespace)
+		oc.CreateSpecifiedNamespaceAsAdmin(e2eTestNamespace)
+
+		g.By("create a web-server-rc pod and its services")
+		defer operateResourceFromFile(oc, "delete", e2eTestNamespace, testPodSvc)
+		createResourceFromFile(oc, e2eTestNamespace, testPodSvc)
+		err := waitForPodWithLabelReady(oc, e2eTestNamespace, "name=web-server-rc")
+		exutil.AssertWaitPollNoErr(err, "the pod with name=web-server-rc, Ready status not met")
+		podName := getPodName(oc, e2eTestNamespace, "name=web-server-rc")
+		ingressPod := getRouterPod(oc, "default")
+
+		g.By("create ingress using the file and get the route details")
+		defer operateResourceFromFile(oc, "delete", e2eTestNamespace, ingressFile)
+		createResourceFromFile(oc, e2eTestNamespace, ingressFile)
+		getIngress(oc, e2eTestNamespace)
+		getRoutes(oc, e2eTestNamespace)
+		routeNames := getResourceName(oc, e2eTestNamespace, "route")
+
+		g.By("check whether http route details are present")
+		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".spec.port.targetPort", "http")
+		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".status.ingress[0].host", httpRoute)
+		waitForOutput(oc, e2eTestNamespace, "route/"+routeNames[0], ".status.ingress[0].conditions[0].type", "Admitted")
+
+		g.By("check the reachability of the host in test pod for http route")
+		routerPodIP := getPodv4Address(oc, ingressPod, "openshift-ingress")
+		curlCmd := fmt.Sprintf("curl --resolve  %s:80:%s http://%s -I", httpRoute, routerPodIP, httpRoute)
+		statsOut, err := exutil.RemoteShPod(oc, e2eTestNamespace, podName[0], "sh", "-c", curlCmd)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(statsOut).Should(o.ContainSubstring("HTTP/1.1 200 OK"))
+
+		g.By("check the router pod and ensure the http route is loaded in haproxy.config")
+		searchOutput := readRouterPodData(oc, ingressPod, "cat haproxy.config", "ingress-on-microshift")
+		o.Expect(searchOutput).To(o.ContainSubstring("backend be_http:" + e2eTestNamespace + ":" + routeNames[0]))
 	})
 })
