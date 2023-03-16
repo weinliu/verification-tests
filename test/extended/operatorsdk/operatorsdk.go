59ab4960f61bc138a12e75e0fd91f6dea0423518
Keenon Lee <jitli@redhat.com>
2023-03-16 15:25:55 +0800

    Update K8s version (#8162)

--- test/extended/operatorsdk/operatorsdk.go
+++ test/extended/operatorsdk/operatorsdk.go
@@ -847,10 +847,10 @@ var _ = g.Describe("[sig-operators] Operator_SDK should", func() {
 	})
 
 	// author: chuo@redhat.com
-	g.It("Author:xzha-High-52126-Sync 1.25 to downstream", func() {
+	g.It("Author:xzha-High-52126-Sync 1.26 to downstream", func() {
 		operatorsdkCLI.showInfo = true
 		output, _ := operatorsdkCLI.Run("version").Args().Output()
-		o.Expect(output).To(o.ContainSubstring("v1.25"))
+		o.Expect(output).To(o.ContainSubstring("v1.26"))
 	})
 
 	// author: chuo@redhat.com
