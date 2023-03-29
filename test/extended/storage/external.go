dee9ba85ca9d03827a8ef214edd22a7235b4459b
chaoyang <chaoyang@redhat.com>
2023-03-29 11:50:15 +0800

    Auto OCP-61553 (#8394)
    * Auto OCP-61553
    
    * Update based on comments

---
+++ test/extended/storage/external.go
@@ -0,0 +1,46 @@
+package storage
+
+import (
+	"encoding/json"
+	"strings"
+
+	g "github.com/onsi/ginkgo/v2"
+	o "github.com/onsi/gomega"
+	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+)
+
+var _ = g.Describe("[sig-storage] STORAGE", func() {
+	defer g.GinkgoRecover()
+	var (
+		oc = exutil.NewCLI("storage-external", exutil.KubeConfigPath())
+	)
+	g.BeforeEach(func() {
+		checkOptionalCapability(oc, "Storage")
+		cloudProvider = getCloudProvider(oc)
+
+		if !strings.Contains(cloudProvider, "external") {
+			g.Skip("Skip for non-supported cloud provider: *" + cloudProvider + "* !!!")
+		}
+	})
+
+	// author: chaoyang@redhat.com
+	// https://issues.redhat.com/browse/STOR-1074
+	g.It("NonHyperShiftHOST-Author:chaoyang-High-61553-[EXTERNAL][CSO] should be healthy on external platform", func() {
+
+		g.By("Check CSO is healthy")
+		checkCSOhealthy(oc)
+
+		g.By("Check clusteroperator storage type==Available message is as expected")
+		conditions, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("co", "storage", `-o=jsonpath={.status.conditions[?(@.type=="Available")]}`).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		var conditionJSONMap map[string]interface{}
+		err = json.Unmarshal([]byte(conditions), &conditionJSONMap)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(conditionJSONMap["message"]).To(o.Equal("DefaultStorageClassControllerAvailable: No default StorageClass for this platform"))
+
+		g.By("Check pods cluster-storage-operator is running")
+		checkPodStatusByLabel(oc.AsAdmin(), "openshift-cluster-storage-operator", "name=cluster-storage-operator", "Running")
+
+	})
+
+})
