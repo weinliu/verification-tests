dbc615226cbd4991eb2233b0b36319e927f4a3dc
Yanping Zhang <yanpzhan@redhat.com>
2023-04-06 14:43:48 +0800

    auto ocp-60809 (#8450)

---
+++ frontend/tests/masthead/masthead.cy.ts
@@ -0,0 +1,19 @@
+import { guidedTour } from "upstream/views/guided-tour";
+import { masthead } from '../../views/masthead';
+describe('masthead related', () => {
+  before(() => {
+    cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+    cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
+    guidedTour.close();
+  });
+
+  after(() => {
+    cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+  });
+
+  it('(OCP-60809,yanpzhan) Add customer feedback to console', {tags: ['e2e', 'admin']}, () => {
+    masthead.openHelpItem('Share Feedback');
+    masthead.checkFeedbackModal();
+    masthead.cancelFeedback();
+  });
+})
--- frontend/views/masthead.ts
+++ frontend/views/masthead.ts
@@ -15,5 +15,18 @@ export const masthead = {
   },
   clearFilters: () => {
     cy.byButtonText('Clear filter').click();
+  },
+  openHelpItem: (itemName: string) => {
+    cy.get('button[aria-label="Help menu"]').click();
+    cy.contains(`${itemName}`).click();
+  },
+  checkFeedbackModal: () => {
+    cy.contains('Share feedback').should('exist');
+    cy.contains(/Report a bug|Open a support case/).should('exist');
+    cy.contains('Inform the direction of Red Hat').should('exist');
+  },
+  cancelFeedback: () => {
+    cy.contains('Cancel').click();
+    cy.get('[aria-label="Feedback modal"]').should('not.exist');
   }
 }
