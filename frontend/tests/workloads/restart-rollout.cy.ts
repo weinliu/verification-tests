930a3bd93c41cd32363094de567dde734ac4f3ef
Xiyun Zhao <xiyuzhao@redhat.com>
2023-04-11 16:08:17 +0800

    Update for OCP-52579 (#8706)

--- frontend/tests/workloads/restart-rollout.cy.ts
+++ frontend/tests/workloads/restart-rollout.cy.ts
@@ -3,8 +3,8 @@ import { listPage } from "upstream/views/list-page"
 
 describe('Check rollout restart and retry in Deployment/DC', () => {
   const params = {
-    namespace: 'auto-52579',
-    deploymentName: 'hello-openshift',
+    namespace: 'ocp52579-project',
+    deploymentName: 'ocp50657-example-deployment',
     dcName: 'hooks',
     deploymentFile: 'deployments.yaml',
     dcFileName: 'deploymentconfig.yaml',
@@ -13,7 +13,7 @@ describe('Check rollout restart and retry in Deployment/DC', () => {
   before(() => {
     cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
     guidedTour.close();
-    cy.switchPerspective('Administrator');
+    cy.switchPerspective('Administrator');   
     cy.createProject(params.namespace)
     cy.byTestID('resource-status').contains('Active')
     cy.adminCLI(`oc apply -f ./fixtures/${params.deploymentFile} -n ${params.namespace}`)
@@ -21,62 +21,50 @@ describe('Check rollout restart and retry in Deployment/DC', () => {
   })
 
   after(() => {
-    cy.adminCLI(`oc delete project ${params.namespace}`)
-    cy.logout()
+     cy.adminCLI(`oc delete project ${params.namespace}`)
   })
 
-  it('(OCP-52579, xiyuzhao) Add Rollout Restart and Retry function to Deployment/Deployment Config', {tags: ['e2e']}, () => {
-      // Check point 1: Kebab and action list button click for deployment
-      cy.visit(`/k8s/ns/${params.namespace}/apps~v1~Deployment`)
+  it('(OCP-52579, xiyuzhao) Add Rollout Restart and Retry function to Deployment/Deployment Config', {tags: ['e2e','@rosa']}, () => {  
+    const checkRolloutState = (judge: string) => {
       cy.adminCLI(`oc get deployment/${params.deploymentName} -n ${params.namespace} -o yaml`)
         .its('stdout')
-        .should('not.contain', 'restartedAt')
+        .should(judge, 'restartedAt');
       cy.adminCLI(`oc rollout history deployment/${params.deploymentName} -n ${params.namespace}`)
         .its('stdout')
-        .should('not.contain', '2') // There should be only one revision
-      listPage.rows.clickKebabAction(params.deploymentName, 'Restart rollout')
-      cy.visit(`/k8s/ns/${params.namespace}/deployments/${params.deploymentName}`)
-      cy.adminCLI(`oc get deployment/${params.deploymentName} -n ${params.namespace} -o yaml`)
-        .its('stdout')
-        .should('contain', 'restartedAt')
-      cy.adminCLI(`oc rollout history deployment/${params.deploymentName} -n ${params.namespace}`)
-        .its('stdout')
-        .should('contain', '2')
-      cy.adminCLI(`oc rollout pause deployment/${params.deploymentName} -n ${params.namespace}`)
-        .its('stdout')
-        .should('contain', 'paused')
-      cy.byLegacyTestID('actions-menu-button').click()
-      cy.byLegacyTestID('action-items')
-        .within(($div) => {
-          cy.contains('button', 'Restart rollout')
-            .should('be.disabled')
-        })
-      
-      // Check point 2: Everything goes the same for the deploymentconfig except for action name. 
-      // 2.1 Check that retry button is disabled at first and the tool tip hovers accordingly. 
-      cy.visit(`/k8s/ns/${params.namespace}/apps.openshift.io~v1~DeploymentConfig`)
-      cy.get(`[data-test-rows="resource-row"]`)
-        .contains(params.dcName)
-        .parents('tr')
-        .within(() => {
-          cy.get('[data-test-id="kebab-button"]').click()
-        })
-      cy.byLegacyTestID('action-items').within(($div) => {
-        cy.get('.pf-m-disabled').should('contain', 'Retry rollout')
-      })
-      // 2.2 Go to detail page
-      cy.visit(`/k8s/ns/${params.namespace}/deploymentconfigs/${params.dcName}/replicationcontrollers`)
-      cy.byTestID('status-text').contains('Complete')
-      cy.adminCLI(`oc rollout latest dc/${params.dcName} -n ${params.namespace}`)
-      cy.adminCLI(`oc rollout cancel dc/${params.dcName} -n ${params.namespace}`)
-      cy.visit(`/k8s/ns/${params.namespace}/deploymentconfigs/${params.dcName}/replicationcontrollers`) // refresh the page
-      cy.byTestID('status-text').contains('Failed')
-      cy.byLegacyTestID('actions-menu-button').click()
-      cy.get('[data-test-action="Retry rollout"]').click()
-      cy.byTestID('status-text').contains('Failed').should('not.exist')
-      // start to check if the rollout was successful
-      cy.adminCLI(`oc rollout status dc/${params.dcName} -n ${params.namespace}`)
-        .its('stdout')
-        .should('contain', 'successfully')
+        .should(judge, '2');
+    }
+    // Check 'Restart rollout' action in Deployment page
+    cy.visit(`/k8s/ns/${params.namespace}/apps~v1~Deployment`);
+    checkRolloutState('not.contain');
+    listPage.rows.clickKebabAction(params.deploymentName, 'Restart rollout');
+    listPage.rows.clickRowByName(params.deploymentName);
+    checkRolloutState('contain');
+    cy.adminCLI(`oc rollout pause deployment/${params.deploymentName} -n ${params.namespace}`)
+      .its('stdout')
+      .should('contain', 'paused');
+    cy.byLegacyTestID('actions-menu-button').click();
+    cy.get('[data-test-action="Restart rollout"] button').should('be.disabled');
+     
+    // Check 'Retry rollout' in Deployment Configs page
+    cy.visit(`/k8s/ns/${params.namespace}/apps.openshift.io~v1~DeploymentConfig`);
+    listPage.filter.byName(params.dcName);
+    cy.byLegacyTestID('kebab-button').click();
+    cy.get('[data-test-action="Retry rollout"] button').should('be.disabled');
+
+    /* Check 'Retry rollout' function on DeploymentConfig details - ReplicationControllers page
+      'Retry rollout' is enabled when the latest revision of the ReplicationControllers is in Failed state */
+    cy.visit(`/k8s/ns/${params.namespace}/deploymentconfigs/${params.dcName}/replicationcontrollers`);
+    cy.byTestID('status-text').contains('Complete');
+    cy.adminCLI(`oc rollout latest dc/${params.dcName} -n ${params.namespace}`);
+    cy.adminCLI(`oc rollout cancel dc/${params.dcName} -n ${params.namespace}`);
+    cy.wait(50000);
+    cy.byTestID('status-text').contains('Failed');
+    cy.reload(true);
+    cy.byLegacyTestID('actions-menu-button').click();
+    cy.byTestActionID('Retry rollout').click();
+    cy.wait(15000); 
+    cy.adminCLI(`oc rollout status dc/${params.dcName} -n ${params.namespace}`)
+      .its('stdout')
+      .should('contain', 'successfully');
     })
 })
