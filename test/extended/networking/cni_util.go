c30e6638348939ab8cbbd4c1e89e46a3e61776cf
Weibin Liang <weliang@redhat.com>
2023-04-28 01:08:44 -0400

    Automate-60511-60512-60516 (#8956)
    * Automate-60511-60512-60516
    
    * Create common functions to be used by several cases
    
    * Add defer step to delete net-attach-def

--- test/extended/networking/cni_util.go
+++ test/extended/networking/cni_util.go
@@ -3,13 +3,16 @@ package networking
 import (
 	"fmt"
 	"net"
+	"path/filepath"
 	"strings"
 	"time"
 
+	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
+	e2enode "k8s.io/kubernetes/test/e2e/framework/node"
 	e2eoutput "k8s.io/kubernetes/test/e2e/framework/pod/output"
 )
 
@@ -104,4 +107,216 @@ func getPodMultiNetworks(oc *exutil.CLI, namespace string, podName string, netNa
 	o.Expect(err1).NotTo(o.HaveOccurred())
 	podIPv6 := strings.TrimSpace(podv6Output)
 	return podIPv4, podIPv6
+}
+
+func multihomingBeforeCheck(oc *exutil.CLI) ([]string, []string, []string, []string, string, string, string) {
+	var (
+		buildPruningBaseDir    = exutil.FixturePath("testdata", "networking/multihoming")
+		multihomingNADTemplate = filepath.Join(buildPruningBaseDir, "multihoming-NAD-template.yaml")
+		multihomingPodTemplate = filepath.Join(buildPruningBaseDir, "multihoming-pod-template.yaml")
+	)
+
+	g.By("Get the ready-schedulable worker nodes")
+	nodeList, nodeErr := e2enode.GetReadySchedulableNodes(oc.KubeFramework().ClientSet)
+	o.Expect(nodeErr).NotTo(o.HaveOccurred())
+	if len(nodeList.Items) < 2 {
+		g.Skip("This case requires 2 nodes, but the cluster has less than two nodes")
+	}
+
+	g.By("Create a test namespace")
+	ns1 := oc.Namespace()
+
+	nadName := "layer2dualstacknetwork"
+	nsWithnad := ns1 + "/" + nadName
+
+	g.By("Create a custom resource network-attach-defintion in tested namespace")
+	nad1ns1 := multihomingNAD{
+		namespace:      ns1,
+		nadname:        nadName,
+		subnets:        "192.168.100.0/24,fd00:dead:beef::0/64",
+		nswithnadname:  nsWithnad,
+		excludeSubnets: "",
+		topology:       "layer2",
+		template:       multihomingNADTemplate,
+	}
+	nad1ns1.createMultihomingNAD(oc)
+
+	g.By("Check if the network-attach-defintion is created")
+	if checkNAD(oc, ns1, nadName) {
+		e2e.Logf("The correct network-attach-defintion: %v is created!", nadName)
+	} else {
+		e2e.Failf("The correct network-attach-defintion: %v is not created!", nadName)
+	}
+
+	g.By("Check if the new OVN switch is created")
+	ovnMasterPodName := getOVNLeaderPod(oc, "north")
+	o.Expect(ovnMasterPodName).ShouldNot(o.Equal(""))
+	if checkOVNSwitch(oc, nadName, ovnMasterPodName) {
+		e2e.Logf("The correct OVN switch is created")
+	} else {
+		e2e.Failf("The correct OVN switch is not created")
+	}
+
+	g.By("Create 1st pod consuming above network-attach-defintion in ns1")
+	pod1 := testMultihomingPod{
+		name:       "multihoming-pod-1",
+		namespace:  ns1,
+		podlabel:   "multihoming-pod1",
+		nadname:    nadName,
+		nodename:   nodeList.Items[0].Name,
+		podenvname: "Hello multihoming-pod-1",
+		template:   multihomingPodTemplate,
+	}
+	pod1.createTestMultihomingPod(oc)
+	o.Expect(waitForPodWithLabelReady(oc, ns1, "name=multihoming-pod1")).NotTo(o.HaveOccurred())
+
+	g.By("Create 2nd pod consuming above network-attach-defintion in ns1")
+	pod2 := testMultihomingPod{
+		name:       "multihoming-pod-2",
+		namespace:  ns1,
+		podlabel:   "multihoming-pod2",
+		nadname:    nadName,
+		nodename:   nodeList.Items[0].Name,
+		podenvname: "Hello multihoming-pod-2",
+		template:   multihomingPodTemplate,
+	}
+	pod2.createTestMultihomingPod(oc)
+	o.Expect(waitForPodWithLabelReady(oc, ns1, "name=multihoming-pod2")).NotTo(o.HaveOccurred())
+
+	g.By("Create 3rd pod consuming above network-attach-defintion in ns1")
+	pod3 := testMultihomingPod{
+		name:       "multihoming-pod-3",
+		namespace:  ns1,
+		podlabel:   "multihoming-pod3",
+		nadname:    nadName,
+		nodename:   nodeList.Items[1].Name,
+		podenvname: "Hello multihoming-pod-3",
+		template:   multihomingPodTemplate,
+	}
+	pod3.createTestMultihomingPod(oc)
+	o.Expect(waitForPodWithLabelReady(oc, ns1, "name=multihoming-pod3")).NotTo(o.HaveOccurred())
+
+	g.By("Get IPs from the pod1's secondary interface")
+	pod1Name := getPodName(oc, ns1, "name=multihoming-pod1")
+	pod1IPv4, pod1IPv6 := getPodMultiNetwork(oc, ns1, pod1Name[0])
+	e2e.Logf("The v4 address of pod1 is: %v", pod1IPv4, "net1", pod1.podenvname)
+	e2e.Logf("The v6 address of pod1 is: %v", pod1IPv6, "net1", pod1.podenvname)
+
+	g.By("Get IPs from the pod2's secondary interface")
+	pod2Name := getPodName(oc, ns1, "name=multihoming-pod2")
+	pod2IPv4, pod2IPv6 := getPodMultiNetwork(oc, ns1, pod2Name[0])
+	e2e.Logf("The v4 address of pod1 is: %v", pod2IPv4, "net1", pod2.podenvname)
+	e2e.Logf("The v6 address of pod1 is: %v", pod2IPv6, "net1", pod2.podenvname)
+
+	g.By("Get IPs from the pod3's secondary interface")
+	pod3Name := getPodName(oc, ns1, "name=multihoming-pod3")
+	pod3IPv4, pod3IPv6 := getPodMultiNetwork(oc, ns1, pod3Name[0])
+	e2e.Logf("The v4 address of pod1 is: %v", pod3IPv4, "net1", pod3.podenvname)
+	e2e.Logf("The v6 address of pod1 is: %v", pod3IPv6, "net1", pod3.podenvname)
+
+	g.By("Check if the new OVN switch ports is created")
+	listSWCmd := "ovn-nbctl show | grep port | grep " + nadName + " "
+	listOutput, listErr := exutil.RemoteShPodWithBash(oc, "openshift-ovn-kubernetes", ovnMasterPodName, listSWCmd)
+	o.Expect(listErr).NotTo(o.HaveOccurred())
+	podname := []string{pod1Name[0], pod2Name[0], pod3Name[0]}
+	if checkOVNswitchPorts(podname, listOutput) {
+		e2e.Logf("The correct OVN switch ports are create")
+	} else {
+		e2e.Failf("The correct OVN switch ports are not created")
+	}
+
+	g.By("Checking connectivity from pod1 to pod2")
+	CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod2IPv4, "net1", pod2.podenvname)
+	CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod2IPv6, "net1", pod2.podenvname)
+
+	g.By("Checking connectivity from pod1 to pod3")
+	CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod3IPv4, "net1", pod3.podenvname)
+	CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod3IPv6, "net1", pod3.podenvname)
+
+	g.By("Checking connectivity from pod2 to pod1")
+	CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod1IPv4, "net1", pod1.podenvname)
+	CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod1IPv6, "net1", pod1.podenvname)
+
+	g.By("Checking connectivity from pod2 to pod3")
+	CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod3IPv4, "net1", pod3.podenvname)
+	CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod3IPv6, "net1", pod3.podenvname)
+
+	g.By("Checking connectivity from pod3 to pod1")
+	CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod1IPv4, "net1", pod1.podenvname)
+	CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod1IPv6, "net1", pod1.podenvname)
+
+	g.By("Checking connectivity from pod3 to pod2")
+	CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod2IPv4, "net1", pod2.podenvname)
+	CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod2IPv6, "net1", pod2.podenvname)
+
+	podName := []string{pod1Name[0], pod2Name[0], pod3Name[0]}
+	podEnvName := []string{pod1.podenvname, pod2.podenvname, pod3.podenvname}
+	podIPv4 := []string{pod1IPv4, pod2IPv4, pod3IPv4}
+	podIPv6 := []string{pod1IPv6, pod2IPv6, pod3IPv6}
+	return podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns1, nadName
+}
+
+func multihomingAfterCheck(oc *exutil.CLI, podName []string, podEnvName []string, podIPv4 []string, podIPv6 []string, ovnMasterPodName string, ns string, nadName string) {
+	pod1Name := podName[0]
+	pod2Name := podName[1]
+	pod3Name := podName[2]
+	pod1envname := podEnvName[0]
+	pod2envname := podEnvName[1]
+	pod3envname := podEnvName[2]
+	pod1IPv4 := podIPv4[0]
+	pod2IPv4 := podIPv4[1]
+	pod3IPv4 := podIPv4[2]
+	pod1IPv6 := podIPv6[0]
+	pod2IPv6 := podIPv6[1]
+	pod3IPv6 := podIPv6[2]
+
+	g.By("Wait for the new ovn-northbound-leader pod is created")
+	o.Eventually(func() string {
+		podName := getOVNLeaderPod(oc, "north")
+		return podName
+	}, "120s", "5s").Should(o.ContainSubstring("ovnkube-master"), fmt.Sprintf("Failed to get correct OVN leader pod"))
+
+	g.By("Checking connectivity from pod to pod after deleting  all ovnkube-node pods")
+	e2e.Logf("pod1Name is %s", pod1Name)
+	e2e.Logf("pod2IPv4 is  %s", pod2IPv4)
+	e2e.Logf("pod2IPv4 is %s", pod2envname)
+	e2e.Logf("ns is %s", ns)
+
+	CurlMultusPod2PodPass(oc, ns, pod1Name, pod2IPv4, "net1", pod2envname)
+	CurlMultusPod2PodPass(oc, ns, pod1Name, pod2IPv6, "net1", pod2envname)
+	CurlMultusPod2PodPass(oc, ns, pod1Name, pod3IPv4, "net1", pod3envname)
+	CurlMultusPod2PodPass(oc, ns, pod1Name, pod3IPv6, "net1", pod3envname)
+	CurlMultusPod2PodPass(oc, ns, pod2Name, pod1IPv4, "net1", pod1envname)
+	CurlMultusPod2PodPass(oc, ns, pod2Name, pod1IPv6, "net1", pod1envname)
+	CurlMultusPod2PodPass(oc, ns, pod2Name, pod3IPv4, "net1", pod3envname)
+	CurlMultusPod2PodPass(oc, ns, pod2Name, pod3IPv6, "net1", pod3envname)
+	CurlMultusPod2PodPass(oc, ns, pod3Name, pod1IPv4, "net1", pod1envname)
+	CurlMultusPod2PodPass(oc, ns, pod3Name, pod1IPv6, "net1", pod1envname)
+	CurlMultusPod2PodPass(oc, ns, pod3Name, pod2IPv4, "net1", pod2envname)
+	CurlMultusPod2PodPass(oc, ns, pod3Name, pod2IPv6, "net1", pod2envname)
+
+	g.By("Check if the new OVN switch ports are deleted after deleting the pods")
+	o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("all", "--all", "-n", ns).Execute()).NotTo(o.HaveOccurred())
+	//After deleting pods, it will take several seconds to delete the switch ports
+	ovnMasterPodNewName := getOVNLeaderPod(oc, "north")
+	listSWCmd := "ovn-nbctl show | grep port | grep " + nadName + " "
+	o.Eventually(func() bool {
+		listOutput, _ := exutil.RemoteShPodWithBash(oc, "openshift-ovn-kubernetes", ovnMasterPodNewName, listSWCmd)
+		return checkOVNswitchPorts(podName, listOutput)
+	}, 30*time.Second, 5*time.Second).ShouldNot(o.BeTrue(), "The correct OVN switch ports are not deleted")
+
+	g.By("Check if the network-attach-defintion is deleted")
+	o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns).Execute()).NotTo(o.HaveOccurred())
+	if !checkNAD(oc, ns, nadName) {
+		e2e.Logf("The correct network-attach-defintion: %v is deleted!", nadName)
+	} else {
+		e2e.Failf("The correct network-attach-defintion: %v is not deleted!", nadName)
+	}
+
+	g.By("Check if the new created OVN switch is deleted")
+	if !checkOVNSwitch(oc, nadName, ovnMasterPodNewName) {
+		e2e.Logf("The correct OVN switch is deleted")
+	} else {
+		e2e.Failf("The correct OVN switch is not deleted")
+	}
 }
--- test/extended/networking/multihoming.go
+++ test/extended/networking/multihoming.go
@@ -341,168 +341,12 @@ var _ = g.Describe("[sig-networking] SDN", func() {
 
 	// author: weliang@redhat.com
 	g.It("NonHyperShiftHOST-Author:weliang-Medium-60507-Multihoming Verify multihoming pods dualstack connectivity", func() {
-		var (
-			buildPruningBaseDir    = exutil.FixturePath("testdata", "networking/multihoming")
-			multihomingNADTemplate = filepath.Join(buildPruningBaseDir, "multihoming-NAD-template.yaml")
-			multihomingPodTemplate = filepath.Join(buildPruningBaseDir, "multihoming-pod-template.yaml")
-		)
+		var podName, podEnvName, podIPv4, podIPv6 []string
+		var ovnMasterPodName, ns, nadName string
 
-		g.By("Get the ready-schedulable worker nodes")
-		nodeList, nodeErr := e2enode.GetReadySchedulableNodes(oc.KubeFramework().ClientSet)
-		o.Expect(nodeErr).NotTo(o.HaveOccurred())
-		if len(nodeList.Items) < 2 {
-			g.Skip("This case requires 2 nodes, but the cluster has less than two nodes")
-		}
-
-		g.By("Create a test namespace")
-		ns1 := oc.Namespace()
-
-		nadName := "layer2dualstacknetwork"
-		nsWithnad := ns1 + "/" + nadName
-
-		g.By("Create a custom resource network-attach-defintion in tested namespace")
-		defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns1).Execute()
-		nad1ns1 := multihomingNAD{
-			namespace:      ns1,
-			nadname:        nadName,
-			subnets:        "192.168.100.0/24,fd00:dead:beef::0/64",
-			nswithnadname:  nsWithnad,
-			excludeSubnets: "",
-			topology:       "layer2",
-			template:       multihomingNADTemplate,
-		}
-		nad1ns1.createMultihomingNAD(oc)
-
-		g.By("Check if the network-attach-defintion is created")
-		if checkNAD(oc, ns1, nadName) {
-			e2e.Logf("The correct network-attach-defintion: %v is created!", nadName)
-		} else {
-			e2e.Failf("The correct network-attach-defintion: %v is not created!", nadName)
-		}
-
-		g.By("Check if the new OVN switch is created")
-		ovnMasterPodName := getOVNLeaderPod(oc, "north")
-		o.Expect(ovnMasterPodName).ShouldNot(o.Equal(""))
-		if checkOVNSwitch(oc, nadName, ovnMasterPodName) {
-			e2e.Logf("The correct OVN switch is created")
-		} else {
-			e2e.Failf("The correct OVN switch is not created")
-		}
-
-		g.By("Create 1st pod consuming above network-attach-defintion in ns1")
-		pod1 := testMultihomingPod{
-			name:       "multihoming-pod-1",
-			namespace:  ns1,
-			podlabel:   "multihoming-pod1",
-			nadname:    nadName,
-			nodename:   nodeList.Items[0].Name,
-			podenvname: "Hello multihoming-pod-1",
-			template:   multihomingPodTemplate,
-		}
-		pod1.createTestMultihomingPod(oc)
-		o.Expect(waitForPodWithLabelReady(oc, ns1, "name=multihoming-pod1")).NotTo(o.HaveOccurred())
-
-		g.By("Create 2nd pod consuming above network-attach-defintion in ns1")
-		pod2 := testMultihomingPod{
-			name:       "multihoming-pod-2",
-			namespace:  ns1,
-			podlabel:   "multihoming-pod2",
-			nadname:    nadName,
-			nodename:   nodeList.Items[0].Name,
-			podenvname: "Hello multihoming-pod-2",
-			template:   multihomingPodTemplate,
-		}
-		pod2.createTestMultihomingPod(oc)
-		o.Expect(waitForPodWithLabelReady(oc, ns1, "name=multihoming-pod2")).NotTo(o.HaveOccurred())
-
-		g.By("Create 3rd pod consuming above network-attach-defintion in ns1")
-		pod3 := testMultihomingPod{
-			name:       "multihoming-pod-3",
-			namespace:  ns1,
-			podlabel:   "multihoming-pod3",
-			nadname:    nadName,
-			nodename:   nodeList.Items[1].Name,
-			podenvname: "Hello multihoming-pod-3",
-			template:   multihomingPodTemplate,
-		}
-		pod3.createTestMultihomingPod(oc)
-		o.Expect(waitForPodWithLabelReady(oc, ns1, "name=multihoming-pod3")).NotTo(o.HaveOccurred())
-
-		g.By("Get IPs from the pod1's secondary interface")
-		pod1Name := getPodName(oc, ns1, "name=multihoming-pod1")
-		pod1IPv4, pod1IPv6 := getPodMultiNetwork(oc, ns1, pod1Name[0])
-		e2e.Logf("The v4 address of pod1 is: %v", pod1IPv4, "net1", pod1.podenvname)
-		e2e.Logf("The v6 address of pod1 is: %v", pod1IPv6, "net1", pod1.podenvname)
-
-		g.By("Get IPs from the pod2's secondary interface")
-		pod2Name := getPodName(oc, ns1, "name=multihoming-pod2")
-		pod2IPv4, pod2IPv6 := getPodMultiNetwork(oc, ns1, pod2Name[0])
-		e2e.Logf("The v4 address of pod1 is: %v", pod2IPv4, "net1", pod2.podenvname)
-		e2e.Logf("The v6 address of pod1 is: %v", pod2IPv6, "net1", pod2.podenvname)
-
-		g.By("Get IPs from the pod3's secondary interface")
-		pod3Name := getPodName(oc, ns1, "name=multihoming-pod3")
-		pod3IPv4, pod3IPv6 := getPodMultiNetwork(oc, ns1, pod3Name[0])
-		e2e.Logf("The v4 address of pod1 is: %v", pod3IPv4, "net1", pod3.podenvname)
-		e2e.Logf("The v6 address of pod1 is: %v", pod3IPv6, "net1", pod3.podenvname)
-
-		g.By("Check if the new OVN switch ports is created")
-		listSWCmd := "ovn-nbctl show | grep port | grep " + nadName + " "
-		listOutput, listErr := exutil.RemoteShPodWithBash(oc, "openshift-ovn-kubernetes", ovnMasterPodName, listSWCmd)
-		o.Expect(listErr).NotTo(o.HaveOccurred())
-		podname := []string{pod1Name[0], pod2Name[0], pod3Name[0]}
-		if checkOVNswitchPorts(podname, listOutput) {
-			e2e.Logf("The correct OVN switch ports are create")
-		} else {
-			e2e.Failf("The correct OVN switch ports are not created")
-		}
-
-		g.By("Checking connectivity from pod1 to pod2")
-		CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod2IPv4, "net1", pod2.podenvname)
-		CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod2IPv6, "net1", pod2.podenvname)
-
-		g.By("Checking connectivity from pod1 to pod3")
-		CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod3IPv4, "net1", pod3.podenvname)
-		CurlMultusPod2PodPass(oc, ns1, pod1Name[0], pod3IPv6, "net1", pod3.podenvname)
-
-		g.By("Checking connectivity from pod2 to pod1")
-		CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod1IPv4, "net1", pod1.podenvname)
-		CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod1IPv6, "net1", pod1.podenvname)
-
-		g.By("Checking connectivity from pod2 to pod3")
-		CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod3IPv4, "net1", pod3.podenvname)
-		CurlMultusPod2PodPass(oc, ns1, pod2Name[0], pod3IPv6, "net1", pod3.podenvname)
-
-		g.By("Checking connectivity from pod3 to pod1")
-		CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod1IPv4, "net1", pod1.podenvname)
-		CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod1IPv6, "net1", pod1.podenvname)
-
-		g.By("Checking connectivity from pod3 to pod2")
-		CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod2IPv4, "net1", pod2.podenvname)
-		CurlMultusPod2PodPass(oc, ns1, pod3Name[0], pod2IPv6, "net1", pod2.podenvname)
-
-		g.By("Check if the new OVN switch ports are deleted after deleting the pods")
-		o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("all", "--all", "-n", ns1).Execute()).NotTo(o.HaveOccurred())
-		//After deleting pods, it will take several seconds to delete the switch ports
-		o.Eventually(func() bool {
-			listOutput, _ := exutil.RemoteShPodWithBash(oc, "openshift-ovn-kubernetes", ovnMasterPodName, listSWCmd)
-			return checkOVNswitchPorts(podname, listOutput)
-		}, 20*time.Second, 5*time.Second).ShouldNot(o.BeTrue(), "The correct OVN switch ports are not deleted")
-
-		g.By("Check if the network-attach-defintion is deleted")
-		o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns1).Execute()).NotTo(o.HaveOccurred())
-		if !checkNAD(oc, ns1, nadName) {
-			e2e.Logf("The correct network-attach-defintion: %v is deleted!", nadName)
-		} else {
-			e2e.Failf("The correct network-attach-defintion: %v is not deleted!", nadName)
-		}
-
-		g.By("Check if the new created OVN switch is deleted")
-		if !checkOVNSwitch(oc, nadName, ovnMasterPodName) {
-			e2e.Logf("The correct OVN switch is deleted")
-		} else {
-			e2e.Failf("The correct OVN switch is not deleted")
-		}
+		defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns).Execute()
+		podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName = multihomingBeforeCheck(oc)
+		multihomingAfterCheck(oc, podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName)
 	})
 
 	// author: weliang@redhat.com
@@ -997,5 +841,54 @@ var _ = g.Describe("[sig-networking] SDN", func() {
 		} else {
 			e2e.Failf("The correct OVN switch: %v is not deleted!", nadName2)
 		}
+	})
+
+	// author: weliang@redhat.com
+	g.It("NonHyperShiftHOST-Author:weliang-Medium-60511-Multihoming Verify multihoming pods’ dualstack connectivity after deleting ovn-northbound-leader pod. [Disruptive]", func() {
+		var podName, podEnvName, podIPv4, podIPv6 []string
+		var ovnMasterPodName, ns, nadName string
+
+		defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns).Execute()
+		podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName = multihomingBeforeCheck(oc)
+
+		g.By("Delete ovn-northbound-leader pod")
+		o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("pod", ovnMasterPodName, "-n", "openshift-ovn-kubernetes").Execute()).NotTo(o.HaveOccurred())
+
+		multihomingAfterCheck(oc, podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName)
+
+	})
+
+	// author: weliang@redhat.com
+	g.It("NonHyperShiftHOST-Author:weliang-Medium-60512-Multihoming Verify multihoming pods’ dualstack connectivity after deleting all ovnkube-master pods. [Disruptive]", func() {
+		var podName, podEnvName, podIPv4, podIPv6 []string
+		var ovnMasterPodName, ns, nadName string
+
+		defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns).Execute()
+		podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName = multihomingBeforeCheck(oc)
+
+		g.By("Delete  all ovnkube-master pods")
+		ovnMasterPodNames := getPodName(oc, "openshift-ovn-kubernetes", "app=ovnkube-master")
+		for _, ovnPod := range ovnMasterPodNames {
+			o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("pod", ovnPod, "-n", "openshift-ovn-kubernetes").Execute()).NotTo(o.HaveOccurred())
+		}
+
+		multihomingAfterCheck(oc, podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName)
+	})
+
+	// author: weliang@redhat.com
+	g.It("NonHyperShiftHOST-Author:weliang-Medium-60516-Multihoming Verify multihoming pods’ dualstack connectivity after deleting all ovnkube-node pods. [Disruptive]", func() {
+		var podName, podEnvName, podIPv4, podIPv6 []string
+		var ovnMasterPodName, ns, nadName string
+
+		defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("net-attach-def", nadName, "-n", ns).Execute()
+		podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName = multihomingBeforeCheck(oc)
+
+		g.By("Delete all ovnkube-node pods")
+		ovnNodePodNames := getPodName(oc, "openshift-ovn-kubernetes", "app=ovnkube-node")
+		for _, ovnPod := range ovnNodePodNames {
+			o.Expect(oc.AsAdmin().WithoutNamespace().Run("delete").Args("pod", ovnPod, "-n", "openshift-ovn-kubernetes").Execute()).NotTo(o.HaveOccurred())
+		}
+
+		multihomingAfterCheck(oc, podName, podEnvName, podIPv4, podIPv6, ovnMasterPodName, ns, nadName)
 	})
 })
