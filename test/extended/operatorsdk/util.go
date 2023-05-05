703d9417c110e3a7d3957ecff4cef3734490bd93
Keenon Lee <jitli@redhat.com>
2023-05-05 20:26:42 +0800

    enhance create catsrc (#9222)

--- test/extended/operatorsdk/util.go
+++ test/extended/operatorsdk/util.go
@@ -2,14 +2,15 @@ package operatorsdk
 
 import (
 	"fmt"
-	"github.com/openshift/openshift-tests-private/test/extended/util/architecture"
 	"strings"
 	"time"
 
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
+	"github.com/openshift/openshift-tests-private/test/extended/util/architecture"
 	container "github.com/openshift/openshift-tests-private/test/extended/util/container"
+	"github.com/tidwall/gjson"
 	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
@@ -190,8 +191,33 @@ func (catsrc *catalogSourceDescription) create(oc *exutil.CLI, itName string, dr
 		"DISPLAYNAME="+"\""+catsrc.displayName+"\"", "PUBLISHER="+"\""+catsrc.publisher+"\"", "SOURCETYPE="+catsrc.sourceType,
 		"INTERVAL="+catsrc.interval, "IMAGETEMPLATE="+catsrc.imageTemplate)
 	o.Expect(err).NotTo(o.HaveOccurred())
+	catsrc.setSCCRestricted(oc)
 	dr.getIr(itName).add(newResource(oc, "catsrc", catsrc.name, requireNS, catsrc.namespace))
 	e2e.Logf("create catsrc %s SUCCESS", catsrc.name)
+}
+
+func (catsrc *catalogSourceDescription) setSCCRestricted(oc *exutil.CLI) {
+	output, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("configmaps", "-n", "openshift-kube-apiserver", "config", `-o=jsonpath={.data.config\.yaml}`).Output()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	psa := gjson.Get(output, "admission.pluginConfig.PodSecurity.configuration.defaults.enforce").String()
+	e2e.Logf("pod-security.kubernetes.io/enforce is %s", string(psa))
+	if strings.Contains(string(psa), "restricted") {
+		originSCC := catsrc.getSCC(oc)
+		e2e.Logf("spec.grpcPodConfig.securityContextConfig is %s", originSCC)
+		if strings.Compare(originSCC, "") == 0 {
+			e2e.Logf("set spec.grpcPodConfig.securityContextConfig to be restricted")
+			err := oc.AsAdmin().WithoutNamespace().Run("patch").Args("catsrc", catsrc.name, "-n", catsrc.namespace, "--type=merge", "-p", `{"spec":{"grpcPodConfig":{"securityContextConfig":"restricted"}}}`).Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+		} else {
+			e2e.Logf("spec.grpcPodConfig.securityContextConfig is not empty, skip setting")
+		}
+	}
+}
+
+func (catsrc *catalogSourceDescription) getSCC(oc *exutil.CLI) string {
+	output, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("catsrc", catsrc.name, "-n", catsrc.namespace, "-o=jsonpath={.spec.grpcPodConfig.securityContextConfig}").Output()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	return output
 }
 
 func (catsrc *catalogSourceDescription) createWithCheck(oc *exutil.CLI, itName string, dr describerResrouce) {
