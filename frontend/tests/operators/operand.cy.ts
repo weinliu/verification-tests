f4dd4e445de712f57c4de0daca853c2e799bc98f
Ya Dan Pei <yapei@redhat.com>
2023-05-09 15:12:59 +0800

    auto OCP-63078 (#9215)

--- frontend/tests/operators/operand.cy.ts
+++ frontend/tests/operators/operand.cy.ts
@@ -1,34 +1,34 @@
 import { testName } from "upstream/support";
+import { Operand } from "../../views/operator-hub-page";
 
 describe('operand tests', () => {
     before(() => {
-        const fileNames = ['crd','csv1','csv2','csv3','operand']
-        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
-        cy.adminCLI(`oc new-project ${testName}`);
-        cy.wrap(fileNames).each(fileName => {
-            cy.adminCLI(`oc create -f ./fixtures/${fileName}.yaml -n ${testName}`)
-              .then(result => { expect(result.stdout).contain("created")
-            })
-        });  
-        cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));        
+      const fileNames = ['crd','csv1','csv2','csv3','operand']
+      cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+      cy.adminCLI(`oc new-project ${testName}`);
+      cy.wrap(fileNames).each(fileName => {
+        cy.adminCLI(`oc create -f ./fixtures/${fileName}.yaml -n ${testName}`)
+          .then(result => { expect(result.stdout).contain("created")
+        })
+      });
+      cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
     });
 
     after(() => {
-        cy.adminCLI(`oc delete project ${testName}`);
-        cy.adminCLI(`oc delete customresourcedefinition mock-resources.test.tectonic.com`);
-        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
-        cy.logout();
+      cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+      cy.adminCLI(`oc delete project ${testName}`);
+      cy.adminCLI(`oc delete customresourcedefinition mock-resources.test.tectonic.com`);
     });
 
     it('(OCP-46583,xiyuzhao) Operator should be able to customize order of conditions table', {tags: ['e2e','admin','@osd-ccs','@rosa']}, () => {
-        // If a descriptor is defined on the status.conditions property, 
+        // If a descriptor is defined on the status.conditions property,
         // It will be rendered in the order it appears in the CSV descriptors array
         cy.visit(`/k8s/ns/${testName}/clusterserviceversions/mock-operator/test.tectonic.com~v1~MockResource/mock-resource-instance`)
           .get('[data-test-id="resource-title"]')
           .should("contain.text","mock");
-        cy.wait(5000);        
+        cy.wait(5000);
         cy.get(".co-section-heading span").should('have.length',4)
-          .and(($span) => { 
+          .and(($span) => {
             expect($span.get(1).textContent, 'first section').to.equal('Custom Conditions')
             expect($span.get(2).textContent, 'second section').to.equal('Conditions')
             expect($span.get(3).textContent, 'third section').to.equal('Other Custom Conditions')
@@ -39,9 +39,9 @@ describe('operand tests', () => {
         cy.visit(`/k8s/ns/${testName}/clusterserviceversions/mock-operator2/test.tectonic.com~v1~MockResource/mock-resource-instance`)
           .get('[data-test-id="resource-title"]')
           .should("contain.text","mock");
-        cy.wait(5000);        
+        cy.wait(5000);
         cy.get(".co-section-heading span").should('have.length',3)
-          .and(($span) => { 
+          .and(($span) => {
             expect($span.get(1).textContent, 'first section').to.equal('Conditions')
             expect($span.get(2).textContent, 'second section').to.equal('Custom Conditions')
            });
@@ -51,12 +51,77 @@ describe('operand tests', () => {
         cy.visit(`/k8s/ns/${testName}/clusterserviceversions/mock-operator3/test.tectonic.com~v1~MockResource/mock-resource-instance`)
           .get('[data-test-id="resource-title"]')
           .should("contain.text","mock");
-        cy.wait(5000);        
+        cy.wait(5000);
         cy.get(".co-section-heading span").should('have.length',4)
-          .and(($span) => { 
+          .and(($span) => {
             expect($span.get(1).textContent, 'first section').to.equal('Conditions')
             expect($span.get(2).textContent, 'second section').to.equal('Other Custom Conditions')
             expect($span.get(3).textContent, 'third section').to.equal('Custom Conditions')
            });
     });
-}) 
+
+    it('(OCP-63078,yapei)Enable filtering for k8sResourcePrefix x-descriptor', {tags: ['e2e','admin','@osd-ccs','@rosa']}, () => {
+      // create several secrets with different labels for later filtering
+      let secret = [{'name': 'test-secret-equity', 'literal': 'key111=value111', 'labels': 'test=true level=staging'},
+                    {'name': 'test-secret-set', 'literal': 'key222=value222', 'labels': 'test=true level=production'},
+                    {'name': 'test-secret-mix', 'literal': 'key333=value333', 'labels': 'level=qa'},
+                    {'name': 'test-secret-all', 'literal': 'key444=value444', 'labels': 'all=true'}]
+      cy.wrap(secret).each((item) => {
+        cy.adminCLI(`oc create secret generic ${item.name} --from-literal=${item.literal} -n ${testName}`);
+        cy.adminCLI(`oc label secret ${item.name} ${item.labels} -n ${testName}`);
+      });
+      cy.exec(`oc get secret --show-labels -n ${testName}`)
+        .its('stdout')
+        .should('match', /test-secret-equity.*level=staging,test=true/)
+        .and('match', /test-secret-mix.*level=qa/)
+        .and('match', /test-secret-set.*level=production,test=true/)
+        .and('match', /all=true/)
+      cy.visit(`/k8s/ns/${testName}/operators.coreos.com~v1alpha1~ClusterServiceVersion/mock-operator/test.tectonic.com~v1~MockResource/~new`);
+      Operand.switchToFormView();
+      cy.get('button#root_spec_k8sResourcePrefixNoFilter').click();
+      cy.get('ul[role="listbox"]')
+        .within(() => {
+          cy.contains('test-secret-equity').as('equity').should('be.visible');
+          cy.contains('test-secret-set').as('set').should('be.visible');
+          cy.contains('test-secret-mix').as('mix').should('be.visible');
+          cy.contains('test-secret-all').as('all').should('be.visible').click();
+        })
+
+      cy.get('button#root_spec_k8sResourcePrefixEquityFilter').click();
+      cy.get('ul[role="listbox"]')
+        .within(() => {
+          cy.get('@set').should('not.exist');
+          cy.get('@mix').should('not.exist');
+          cy.get('@all').should('not.exist');
+          cy.get('@equity').should('be.visible').click();
+        })
+
+      cy.get('button#root_spec_k8sResourcePrefixSetFilter').click();
+      cy.get('ul[role="listbox"]')
+        .within(() => {
+          cy.get('@equity').should('be.visible');
+          cy.get('@mix').should('not.exist');
+          cy.get('@all').should('not.exist');
+          cy.get('@set').should('be.visible').click();
+        })
+
+      cy.get('button#root_spec_k8sResourcePrefixMixedFilter').click();
+      cy.get('ul[role="listbox"]')
+        .within(() => {
+          cy.get('@equity').should('not.exist');
+          cy.get('@set').should('not.exist');
+          cy.get('@all').should('not.exist');
+          cy.get('@mix').should('be.visible').click();
+        })
+
+      cy.contains('No k8sResourcePrefixNone found').should('exist');
+      Operand.submitCreation();
+
+      cy.adminCLI(`oc get mockresource.test.tectonic.com example -n ${testName} -o yaml`)
+        .its('stdout')
+        .should('match', /k8sResourcePrefixEquityFilter: test-secret-equity/)
+        .and('match', /k8sResourcePrefixMixedFilter: test-secret-mix/)
+        .and('match', /k8sResourcePrefixNoFilter: test-secret-all/)
+        .and('match', /k8sResourcePrefixSetFilter: test-secret-set/)
+    });
+})
