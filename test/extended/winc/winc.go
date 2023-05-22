b7f5ee1e60832ca7fc116333973f8f79f3a17589
Ronnie Rasouli <rrasouli@redhat.com>
2023-05-22 11:41:16 +0300

    Adding csi-proxy to payload (#9508)

--- test/extended/winc/winc.go
+++ test/extended/winc/winc.go
@@ -114,7 +114,7 @@ var _ = g.Describe("[sig-windows] Windows_Containers", func() {
 		}{
 			{
 				folder:   "/payload",
-				expected: "azure-cloud-node-manager.exe cni containerd generated hybrid-overlay-node.exe kube-node powershell windows-instance-config-daemon.exe windows_exporter.exe",
+				expected: "azure-cloud-node-manager.exe cni containerd csi-proxy generated hybrid-overlay-node.exe kube-node powershell windows-instance-config-daemon.exe windows_exporter.exe",
 			},
 			{
 				folder:   "/payload/containerd",
