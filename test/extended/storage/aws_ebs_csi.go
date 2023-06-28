975268fe92a2c5be4dd0bb909136fe3cb1d72047
Rohit Patil <ropatil@redhat.com>
2023-06-28 06:35:35 +0530

    fix getpodList (#10204)
    * fix getpodList
    
    * fix replicanumber
    
    ---------
    
    Co-authored-by: rohit010pa <rohit010patil@gmail.com>

--- test/extended/storage/aws_ebs_csi.go
+++ test/extended/storage/aws_ebs_csi.go
@@ -226,7 +226,7 @@ var _ = g.Describe("[sig-storage] STORAGE", func() {
 
 		g.By("# Get the origin aws-ebs-csi-driver-controller pod name")
 		defer waitCSOhealthy(oc.AsAdmin())
-		awsEbsCsiDriverController := newDeployment(setDeploymentName("aws-ebs-csi-driver-controller"), setDeploymentNamespace("openshift-cluster-csi-drivers"), setDeploymentApplabel("app=aws-ebs-csi-driver-controller"))
+		awsEbsCsiDriverController := newDeployment(setDeploymentName("aws-ebs-csi-driver-controller"), setDeploymentNamespace("openshift-cluster-csi-drivers"), setDeploymentApplabel("app=aws-ebs-csi-driver-controller"), setDeploymentReplicasNo("2"))
 		originPodList := awsEbsCsiDriverController.getPodList(oc.AsAdmin())
 		resourceVersionOri, resourceVersionOriErr := oc.WithoutNamespace().AsAdmin().Run("get").Args("deployment", "aws-ebs-csi-driver-controller", "-n", "openshift-cluster-csi-drivers", "-o=jsonpath={.metadata.resourceVersion}").Output()
 		o.Expect(resourceVersionOriErr).ShouldNot(o.HaveOccurred())
