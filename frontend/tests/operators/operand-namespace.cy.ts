9fd32b27bd2ff8a28fe2a8812abef2f4e1655377
Xiyun Zhao <xiyuzhao@redhat.com>
2023-09-05 13:57:23 +0800

    Restructure TestCase OCP-50153 (#11300)
    Update parameter name to make it more clearly
      Update remove function to make data clean
      Remove unnecessary Yaml file
      Update comment details
      Update data-test-id for 'All instances' Tab

--- frontend/tests/operators/operand-namespace.cy.ts
+++ frontend/tests/operators/operand-namespace.cy.ts
@@ -2,46 +2,50 @@ import { operatorHubPage } from "views/operator-hub-page"
 
 describe('Display All Namespace Operands for Global Operators', () => {
   const params = {
-    anotherNamespace: 'argocd-another',
+    ns: 'ocp50153-project',
     operatorName: 'argocd-operator',
     catalogSourceName: 'custom-catalogsource',
     catalogSourceFile: 'custom-catalog-source.json',
-    operandFile: 'operands.yaml',
-    argocd: 'argocd'
+    subscriptionName: 'argocd'
   }
 
   before(() => {
     cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
     cy.adminCLI(`oc apply -f ./fixtures/operators/${params.catalogSourceFile}`)
-    cy.adminCLI(`oc new-project ${params.anotherNamespace}`)
+    cy.adminCLI(`oc new-project ${params.ns}`)
     cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
   })
 
   after(() => {
-    cy.adminCLI(`oc delete project ${params.anotherNamespace}`)
-    cy.adminCLI(`oc delete csv argocd-operator.v0.0.15 -n openshift-operators`)
+    cy.adminCLI(`oc delete subscription ${params.operatorName} -n openshift-operators`);
+    cy.adminCLI(`oc delete clusterserviceversion ${params.operatorName}.v0.0.15 -n openshift-operators`);
+    cy.adminCLI(`oc delete project ${params.ns}`)
+    cy.adminCLI(`oc delete CatalogSource ${params.catalogSourceName} -n openshift-marketplace`);
     cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
-    cy.logout()
   })
 
   it('(OCP-50153, xiyuzhao) - Display All Namespace Operands for Global Operators', {tags: ['e2e','admin']}, () => {
-    // install the operator
-    operatorHubPage.installOperator(params.operatorName, params.catalogSourceName)
-    // wait for operator to install
-    cy.contains('View Operator').click()
-    // install the operands
-    cy.adminCLI(`oc apply -f ./fixtures/${params.operandFile}`)
-    // check namespace radio button is selected for operand on the operator page
-    cy.byLegacyTestID('horizontal-link-olm~All instances').click()
-    // checkpoint 1: Improved Column
+    operatorHubPage.installOperator(params.operatorName, params.catalogSourceName);
+    cy.contains('View Operator').click();
+    cy.exec(`echo '{
+      "kind": "ArgoCD",
+      "apiVersion": "argoproj.io/v1alpha1",
+      "metadata": {
+        "name": "${params.subscriptionName}",
+        "namespace": "${params.ns}"
+      },
+      "spec": {}
+    }' | oc create -f - --kubeconfig=${Cypress.env('KUBECONFIG_PATH')}` );
+    cy.byLegacyTestID('horizontal-link-All instances').click();
+
+    // checkpoint 1: Check column 'Namespace' is added in list
     cy.get('[data-label="Namespace"]').should('be.visible')
-    // checkpoint 2: All namespace radio input is selected by dafault
+    // checkpoint 2: Check 'All namespace' radio input is selected by dafault
     cy.byTestID('All namespaces-radio-input').should('be.checked')
-    // checkpoint 2: Two resources are shown when All Namespaces option is selected
-    cy.byTestID(params.argocd).should('be.visible')
-    // checkpoint 3: Only one corresponding resource is shown on specific ns with Current Namespace Only selected
-    cy.byLegacyTestID('horizontal-link-olm~All instances').click()
-    cy.byTestID('Current namespace only-radio-input').check()
-    cy.byTestID(params.argocd).should('not.exist')
+    // checkpoint 3: Check subscription is listed
+    cy.byTestID(params.subscriptionName).should('be.visible')
+    // checkpoint 4: Check only corresponding resource is displayed on specific ns
+    cy.byTestID('Current namespace only-radio-input').click()
+    cy.byTestID(params.subscriptionName).should('not.exist')
   })
 })
