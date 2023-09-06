12bd2dd956263caac1a57c12b0369787399471bf
Xiyun Zhao <xiyuzhao@redhat.com>
2023-09-06 13:57:00 +0800

    Add new testcase OCP-65876 and update OCP-54975 (#11219)

--- frontend/tests/operators/installed-operators.cy.ts
+++ frontend/tests/operators/installed-operators.cy.ts
@@ -5,47 +5,49 @@ import { guidedTour } from './../../upstream/views/guided-tour';
 describe('Operators Installed page test', () => {
   const params ={
     'ns54975': 'ocp54975-project',
+    'specialNs': 'openshift',
     'csName': 'custom-catalogsource',
     'sonarqube': 'sonarqube-operator',
     'infinispan': 'infinispan-operator'
   }
-  
+
   before(() => {
     cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
     guidedTour.close();
     cy.createProject(params.ns54975);
     cy.switchPerspective('Administrator');
     cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
-    cy.adminCLI(`oc label namespace openshift security.openshift.io/scc.podSecurityLabelSync=false --overwrite`)
-    cy.adminCLI(`oc label namespace openshift pod-security.kubernetes.io/enforce=privileged --overwrite`)
-    cy.adminCLI(`oc label namespace openshift pod-security.kubernetes.io/audit=privileged --overwrite`)
-    cy.adminCLI(`oc label namespace openshift pod-security.kubernetes.io/warn=privileged --overwrite`)
+    cy.adminCLI(`oc label namespace ${params.specialNs} security.openshift.io/scc.podSecurityLabelSync=false --overwrite`);
+    cy.adminCLI(`oc label namespace ${params.specialNs} pod-security.kubernetes.io/enforce=privileged --overwrite`);
+    cy.adminCLI(`oc label namespace ${params.specialNs} pod-security.kubernetes.io/audit=privileged --overwrite`);
+    cy.adminCLI(`oc label namespace ${params.specialNs} pod-security.kubernetes.io/warn=privileged --overwrite`);
     cy.adminCLI(`oc create -f ./fixtures/operators/custom-catalog-source.json`);
+    cy.visit('/k8s/ns/openshift-marketplace/operators.coreos.com~v1alpha1~CatalogSource/custom-catalogsource');
+    cy.get('[data-test-selector="details-item-value__Status"]').should('contain.text', 'READY');
+    operatorHubPage.installOperator(params.sonarqube, params.csName, params.specialNs);
+    operatorHubPage.installOperator(params.infinispan, params.csName);
     });
 
   after(() => {
-    cy.adminCLI(`oc patch olmconfigs cluster --type=merge -p 'spec: {features: {disableCopiedCSVs: false}}'`)
-    cy.adminCLI(`oc delete subscription ${params.sonarqube} -n openshift`);
-    cy.adminCLI(`oc delete clusterserviceversion ${params.sonarqube}.v0.0.6 -n openshift`);
+    cy.adminCLI(`oc patch olmconfigs cluster --type=merge -p 'spec: {features: {disableCopiedCSVs: false}}'`);
+    cy.adminCLI(`oc delete subscription ${params.sonarqube} -n ${params.specialNs}`);
+    cy.adminCLI(`oc delete clusterserviceversion ${params.sonarqube}.v0.0.6 -n ${params.specialNs}`);
     cy.adminCLI(`oc delete subscription ${params.infinispan} -n openshift-operators`);
-    cy.adminCLI(`oc delete clusterserviceversion ${params.infinispan}.v2.1.5 -n openshift-operators `)
+    cy.adminCLI(`oc delete clusterserviceversion ${params.infinispan}.v2.1.5 -n openshift-operators `);
     cy.adminCLI(`oc delete CatalogSource custom-catalogsource -n openshift-marketplace`);
-    cy.adminCLI(`oc label namespace openshift security.openshift.io/scc.podSecurityLabelSync-`)
-    cy.adminCLI(`oc label namespace openshift pod-security.kubernetes.io/enforce-`)
-    cy.adminCLI(`oc label namespace openshift pod-security.kubernetes.io/audit-`)
-    cy.adminCLI(`oc label namespace openshift pod-security.kubernetes.io/warn-`)
+    cy.adminCLI(`oc label namespace ${params.specialNs} security.openshift.io/scc.podSecurityLabelSync-`);
+    cy.adminCLI(`oc label namespace ${params.specialNs} pod-security.kubernetes.io/enforce-`);
+    cy.adminCLI(`oc label namespace ${params.specialNs} pod-security.kubernetes.io/audit-`);
+    cy.adminCLI(`oc label namespace ${params.specialNs} pod-security.kubernetes.io/warn-`);
     cy.adminCLI(`oc delete project ${params.ns54975}`);
+    cy.adminCLI(`oc adm policy remove-role-from-user admin ${Cypress.env('LOGIN_USERNAME')} -n ${params.specialNs}`);
     cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false })
-    });
+  });
 
   it('(OCP-54975,xiyuzhao) Check OCP console works when copied CSVs are disabled',{tags: ['e2e','admin','@osd-ccs','@rosa']}, () => {
-
-    operatorHubPage.installOperator(`${params.sonarqube}`, `${params.csName}`, 'openshift');
-    operatorHubPage.installOperator(`${params.infinispan}`, `${params.csName}`);
     cy.visit(`/k8s/all-namespaces/operators.coreos.com~v1alpha1~ClusterServiceVersion`);
     operatorHubPage.checkOperatorStatus('Sonarqube Operator', 'Succeed');
     operatorHubPage.checkOperatorStatus('Infinispan Operator', 'Succeed');
-   
     /* 1. Check the default value for the Flag is false
        2. Check When Flag = false，CSV have copy file in other namespace */
     let cpt;
@@ -57,16 +59,15 @@ describe('Operators Installed page test', () => {
       .then(result => { expect(result.stdout).contain("infinispan")})
     cy.adminCLI(`oc get csv -n openshift-console`)
       .then(result => { expect(result.stdout).contain("infinispan")})
-    
-    /* 
+    /*
        Check for normal user
        1. The Flag can take affect after OLMConfig being updated
        2. The Flag only take affect for the operator installed in All namespace
        3. When Flag = true, the golbal installed operator's CSV ns will update to 'openshit'
             And new lable 'olm.copiedFrom' is added for the Operator
     */
-    cy.adminCLI(`oc patch olmconfigs cluster --type=merge -p 'spec: {features: {disableCopiedCSVs: true}}'`)
-    cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+    cy.adminCLI(`oc patch olmconfigs cluster --type=merge -p 'spec: {features: {disableCopiedCSVs: true}}'`);
+    cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false })
     cy.logout;
     cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
     cy.visit(`/k8s/ns/${params.ns54975}/operators.coreos.com~v1alpha1~ClusterServiceVersion`);
@@ -78,4 +79,32 @@ describe('Operators Installed page test', () => {
     cy.get('[data-test="label-key"]').should("contain.text","olm.copiedFrom");
     });
 
+  it('(OCP-65876,xiyuzhao) Non cluster-admin user should able to update the operator in Console	',{tags: ['e2e','admin','@osd-ccs','@rosa']}, () => {
+    cy.adminCLI(`oc adm policy add-role-to-user admin ${Cypress.env('LOGIN_USERNAME')} -n ${params.specialNs}`);
+    cy.exec(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false })
+    cy.logout;
+    cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
+    /*' Scription' Tab is exist for the operator installed in the All namespace
+       Page is Restricted Access */
+    cy.visit(`/k8s/ns/${params.specialNs}/operators.coreos.com~v1alpha1~ClusterServiceVersion`);
+    cy.get('[data-test-operator-row="Infinispan Operator"]').click();
+    cy.get('[data-test-id="horizontal-link-Subscription"]')
+      .as('subscriptionTab')
+      .should('exist')
+      .click();
+    cy.get('[data-test="msg-box-title"]').should('contain.text','Restricted Access');
+    /* Check 'Scription' Tab is visible to normal user for operator installed in specific namespace
+       User who has authority is able to check the installPlan and CatalogSource info */
+    cy.visit(`/k8s/ns/${params.specialNs}/operators.coreos.com~v1alpha1~ClusterServiceVersion`);
+    cy.get('[data-test-operator-row="Sonarqube Operator"]').click();
+    cy.get('@subscriptionTab').should('exist').click();
+    cy.get('[title="InstallPlan"]')
+      .next('a')
+      .should('have.attr', 'href')
+      .and('include', 'InstallPlan');
+    cy.get('[title="CatalogSource"]')
+      .next('a')
+      .should('have.attr', 'href')
+      .and('include', 'CatalogSource');
+  });
 })
