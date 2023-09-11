4a2c959fa1d81cbadaa576e5d6b39d637dd5e914
Weibin Liang <weliang@redhat.com>
2023-09-10 22:45:27 -0400

    Disable OCP-46387 (#11417)
    * Disable OCP-46387
    
    * Format the file

--- test/extended/networking/multus.go
+++ test/extended/networking/multus.go
@@ -18,35 +18,39 @@ var _ = g.Describe("[sig-networking] SDN", func() {
 
 	var oc = exutil.NewCLI("networking-multus", exutil.KubeConfigPath())
 
-	// author: weliang@redhat.com
-	g.It("Author:weliang-Medium-46387-[BZ 1896533] network operator degraded due to additionalNetwork in non-existent namespace. [Disruptive]", func() {
-		var (
-			patchSResource = "networks.operator.openshift.io/cluster"
-			patchInfo      = fmt.Sprintf("{\"spec\":{\"additionalNetworks\": [{\"name\": \"secondary\",\"namespace\":\"ocp-46387\",\"simpleMacvlanConfig\": {\"ipamConfig\": {\"staticIPAMConfig\": {\"addresses\": [{\"address\": \"10.1.1.0/24\"}] },\"type\": \"static\"}},\"type\": \"SimpleMacvlan\"}]}}")
-		)
+	// OCP-46387 failed in 4.14 due to https://issues.redhat.com/browse/OCPBUGS-11082 and https://issues.redhat.com/browse/NP-752
+	// Enable this case until Dev fix the issue
+	/*
+		// author: weliang@redhat.com
+		g.It("Author:weliang-Medium-46387-[BZ 1896533] network operator degraded due to additionalNetwork in non-existent namespace. [Disruptive]", func() {
+			var (
+				patchSResource = "networks.operator.openshift.io/cluster"
+				patchInfo      = fmt.Sprintf("{\"spec\":{\"additionalNetworks\": [{\"name\": \"secondary\",\"namespace\":\"ocp-46387\",\"simpleMacvlanConfig\": {\"ipamConfig\": {\"staticIPAMConfig\": {\"addresses\": [{\"address\": \"10.1.1.0/24\"}] },\"type\": \"static\"}},\"type\": \"SimpleMacvlan\"}]}}")
+			)
 
-		g.By("create new namespace")
-		namespace := fmt.Sprintf("ocp-46387")
-		err := oc.Run("new-project").Args(namespace).Execute()
-		o.Expect(err).NotTo(o.HaveOccurred())
-		defer oc.AsAdmin().Run("delete").Args("project", namespace, "--ignore-not-found").Execute()
+			g.By("create new namespace")
+			namespace := fmt.Sprintf("ocp-46387")
+			err := oc.Run("new-project").Args(namespace).Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+			defer oc.AsAdmin().Run("delete").Args("project", namespace, "--ignore-not-found").Execute()
 
-		g.By("Configure network-attach-definition through network operator")
-		patchResourceAsAdmin(oc, patchSResource, patchInfo)
-		defer oc.AsAdmin().WithoutNamespace().Run("patch").Args(patchSResource, "-p", `[{"op": "remove", "path": "/spec/additionalNetworks"}]`, "--type=json").Execute()
+			g.By("Configure network-attach-definition through network operator")
+			patchResourceAsAdmin(oc, patchSResource, patchInfo)
+			defer oc.AsAdmin().WithoutNamespace().Run("patch").Args(patchSResource, "-p", `[{"op": "remove", "path": "/spec/additionalNetworks"}]`, "--type=json").Execute()
 
-		//Testing will exit when network operator is in abnormal state during 60 seconding of checking operator.
-		g.By("Check NetworkOperatorStatus")
-		checkNetworkOperatorState(oc, 10, 60)
+			//Testing will exit when network operator is in abnormal state during 60 seconding of checking operator.
+			g.By("Check NetworkOperatorStatus")
+			checkNetworkOperatorState(oc, 10, 60)
 
-		g.By("Delete the namespace")
-		nsErr := oc.AsAdmin().Run("delete").Args("project", namespace, "--ignore-not-found").Execute()
-		o.Expect(nsErr).NotTo(o.HaveOccurred())
+			g.By("Delete the namespace")
+			nsErr := oc.AsAdmin().Run("delete").Args("project", namespace, "--ignore-not-found").Execute()
+			o.Expect(nsErr).NotTo(o.HaveOccurred())
 
-		//Testing will exit when network operator is in abnormal state during 60 seconding of checking operator.
-		g.By("Check NetworkOperatorStatus after deleting namespace")
-		checkNetworkOperatorState(oc, 10, 60)
-	})
+			//Testing will exit when network operator is in abnormal state during 60 seconding of checking operator.
+			g.By("Check NetworkOperatorStatus after deleting namespace")
+			checkNetworkOperatorState(oc, 10, 60)
+		})
+	*/
 
 	// author: weliang@redhat.com
 	g.It("NonHyperShiftHOST-Author:weliang-High-57589-Whereabouts CNI timesout while iterating exclude range", func() {
