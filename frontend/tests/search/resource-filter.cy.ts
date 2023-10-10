7c2e9769a4bd9d7b358fd995602a8696f1811cd4
Klein Zhao <klzhao@redhat.com>
2023-10-10 12:49:06 +0800

    automate OCP-67717 (#11900)
    * fix OCP-52366
    
    tune
    
    tune
    
    tune
    
    .
    
    tune
    
    * automate OCP-67717
    
    tune
    
    tune

---
+++ frontend/tests/search/resource-filter.cy.ts
@@ -0,0 +1,25 @@
+import { guidedTour } from "upstream/views/guided-tour";
+import { searchPage } from "views/search";
+
+describe('show shortname in console resourse badge', () => {
+  before(() => {
+    cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+    cy.adminCLI(`oc create -f ./fixtures/fence-agents-remediation.medik8s.io_fenceagentsremediationtemplates.yaml`)
+    cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
+    guidedTour.close();
+  })
+
+  after(() => {
+    cy.adminCLI(`oc delete -f ./fixtures/fence-agents-remediation.medik8s.io_fenceagentsremediationtemplates.yaml`)
+    cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
+
+  })
+
+  it('(OCP-67717, klzhao)Correct shortname in console resource barge', {tags:['e2e','admin','@rosa','@osd-ccs']}, () => {
+    searchPage.navToSearchPage();
+    cy.get('button[aria-label="Options menu"]').click();
+    cy.get('input[type="search"]').clear().type(`far`);
+    cy.get(`.co-m-resource-icon.co-m-resource-fenceagentsremediationtemplate`)
+      .should('have.text', 'FAR');
+  })
+})
