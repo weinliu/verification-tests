3c86f2946c07bc9879f4815dd87db4edcee1a16e
Jad Haj Yahya <jhajyahy@redhat.com>
2023-12-06 03:42:09 +0200

    Adding BM related tests (#13242)
    * Adding BM related tests
    
    * Remove commented line
    
    * gofmt format fix

--- test/extended/installer/baremetal/deployment_sanity.go
+++ test/extended/installer/baremetal/deployment_sanity.go
@@ -1,17 +1,25 @@
 package baremetal
 
 import (
+	"fmt"
+	"strings"
+
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
 
+const maxCpuUsageAllowed float64 = 90.0
+
 var _ = g.Describe("[sig-baremetal] INSTALLER IPI on BareMetal", func() {
 	defer g.GinkgoRecover()
 	var (
-		oc           = exutil.NewCLI("baremetal-deployment-sanity", exutil.KubeConfigPath())
-		iaasPlatform string
+		oc               = exutil.NewCLI("baremetal-deployment-sanity", exutil.KubeConfigPath())
+		iaasPlatform     string
+		goodPodStates    = []string{"Running", "Succeeded", "Completed", "NodeAffinity"}
+		warningPodStates = []string{"Pending", "Terminating"}
+		issueReported    bool
 	)
 	g.BeforeEach(func() {
 		exutil.SkipForSNOCluster(oc)
@@ -57,5 +65,97 @@ var _ = g.Describe("[sig-baremetal] INSTALLER IPI on BareMetal", func() {
 		podName := getPodName(oc, ns32361)
 		podStatus := getPodStatus(oc, ns32361, podName)
 		o.Expect(podStatus).To(o.Equal("Running"))
+	})
+
+	// author: jhajyahy@redhat.com
+	g.It("Author:jhajyahy-Medium-32195-Verify that all pods in all namespaces are in good state", func() {
+		runningPods := []string{}
+		warnningPods := []string{}
+		failedPods := []string{}
+
+		g.By("Running oc get pods -A")
+		allNamespaces, err := oc.AsAdmin().Run("get").Args("namespaces", "-o=jsonpath={.items[*].metadata.name}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		nameSpaces := strings.Fields(allNamespaces)
+		for _, ns := range nameSpaces {
+			allPods, err := oc.AsAdmin().Run("get").Args("pods", "-n", ns, "-o=jsonpath={.items[*].metadata.name}").Output()
+			o.Expect(err).NotTo(o.HaveOccurred())
+			pods := strings.Fields(allPods)
+			for _, pod := range pods {
+				podStatus := getPodStatus(oc, ns, pod)
+				if stringInSlice(podStatus, goodPodStates) {
+					runningPods = append(runningPods, pod)
+				} else if stringInSlice(podStatus, warningPodStates) {
+					warnningPods = append(warnningPods, pod+"/"+ns)
+				} else {
+					failedPods = append(failedPods, pod+"/"+ns)
+				}
+			}
+		}
+
+		if len(runningPods) == 0 {
+			e2e.Logf("\nList of running pods is empty %s\n", runningPods)
+			issueReported = true
+		}
+		if len(failedPods) != 0 {
+			e2e.Logf("\nFailed pods are: %s\n", failedPods)
+			issueReported = true
+		}
+		if len(warnningPods) != 0 {
+			e2e.Logf("\nWarning pods are: %s\n", warnningPods)
+			issueReported = true
+		}
+		o.Expect(issueReported).NotTo(o.BeTrue())
+	})
+
+	// author: jhajyahy@redhat.com
+	g.It("Author:jhajyahy-Medium-34195-Verify all pods replicas are running on workers only", func() {
+		g.By("Create new namespace")
+		oc.SetupProject()
+		ns34195 := oc.Namespace()
+
+		g.By("Create deployment with num of workers + 1 replicas")
+		workerNodes, err := exutil.GetClusterNodesBy(oc, "worker")
+		o.Expect(err).NotTo(o.HaveOccurred())
+		replicasNum := len(workerNodes) + 1
+		deployCreationErr := oc.Run("create").Args("deployment", "deploy34195", "-n", ns34195, fmt.Sprintf("--replicas=%d", replicasNum), "--image", "quay.io/openshifttest/hello-openshift@sha256:4200f438cf2e9446f6bcff9d67ceea1f69ed07a2f83363b7fb52529f7ddd8a83").Execute()
+		o.Expect(deployCreationErr).NotTo(o.HaveOccurred())
+		waitForDeployStatus(oc, "deploy34195", ns34195, "True")
+
+		g.By("Check deployed pods number is as expected")
+		pods, err := oc.AsAdmin().Run("get").Args("pods", "-n", ns34195, "--field-selector=status.phase=Running", "-o=jsonpath={.items[*].metadata.name}").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		podList := strings.Fields(pods)
+		o.Expect(len(podList)).To(o.Equal(replicasNum))
+
+		g.By("Check pods are deployed on worker nodes only")
+		for _, pod := range podList {
+			podNodeName, err := exutil.GetPodNodeName(oc, ns34195, pod)
+			o.Expect(err).NotTo(o.HaveOccurred())
+			res := exutil.IsWorkerNode(oc, podNodeName)
+			if !res {
+				e2e.Logf("\nPod %s was deployed on non worker node  %s\n", pod, podNodeName)
+			}
+			o.Expect(res).To(o.BeTrue())
+		}
+	})
+
+	// author: jhajyahy@redhat.com
+	g.It("Author:jhajyahy-Medium-39126-Verify maximum CPU usage limit hasn't reached on each of the nodes", func() {
+		g.By("Running oc get nodes")
+		cpuExceededNodes := []string{}
+		sampling_time, err := getClusterUptime(oc)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		nodeNames, nodeErr := oc.AsAdmin().WithoutNamespace().Run("get").Args("nodes", "-o=jsonpath={.items[*].metadata.name}").Output()
+		o.Expect(nodeErr).NotTo(o.HaveOccurred(), "Failed to execute oc get nodes")
+		nodes := strings.Fields(nodeNames)
+		for _, node := range nodes {
+			cpuUsage := getNodeCpuUsage(oc, node, sampling_time)
+			if cpuUsage > maxCpuUsageAllowed {
+				cpuExceededNodes = append(cpuExceededNodes, node)
+				e2e.Logf("\ncpu usage of exceeded node: %s is %.2f%%", node, cpuUsage)
+			}
+		}
+		o.Expect(cpuExceededNodes).Should(o.BeEmpty(), "These nodes exceed max CPU usage allowed: %s", cpuExceededNodes)
 	})
 })
--- test/extended/installer/baremetal/utils.go
+++ test/extended/installer/baremetal/utils.go
@@ -2,7 +2,9 @@ package baremetal
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -11,6 +13,19 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
 )
+
+type Response struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric struct {
+				Instance string `json:"instance"`
+			} `json:"metric"`
+			Value []interface{} `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
 
 func checkOperatorsRunning(oc *exutil.CLI) (bool, error) {
 	jpath := `{range .items[*]}{.metadata.name}:{.status.conditions[?(@.type=='Available')].status}{':'}{.status.conditions[?(@.type=='Degraded')].status}{'\n'}{end}`
@@ -80,8 +95,51 @@ func getPodName(oc *exutil.CLI, ns string) string {
 }
 
 func getPodStatus(oc *exutil.CLI, namespace string, podName string) string {
-	podStatus, err := oc.WithoutNamespace().Run("get").Args("pod", "-n", namespace, podName, "-o=jsonpath={.status.phase}").Output()
+	podStatus, err := oc.AsAdmin().Run("get").Args("pod", "-n", namespace, podName, "-o=jsonpath={.status.phase}").Output()
 	o.Expect(err).NotTo(o.HaveOccurred())
-	e2e.Logf("The pod  %s status is %q", podName, podStatus)
+	e2e.Logf("The pod %s status is %q", podName, podStatus)
 	return podStatus
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func getNodeCpuUsage(oc *exutil.CLI, node string, sampling_time int) float64 {
+	samplingTime := strconv.Itoa(sampling_time)
+
+	cpu_sampling := "node_cpu_seconds_total%20%7Binstance%3D%27" + node
+	cpu_sampling += "%27%2C%20mode%3D%27idle%27%7D%5B5" + samplingTime + "m%5D"
+	query := "query=100%20-%20(avg%20by%20(instance)(irate(" + cpu_sampling + "))%20*%20100)"
+	url := "http://localhost:9090/api/v1/query?" + query
+
+	jsonString, err := oc.AsAdmin().WithoutNamespace().Run("exec").Args("-n", "openshift-monitoring", "-c", "prometheus", "prometheus-k8s-0", "--", "curl", "-s", url).Output()
+	o.Expect(err).NotTo(o.HaveOccurred())
+
+	var response Response
+	unmarshalErr := json.Unmarshal([]byte(jsonString), &response)
+	o.Expect(unmarshalErr).NotTo(o.HaveOccurred())
+	cpuUsage := response.Data.Result[0].Value[1].(string)
+	cpu_usage, err := strconv.ParseFloat(cpuUsage, 64)
+	o.Expect(err).NotTo(o.HaveOccurred())
+	return cpu_usage
+}
+
+func getClusterUptime(oc *exutil.CLI) (int, error) {
+	layout := "2006-01-02T15:04:05Z"
+	completionTime, _ := oc.AsAdmin().WithoutNamespace().Run("get").Args("clusterversion", "-o=jsonpath={.items[*].status.history[*].completionTime}").Output()
+	returnTime, perr := time.Parse(layout, completionTime)
+	if perr != nil {
+		e2e.Logf("Error trying to parse uptime %s", perr)
+		return 0, perr
+	}
+	now := time.Now()
+	uptime := now.Sub(returnTime)
+	uptimeByMin := int(uptime.Minutes())
+	return uptimeByMin, nil
 }
