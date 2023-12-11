855ee6930c2ddc673aa966b78ee72bd5ea139e22
Tristan Radisson <tradisso@redhat.com>
2023-12-11 04:45:58 +0100

    [OCM-5108] Removed useless testing (#13253)
    * [OCM-5108] Removed useless testing
    
    * update test

--- test/extended/rosacli/test_rosacli_provision.go
+++ test/extended/rosacli/test_rosacli_provision.go
@@ -9,9 +9,6 @@ import (
 var _ = g.Describe("[sig-rosacli] Service_Development_A rosa create cluster with admin negative testing", func() {
 	defer g.GinkgoRecover()
 	var (
-		// To set back once OCM-5108 is solved
-		// invalidUser     = "ad/min" // disallowed character
-		validUser       = "admin"
 		invalidPassword = "password1" // disallowed password
 		validPassword   = "Th3long,validpassword"
 		clusterID       string
@@ -21,45 +18,24 @@ var _ = g.Describe("[sig-rosacli] Service_Development_A rosa create cluster with
 		rosaClient := rosacli.NewClient()
 		clusterID = "fake-cluster" // these tests do not create or use a real cluster so no need to address an existing one.
 
-		// To set back once OCM-5108 is solved
-		// g.By("Try to create classic non STS cluster with invalid admin username")
-		// output, err := rosaClient.Cluster.CreateDryRun(clusterID, "--create-admin-user", invalidUser,
-		// 	"--cluster-admin-password", validPassword, "--region", "us-east-2",
-		// 	"--mode", "auto", "-y")
-		// fmt.Println(output)
-		// textData := rosaClient.Parser.TextData.Input(output).Parse().Tip()
-		// o.Expect(err).To(o.HaveOccurred())
-		// o.Expect(textData).Should(o.ContainSubstring("sername must not contain"))
-
 		g.By("Try to create classic non STS cluster with invalid admin password")
-		output, err := rosaClient.Cluster.CreateDryRun(clusterID, "--create-admin-user", validUser,
-			"--cluster-admin-password", invalidPassword, "--region", "us-east-2",
-			"--mode", "auto", "-y")
+		output, err := rosaClient.Cluster.CreateDryRun(clusterID, "--cluster-admin-password", invalidPassword,
+			"--region", "us-east-2", "--mode", "auto", "-y")
 		textData := rosaClient.Parser.TextData.Input(output).Parse().Tip()
 		o.Expect(err).To(o.HaveOccurred())
 		o.Expect(textData).Should(o.ContainSubstring("assword must be at least"))
 
-		// To set back once OCM-5108 is solved
-		// g.By("Try to create cluster with invalid admin username on classic STS cluster")
-		// output, err = rosaClient.Cluster.CreateDryRun(clusterID, "--sts", "--create-admin-user", invalidUser,
-		// 	"--cluster-admin-password", validPassword, "--region", "us-east-2",
-		// 	"--mode", "auto", "-y")
-		// textData = rosaClient.Parser.TextData.Input(output).Parse().Tip()
-		// o.Expect(err).To(o.HaveOccurred())
-		// o.Expect(textData).Should(o.ContainSubstring("sername must not contain"))
-
 		g.By("Try to create cluster with invalid admin password on classic STS cluster")
-		output, err = rosaClient.Cluster.CreateDryRun(clusterID, "--sts", "--create-admin-user", validUser,
-			"--cluster-admin-password", invalidPassword, "--region", "us-east-2",
-			"--mode", "auto", "-y")
+		output, err = rosaClient.Cluster.CreateDryRun(clusterID, "--sts", "--cluster-admin-password", invalidPassword,
+			"--region", "us-east-2", "--mode", "auto", "-y")
 		textData = rosaClient.Parser.TextData.Input(output).Parse().Tip()
 		o.Expect(err).To(o.HaveOccurred())
 		o.Expect(textData).Should(o.ContainSubstring("assword must be at least"))
 
 		g.By("Try to create Hypershift cluster with admin username and password set (unsupported)")
-		output, err = rosaClient.Cluster.CreateDryRun(clusterID, "--hosted-cp", "--create-admin-user", validUser,
-			"--cluster-admin-password", validPassword, "--region", "us-west-2", "--support-role-arn", "--controlplane-iam-role",
-			"--worker-iam-role", "--mode", "auto", "-y")
+		output, err = rosaClient.Cluster.CreateDryRun(clusterID, "--hosted-cp", "--cluster-admin-password", validPassword,
+			"--region", "us-west-2", "--support-role-arn", "--controlplane-iam-role", "--worker-iam-role",
+			"--mode", "auto", "-y")
 		textData = rosaClient.Parser.TextData.Input(output).Parse().Tip()
 		o.Expect(err).To(o.HaveOccurred())
 		o.Expect(textData).Should(o.ContainSubstring("is only supported in classic"))
