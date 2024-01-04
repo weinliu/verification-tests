611b9c14777c34d6604cb1ed0862d42b7ee3e8d1
Amogh Rameshappa Devapura <aramesha@redhat.com>
2024-01-03 20:20:46 -0500

    Ingress operator dashboard (#13849)

---
+++ frontend/tests/netobserv/Ingress_dashboard.cy.ts
@@ -0,0 +1,49 @@
+import { dashboard } from "views/dashboards-page"
+
+const bytesHTTP = [
+    "incoming-bytes-chart",
+    "outgoing-bytes-chart",
+    // No metrics found for the below graph
+    //"http-server-response-error-rate-chart",
+    "average-http-server-response-latency-(ms)-chart"
+]
+
+describe('NETOBSERV networking dashboards tests', { tags: ['NETOBSERV'] }, function () {
+
+    before('any test', function () {
+        cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'))
+        cy.switchPerspective('Administrator');
+    })
+
+    it('(OCP-69946, aramesha), should have ingress operator dashboards', function () {
+        // navigate to 'Networking / Ingress' Dashboard page
+        dashboard.visit()
+        dashboard.visitDashboard("grafana-dashboard-ingress-operator")
+
+        // verify that 'Current Total Incoming Bandwidth', 'Current Total Outgoing Bandwidth', 'HTTP Error Rate' and 'HTTP Server Average Response Latency' panels exist and are populated
+        var ingressPanels: string[] = ['current-total-incoming-bandwidth-chart', 'current-total-outgoing-bandwidth-chart', 'http-error-rate-chart', 'http-server-average-response-latency-chart']
+        cy.checkDashboards(ingressPanels)
+
+        // verify 'Top 10 Per Route' bytes and HTTP panels exists and are populated
+        cy.byLegacyTestID('panel-top-10-per-route').should('exist').within(routes => {
+            cy.checkDashboards(bytesHTTP)
+        })
+
+        // verify 'Top 10 Per Namespace' bytes and HTTP panels exists and are populated
+        cy.byLegacyTestID('panel-top-10-per-namespace').should('exist').within(namespace => {
+            cy.checkDashboards(bytesHTTP)
+        })
+
+        // verify 'Top 10 Per Shard' bytes and HTTP panels exists and are populated
+        cy.byLegacyTestID('panel-top-10-per-shard').should('exist').within(shard => {
+            let bytesHTTPRoutes = bytesHTTP.concat('number-of-routes-chart')
+            cy.checkDashboards(bytesHTTPRoutes)
+        })
+    })
+
+    after("all tests", function () {
+        cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`)
+        cy.uiLogout()
+    })
+})
