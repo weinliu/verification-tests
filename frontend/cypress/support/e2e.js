9f3593c4c52df59ece1605d28cb41274b40ba3d3
Mehul Modi <memodi@redhat.com>
2024-01-11 03:21:52 -0500

    workaround for mobx error (#14055)

--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -3,3 +3,14 @@ import './commands'
 
 const registerCypressGrep = require('@cypress/grep')
 registerCypressGrep()
+
+// remove this when NETOBSERV-1450 is resolved
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a different versions of MobX active error with 4.15 
+    // and don't want to fail the test so we return false
+    if (err.message.includes('different versions of MobX active')) {
+        return false
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+})
