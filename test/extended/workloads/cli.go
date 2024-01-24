068d6e9b5bd4630972d3fd6fdf04e1ec027ef0dd
Ying Zhou <yinzhou@redhat.com>
2024-01-24 18:31:06 +0800

    add skip for 11202 (#14299)

--- test/extended/workloads/cli.go
+++ test/extended/workloads/cli.go
@@ -1987,6 +1987,9 @@ var _ = g.Describe("[sig-cli] Workloads client test", func() {
 
 	// author: yinzhou@redhat.com
 	g.It("ROSA-OSD_CCS-ARO-ConnectedOnly-Author:yinzhou-Low-11202-Use oc explain to see detailed documentation of resources", func() {
+		if isBaselineCapsSet(oc, "None") || isBaselineCapsSet(oc, "v4.13") || isBaselineCapsSet(oc, "v4.12") || isBaselineCapsSet(oc, "v4.14") || isBaselineCapsSet(oc, "v4.15") && !isEnabledCapability(oc, "DeploymentConfig") {
+			g.Skip("Skipping the test as baselinecaps have been set and some of API capabilities are not enabled!")
+		}
 		g.By("Check if baremetal cluster")
 		iaasPlatform := exutil.CheckPlatform(oc)
 		if iaasPlatform == "baremetal" {
