b53624cfebdcc9fd8fad1db946c83fb9bcd4d064
Ya Dan Pei <yapei@redhat.com>
2024-01-30 13:51:16 +0800

    update managed service test scripts (#14502)
    * update managed service test scripts
    
    * rename

---
+++ frontend/console-test-managed-service.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
+cp -L $KUBECONFIG /tmp/kubeconfig && export CYPRESS_KUBECONFIG_PATH=/tmp/kubeconfig
+mkdir -p $ARTIFACT_DIR
+SCREENSHOTS_DIR=gui_test_screenshots
+source "${SHARED_DIR}/runtime_env"
+
+function copyArtifacts {
+    yarn merge-reports
+    python3 parse-xml.py
+    if [ -d "$ARTIFACT_DIR" ] && [ -d "$SCREENSHOTS_DIR" ]; then
+        echo "Copying artifacts from $(pwd)..."
+        cp console-cypress.xml "$SCREENSHOTS_DIR"
+        cp -r "$SCREENSHOTS_DIR" "${ARTIFACT_DIR}/gui_test_screenshots"
+    fi
+}
+
+trap copyArtifacts EXIT
+
+# clone upstream console repo and create soft link
+set -x
+git clone -b master --depth=1 https://github.com/openshift/console.git upstream_console && cd upstream_console/frontend && yarn install
+cd ../../
+ln -s ./upstream_console/frontend/packages/integration-tests-cypress upstream
+
+# in frontend dir, install deps and trigger tests
+yarn install
+
+# trigger tests
+set +x
+console_route=$(oc get route console -n openshift-console -o jsonpath='{.spec.host}')
+idp_name=$(oc get oauths.config.openshift.io cluster -o jsonpath='{.spec.identityProviders[-1].name}')
+export CYPRESS_BASE_URL=https://$console_route
+export CYPRESS_LOGIN_IDP=$idp_name
+export CYPRESS_LOGIN_USERNAME=$(echo ${USERS} | awk -F ',' '{print $1}' | awk -F ':' '{print $1}')
+export CYPRESS_LOGIN_PASSWORD=$(echo ${USERS} | awk -F ',' '{print $1}' | awk -F ':' '{print $2}')
+export CYPRESS_LOGIN_UP_PAIR=${USERS}
+export NO_COLOR=1
+ls -ltr
+echo "triggering tests"
+set -x
+if [[ ${SHARED_DIR}/cluster-type == 'rosa' ]] ; then
+  yarn run test-cypress-console-rosa
+elif [[ ${SHARED_DIR}/cluster-type == 'osd' ]]; then
+  yarn run test-cypress-console-osd
+fi
