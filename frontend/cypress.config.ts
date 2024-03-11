02659bba62cf0452619e603b66ac136eb595d793
Ya Dan Pei <yapei@redhat.com>
2024-03-11 16:31:44 +0800

    only save video for failed cases (#15241)

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -83,6 +83,21 @@ export default defineConfig({
           });
         });
       });
+      on(
+        'after:spec',
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === 'failed')
+            )
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video)
+            }
+          }
+        }
+      );
       require('@cypress/grep/src/plugin')(config);
       return config;
     },
