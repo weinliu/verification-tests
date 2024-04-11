88728c0469de239d5329656a9a709b330eba3a55
Melvin Joseph <mjoseph@redhat.com>
2024-04-11 09:06:01 +0530

    Migrating wildcardPolicy test case to ginkgo - OCPQE-20179 (#15864)
    * Migrating wildcardPolicy test case to ginkgo - OCPQE-20179
    
    * Address the review comments

--- test/extended/router/route-admission.go
+++ test/extended/router/route-admission.go
@@ -2,11 +2,12 @@ package router
 
 import (
 	"fmt"
+	"os/exec"
+	"path/filepath"
+
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
-	"os/exec"
-	"path/filepath"
 )
 
 var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
@@ -15,11 +16,11 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 	var oc = exutil.NewCLI("router-admission", exutil.KubeConfigPath())
 
 	// Test case creater: hongli@redhat.com
-	g.It("Author:mjoseph-Critical-27594-Set namespaceOwnership of routeAdmission to InterNamespaceAllowed", func() {
+	g.It("ROSA-OSD_CCS-ARO-Author:mjoseph-Critical-27594-Set namespaceOwnership of routeAdmission to InterNamespaceAllowed", func() {
 		var (
 			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
 			testPodSvc          = filepath.Join(buildPruningBaseDir, "web-server-rc.yaml")
-			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np-ra.yaml")
+			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np.yaml")
 			srvrcInfo           = "web-server-rc"
 			srvName             = "service-unsecure"
 			e2eTestNamespace2   = "e2e-ne-ocp27594-" + getRandomString()
@@ -41,6 +42,10 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 		exutil.By("2. Create a custom ingresscontroller")
 		ingctrl.domain = ingctrl.name + "." + getBaseDomain(oc)
+		// Updating namespaceOwnership as 'InterNamespaceAllowed' in the yaml file
+		sedCmd := fmt.Sprintf(`sed -i'' -e 's|Strict|%s|g' %s`, "InterNamespaceAllowed", customTemp)
+		_, err := exec.Command("bash", "-c", sedCmd).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
 		defer ingctrl.delete(oc)
 		ingctrl.create(oc)
 		ensureRouterDeployGenerationIs(oc, ingctrl.name, "1")
@@ -52,7 +57,7 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 		exutil.By("4. Create a server pod and an unsecure service in one ns")
 		createResourceFromFile(oc, e2eTestNamespace1, testPodSvc)
-		err := waitForPodWithLabelReady(oc, e2eTestNamespace1, "name="+srvrcInfo)
+		err = waitForPodWithLabelReady(oc, e2eTestNamespace1, "name="+srvrcInfo)
 		exutil.AssertWaitPollNoErr(err, "server pod failed to be ready state within allowed time in the first ns!")
 
 		exutil.By("5. Create a server pod and an unsecure service in the other ns")
@@ -82,11 +87,11 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 	})
 
 	// Test case creater: hongli@redhat.com
-	g.It("Author:mjoseph-Critical-27595-Set namespaceOwnership of routeAdmission to Strict", func() {
+	g.It("ROSA-OSD_CCS-ARO-Author:mjoseph-Critical-27595-Set namespaceOwnership of routeAdmission to Strict", func() {
 		var (
 			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
 			testPodSvc          = filepath.Join(buildPruningBaseDir, "web-server-rc.yaml")
-			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np-ra.yaml")
+			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np.yaml")
 			srvrcInfo           = "web-server-rc"
 			srvName             = "service-unsecure"
 			e2eTestNamespace2   = "e2e-ne-ocp27595-" + getRandomString()
@@ -108,10 +113,6 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 		exutil.By("2. Create a custom ingresscontroller")
 		ingctrl.domain = ingctrl.name + "." + getBaseDomain(oc)
-		// Updating namespaceOwnership as 'Strict' in the yaml file
-		sedCmd := fmt.Sprintf(`sed -i'' -e 's|InterNamespaceAllowed|%s|g' %s`, "Strict", customTemp)
-		_, err := exec.Command("bash", "-c", sedCmd).Output()
-		o.Expect(err).NotTo(o.HaveOccurred())
 		defer ingctrl.delete(oc)
 		ingctrl.create(oc)
 		ensureRouterDeployGenerationIs(oc, ingctrl.name, "1")
@@ -124,7 +125,7 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 		exutil.By("4. Create a server pod and an unsecure service in one ns")
 		createResourceFromFile(oc, e2eTestNamespace1, testPodSvc)
-		err = waitForPodWithLabelReady(oc, e2eTestNamespace1, "name="+srvrcInfo)
+		err := waitForPodWithLabelReady(oc, e2eTestNamespace1, "name="+srvrcInfo)
 		exutil.AssertWaitPollNoErr(err, "server pod failed to be ready state within allowed time in the first ns!")
 
 		exutil.By("5. Create a server pod and an unsecure service in the other ns")
@@ -154,7 +155,7 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 
 	// Test case creater: hongli@redhat.com
 	// For OCP-27596 and OCP-27605
-	g.It("Author:mjoseph-Critical-27596-Update the namespaceOwnership of routeAdmission", func() {
+	g.It("ROSA-OSD_CCS-ARO-Author:mjoseph-Critical-27596-Update the namespaceOwnership of routeAdmission", func() {
 		var (
 			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
 			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np.yaml")
@@ -177,21 +178,21 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		namespaceOwnershipEnv := readRouterPodEnv(oc, routerpod, "ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK")
 		o.Expect(namespaceOwnershipEnv).To(o.ContainSubstring("ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK=false"))
 
-		exutil.By("3. Patch the custom ingress controller and set namespaceOwnership to Strict")
-		patchResourceAsAdmin(oc, ingctrl.namespace, "ingresscontrollers/"+ingctrl.name, "{\"spec\":{\"routeAdmission\":{\"namespaceOwnership\":\"Strict\"}}}")
-
-		exutil.By("4. Check the ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK env variable, which should be false")
-		namespaceOwnershipEnv = readRouterPodEnv(oc, routerpod, "ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK")
-		o.Expect(namespaceOwnershipEnv).To(o.ContainSubstring("ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK=false"))
-
-		exutil.By("5. Patch the custom ingress controller and set namespaceOwnership to InterNamespaceAllowed")
+		exutil.By("3. Patch the custom ingress controller and set namespaceOwnership to InterNamespaceAllowed")
 		patchResourceAsAdmin(oc, ingctrl.namespace, "ingresscontrollers/"+ingctrl.name, "{\"spec\":{\"routeAdmission\":{\"namespaceOwnership\":\"InterNamespaceAllowed\"}}}")
 		ensureRouterDeployGenerationIs(oc, ingctrl.name, "2")
 
-		exutil.By("6. Check the ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK env variable, which should be true")
+		exutil.By("4. Check the ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK env variable, which should be true")
 		newRouterpod := getNewRouterPod(oc, ingctrl.name)
 		namespaceOwnershipEnv = readRouterPodEnv(oc, newRouterpod, "ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK")
 		o.Expect(namespaceOwnershipEnv).To(o.ContainSubstring("ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK=true"))
+
+		exutil.By("5. Patch the custom ingress controller and set namespaceOwnership to Strict")
+		patchResourceAsAdmin(oc, ingctrl.namespace, "ingresscontrollers/"+ingctrl.name, "{\"spec\":{\"routeAdmission\":{\"namespaceOwnership\":\"Strict\"}}}")
+
+		exutil.By("6. Check the ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK env variable, which should be false")
+		namespaceOwnershipEnv = readRouterPodEnv(oc, routerpod, "ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK")
+		o.Expect(namespaceOwnershipEnv).To(o.ContainSubstring("ROUTER_DISABLE_NAMESPACE_OWNERSHIP_CHECK=false"))
 
 		exutil.By("7. Patch the custom ingress controller and set namespaceOwnership to Null")
 		patchResourceAsAdmin(oc, ingctrl.namespace, "ingresscontrollers/"+ingctrl.name, "{\"spec\":{\"routeAdmission\":{\"namespaceOwnership\":null}}}")
@@ -206,5 +207,115 @@ var _ = g.Describe("[sig-network-edge] Network_Edge should", func() {
 		exutil.By("9. Patch the custom ingress controller and set namespaceOwnership to a invalid string like 'InvalidTest'")
 		output, _ := oc.AsAdmin().WithoutNamespace().Run("patch").Args("ingresscontroller/"+ingctrl.name, "-p", "{\"spec\":{\"routeAdmission\":{\"namespaceOwnership\":\"InvalidTest\"}}}", "--type=merge", "-n", ingctrl.namespace).Output()
 		o.Expect(output).To(o.ContainSubstring("spec.routeAdmission.namespaceOwnership: Unsupported value: \"InvalidTest\": supported values: \"InterNamespaceAllowed\", \"Strict\""))
+	})
+
+	// Test case creater: hongli@redhat.com
+	g.It("ROSA-OSD_CCS-ARO-Author:mjoseph-Critical-30190-Set wildcardPolicy of routeAdmission to WildcardsAllowed", func() {
+		var (
+			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
+			testPodSvc          = filepath.Join(buildPruningBaseDir, "web-server-rc.yaml")
+			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np.yaml")
+			srvName             = "service-unsecure"
+			ingctrl             = ingressControllerDescription{
+				name:      "ocp30190",
+				namespace: "openshift-ingress-operator",
+				domain:    "",
+				template:  customTemp,
+			}
+		)
+		exutil.By("1. Create a custom ingresscontroller")
+		project1 := oc.Namespace()
+		ingctrl.domain = ingctrl.name + "." + getBaseDomain(oc)
+		// Updating wildcardPolicy as 'WildcardsAllowed' in the yaml file
+		sedCmd := fmt.Sprintf(`sed -i'' -e 's|WildcardsDisallowed|%s|g' %s`, "WildcardsAllowed", customTemp)
+		_, err := exec.Command("bash", "-c", sedCmd).Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		defer ingctrl.delete(oc)
+		ingctrl.create(oc)
+		ensureRouterDeployGenerationIs(oc, ingctrl.name, "1")
+		routehost := "wildcard." + project1 + "." + ingctrl.domain
+		anyhost := "any." + project1 + "." + ingctrl.domain
+		custContPod := getNewRouterPod(oc, ingctrl.name)
+
+		exutil.By("2. Check the ROUTER_ALLOW_WILDCARD_ROUTES env variable, which should be true")
+		namespaceOwnershipEnv := readRouterPodEnv(oc, custContPod, "ROUTER_ALLOW_WILDCARD_ROUTES")
+		o.Expect(namespaceOwnershipEnv).To(o.ContainSubstring("ROUTER_ALLOW_WILDCARD_ROUTES=true"))
+
+		exutil.By("3. Create a server pod and an unsecure service")
+		createResourceFromFile(oc, project1, testPodSvc)
+		err = waitForPodWithLabelReady(oc, project1, "name=web-server-rc")
+		exutil.AssertWaitPollNoErr(err, "the pod with name=web-server-rc, Ready status not met")
+
+		exutil.By("4. Expose a http wildcard route")
+		err = oc.WithoutNamespace().Run("expose").Args("service", srvName, "--hostname="+routehost, "-n", project1, "--wildcard-policy=Subdomain").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		getRoutes(oc, project1)
+
+		exutil.By("5. Check the reachability of the wildcard route")
+		ingressContPod := getPodName(oc, "openshift-ingress-operator", "name=ingress-operator")
+		iplist := getPodIP(oc, "openshift-ingress", custContPod)
+		curlCmd := fmt.Sprintf("curl --resolve %s:80:%s http://%s -I -k --connect-timeout 10", routehost, iplist[0], routehost)
+		statsOut, err := exutil.RemoteShPod(oc, "openshift-ingress-operator", ingressContPod[0], "sh", "-c", curlCmd)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(statsOut).Should(o.ContainSubstring("HTTP/1.1 200 OK"))
+
+		exutil.By("6. Check the reachability of the test route")
+		curlCmd = fmt.Sprintf("curl --resolve %s:80:%s http://%s -I -k --connect-timeout 10", anyhost, iplist[0], anyhost)
+		statsOut, err = exutil.RemoteShPod(oc, "openshift-ingress-operator", ingressContPod[0], "sh", "-c", curlCmd)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(statsOut).Should(o.ContainSubstring("HTTP/1.1 200 OK"))
+	})
+
+	// Test case creater: hongli@redhat.com
+	// For OCP-30191 and OCP-30192
+	g.It("ROSA-OSD_CCS-ARO-Author:mjoseph-Medium-30191-Set wildcardPolicy of routeAdmission to WildcardsDisallowed", func() {
+		var (
+			buildPruningBaseDir = exutil.FixturePath("testdata", "router")
+			testPodSvc          = filepath.Join(buildPruningBaseDir, "web-server-rc.yaml")
+			customTemp          = filepath.Join(buildPruningBaseDir, "ingresscontroller-np.yaml")
+			srvName             = "service-unsecure"
+			ingctrl             = ingressControllerDescription{
+				name:      "ocp30191",
+				namespace: "openshift-ingress-operator",
+				domain:    "",
+				template:  customTemp,
+			}
+		)
+		exutil.By("1. Create a custom ingresscontroller")
+		project1 := oc.Namespace()
+		ingctrl.domain = ingctrl.name + "." + getBaseDomain(oc)
+		defer ingctrl.delete(oc)
+		ingctrl.create(oc)
+		ensureRouterDeployGenerationIs(oc, ingctrl.name, "1")
+		routehost := "wildcard." + project1 + "." + ingctrl.domain
+		custContPod := getNewRouterPod(oc, ingctrl.name)
+
+		exutil.By("2. Check the ROUTER_ALLOW_WILDCARD_ROUTES env variable, which should be false")
+		namespaceOwnershipEnv := readRouterPodEnv(oc, custContPod, "ROUTER_ALLOW_WILDCARD_ROUTES")
+		o.Expect(namespaceOwnershipEnv).To(o.ContainSubstring("ROUTER_ALLOW_WILDCARD_ROUTES=false"))
+
+		exutil.By("3. Create a server pod and an unsecure service")
+		createResourceFromFile(oc, project1, testPodSvc)
+		err := waitForPodWithLabelReady(oc, project1, "name=web-server-rc")
+		exutil.AssertWaitPollNoErr(err, "the pod with name=web-server-rc, Ready status not met")
+
+		exutil.By("4. Expose a http wildcard route")
+		err = oc.WithoutNamespace().Run("expose").Args("service", srvName, "--hostname="+routehost, "-n", project1, "--wildcard-policy=Subdomain").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		getRoutes(oc, project1)
+
+		exutil.By("5. Confirm the route status is 'RouteNotAdmitted' and confirm the route is not accessible")
+		getNamespaceRouteDetails(oc, project1, "service-unsecure", ".status.ingress[?(@.routerName==\"wildcard\")].conditions[*].reason", "RouteNotAdmitted", true)
+		ingressContPod := getPodName(oc, "openshift-ingress-operator", "name=ingress-operator")
+		iplist := getPodIP(oc, "openshift-ingress", custContPod)
+		curlCmd := fmt.Sprintf("curl --resolve %s:80:%s http://%s -I -k --connect-timeout 10", routehost, iplist[0], routehost)
+		statsOut, err := exutil.RemoteShPod(oc, "openshift-ingress-operator", ingressContPod[0], "sh", "-c", curlCmd)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(statsOut).Should(o.ContainSubstring("HTTP/1.0 503 Service Unavailable"))
+
+		// Incorporating the negative case OCP-30192 here
+		exutil.By("6. Patch the custom ingress controller and set wildcardPolicy to a invalid string like 'unknown'")
+		output, _ := oc.AsAdmin().WithoutNamespace().Run("patch").Args("ingresscontroller/"+ingctrl.name, "-p", "{\"spec\":{\"routeAdmission\":{\"wildcardPolicy\":\"unknown\"}}}", "--type=merge", "-n", ingctrl.namespace).Output()
+		o.Expect(output).To(o.ContainSubstring("spec.routeAdmission.wildcardPolicy: Unsupported value: \"unknown\": supported values: \"WildcardsAllowed\", \"WildcardsDisallowed\""))
 	})
 })
