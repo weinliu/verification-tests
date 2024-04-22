beb27ee787ac32cd807bd50812328f58d07a06cc
Xiyun Zhao <xiyuzhao@redhat.com>
2024-04-22 16:40:58 +0800

    Restructure the function of checkMetricsLoaded for OCP-33771 (#16079)
    Fix OCP-33771 unstable issue
      Restructure the function of checkMetricsLoaded, to make sure it is easy to maintain
      Remove the extra space in the file, to make sure the format is consistent

--- frontend/views/pods.ts
+++ frontend/views/pods.ts
@@ -1,123 +1,116 @@
 export namespace podsPageUtils {
-    export function setProjectPodNamesAlias(project, label, aliasPrefix, pod_label_key = "name") {
-      cy.visit(`/k8s/ns/${project}/pods`).get('[data-test-id="resource-title"]').should('be.visible')
-      cy.get('#content-scrollable').within(() => {
-        cy.get('button.pf-c-dropdown__toggle')
-          .should('have.text', 'Name')
-          .click()
-          .get('#LABEL-link')
-          .click()
-        cy.byLegacyTestID('item-filter')
-          .type(`${pod_label_key}=${label}`)
-          .get('span.co-text-node')
-          .contains(label)
-          .should('be.visible')
-          .click()
-        cy.get('tr > td[id=name]').find('a').each(($el, $index) => {
-          cy.wrap($el).invoke('text').as(`${aliasPrefix}_pod${$index}Name`)
-        })
-      })
-    }
-    export function setPodIPAlias(project, podName) {
-      cy.visit(`./k8s/ns/${project}/pods/${podName}`)
-        .byTestSelector('details-item-value__Pod IP')
+  export function setProjectPodNamesAlias(project, label, aliasPrefix, pod_label_key = "name") {
+    cy.visit(`/k8s/ns/${project}/pods`).get('[data-test-id="resource-title"]').should('be.visible')
+    cy.get('#content-scrollable').within(() => {
+      cy.get('button.pf-c-dropdown__toggle')
+        .should('have.text', 'Name')
+        .click()
+        .get('#LABEL-link')
+        .click()
+      cy.byLegacyTestID('item-filter')
+        .type(`${pod_label_key}=${label}`)
+        .get('span.co-text-node')
+        .contains(label)
         .should('be.visible')
-        .invoke('text')
-        .as(`${podName}IP`)
-    }
-  }
-  export const podsPage = {
-    goToPodsInAllNamespaces: () => {
-      cy.visit('/k8s/all-namespaces/pods');
-      cy.get('tr[data-test-rows="resource-row"]').should('exist');
-    },
-    goToPodsForGivenNamespace: (namespace: String) => {
-      cy.visit('/k8s/ns/'+namespace+'/pods');
-      cy.get('tr[data-test-rows="resource-row"]').should('exist');
-    },
-    // this is to make sure the page is loaded,
-    // the pods page is loaded when the columns are displayed hence checking for this condition
-    isLoaded: () => {
-      cy.get('tr[data-test-rows="resource-row"]').should('exist')
-    },
-    goToPodDetails: (namespace, podName) => {
-      cy.visit('/k8s/ns/'+namespace+'/pods/'+podName);
-    },
-    goToPodsMetricsTab: (namespace: string, podname: string) => {
-      cy.visit(`/k8s/ns/${namespace}/pods/${podname}/metrics`)
-    },
-    goToPodsLogTab: (namespace:string, podname: string) => {
-      cy.visit(`/k8s/ns/${namespace}/pods/${podname}/logs`)
-    },
-    checkContainerLastStateOnPodPage: (containerName, lastState) => {
-      cy.get(`div.row div:contains("${containerName}")`).siblings(`div:contains('${lastState}')`).should('exist');
-    },
-    checkContainerLastStateOnContainerPage: (lastState) => {
-      cy.get('dt:contains("Last State")').next('dd').should('contain',`${lastState}`);
-    }
-  }
-  export const podsMetricsTab ={
-    checkMetricsURL: (pos: number, chart: RegExp, chartdetails?: RegExp) =>{
-      cy.get('[aria-label="View in query browser"]')
-        .eq(pos)
-        .should('have.attr','href')
-        .and('match',chart)
-        .and('match',chartdetails)
-    },
-    clickToMetricsPage: (pos: number,chart: RegExp) => {
-      cy.get('[aria-label="View in query browser"]')
-        .eq(pos)
-        .should('have.attr','href')
-        .and('match',chart)
-        .then((href) => {
-          cy.visit(href)
+        .click()
+      cy.get('tr > td[id=name]').find('a').each(($el, $index) => {
+        cy.wrap($el).invoke('text').as(`${aliasPrefix}_pod${$index}Name`)
       })
-    },
-    checkMetricsLoaded: () => {
-      let retries = 0;
-      const checkMetrics = () => {
-        /* cy.get() is an asynchronous operation,but I returned a
-          boolean value before the loop completes, which leads to a
-          mixture of asynchronous and synchronous code.Cypress will return error.
-          So here I involed 'Promise'.
-          it can be used to resolve or reject when asynchronous operations are completed,
-          and they execute callbacks after the operation is completed.
-          This ensures that a result is not returned until all asynchronous
-          operations in the loop have completed */
-        return new Promise((resolve, reject) => {
-          let foundNoDatapoints = false;
-          cy.get('[aria-label="View in query browser"]').each(($el) => {
-            cy.wrap($el).should('not.contain','No datapoints found');
-            foundNoDatapoints = true ;
-          }).then(() => {
-            if (foundNoDatapoints) {
-              resolve(true);
-            }else {
-              resolve(false);
-            }
+    })
+  }
+  export function setPodIPAlias(project, podName) {
+    cy.visit(`./k8s/ns/${project}/pods/${podName}`)
+      .byTestSelector('details-item-value__Pod IP')
+      .should('be.visible')
+      .invoke('text')
+      .as(`${podName}IP`)
+  }
+}
+
+export const podsPage = {
+  goToPodsInAllNamespaces: () => {
+    cy.visit('/k8s/all-namespaces/pods');
+    cy.get('tr[data-test-rows="resource-row"]').should('exist');
+  },
+  goToPodsForGivenNamespace: (namespace: String) => {
+    cy.visit('/k8s/ns/'+namespace+'/pods');
+    cy.get('tr[data-test-rows="resource-row"]').should('exist');
+  },
+  // this is to make sure the page is loaded,
+  // the pods page is loaded when the columns are displayed hence checking for this condition
+  isLoaded: () => {
+    cy.get('tr[data-test-rows="resource-row"]').should('exist')
+  },
+  goToPodDetails: (namespace, podName) => {
+    cy.visit('/k8s/ns/'+namespace+'/pods/'+podName);
+  },
+  goToPodsMetricsTab: (namespace: string, podname: string) => {
+    cy.visit(`/k8s/ns/${namespace}/pods/${podname}/metrics`)
+  },
+  goToPodsLogTab: (namespace:string, podname: string) => {
+    cy.visit(`/k8s/ns/${namespace}/pods/${podname}/logs`)
+  },
+  checkContainerLastStateOnPodPage: (containerName, lastState) => {
+    cy.get(`div.row div:contains("${containerName}")`).siblings(`div:contains('${lastState}')`).should('exist');
+  },
+  checkContainerLastStateOnContainerPage: (lastState) => {
+    cy.get('dt:contains("Last State")').next('dd').should('contain',`${lastState}`);
+  }
+}
+
+export const podsMetricsTab ={
+  checkMetricsURL: (pos: number, chart: RegExp, chartdetails?: RegExp) =>{
+    cy.get('[aria-label="View in query browser"]')
+      .eq(pos)
+      .should('have.attr','href')
+      .and('match',chart)
+      .and('match',chartdetails)
+  },
+  clickToMetricsPage: (pos: number,chart: RegExp) => {
+    cy.get('[aria-label="View in query browser"]')
+      .eq(pos)
+      .should('have.attr','href')
+      .and('match',chart)
+      .then((href) => {
+        cy.visit(href)
+    })
+  },
+  checkMetricsLoaded: () => {
+    const maxRetries = 5;
+    const pageLoadingDelay = 20000; // 20 seconds
+    const retryDelay = 30000; //30 seconds
+    const textToCheck = 'no datapoints found';
+    const checkMetricsContent = ($metrics, retryCount = 0) => {
+      const checkNoDataPointsFound = $metrics.toArray().every(el => {
+        const metricText = Cypress.$(el).text().trim();
+        if (metricText === '') {
+          return false;
+        }
+        const containsText = !metricText.includes(textToCheck);
+        cy.log(`METRIC TEXT: ${metricText} contain "${textToCheck}": return ${containsText}`);
+        return containsText;
+      });
+
+      if (!checkNoDataPointsFound) {
+        if (retryCount === maxRetries) {
+          cy.log("Max retries exceeded, not all metrics/metric data loaded as expected");
+          throw new Error('Max retries exceeded, not all metrics/metric data loaded as expected');
+        }
+        cy.wait(retryDelay).then(() => {
+          cy.log("Not all chart data is ready, trigger page reload");
+          cy.reload();
+          cy.wait(pageLoadingDelay) // wait for page loading success
+          cy.get('[aria-label="View in query browser"]').should('have.length', 5).then(($els) => {
+            checkMetricsContent($els, retryCount + 1);
           });
         });
-      };
-      const reloadAndWait = () => {
-        cy.reload(true);
-        cy.wait(50000); //wait for 5 seconds
-      };
-      const retry = () => {
-        if (retries < 3) {
-          cy.log(`Wait Metrics load Retry times ${retries +1} ...`);
-          reloadAndWait();
-          checkMetrics().then((continueRetry) => {
-            retries++;
-            if (continueRetry) {
-              retry(); // if continueRetry = true, then retry
-            }
-          });
-        }
-      };
-      checkMetrics().then((continueRetry) => {
-        if(continueRetry) {
-          retry;
-        }
-      });
+      } else {
+        cy.log('All metrics Loaded. And chart do not contain "no datapoints found"');
+      }
     }
+    cy.wait(pageLoadingDelay) // wait for page loading success
+    cy.get('[aria-label="View in query browser"]').should('have.length', 5).then(($els) => {
+      checkMetricsContent($els);
+    });
   }
+}
