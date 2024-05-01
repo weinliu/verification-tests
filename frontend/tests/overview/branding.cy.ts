838f8df0a832b8ce519beae2c6e5d551273b4783
Yanping Zhang <yanpzhan@redhat.com>
2024-05-02 07:11:07 +0800

    improve ocp-48357 (#16340)

--- frontend/tests/overview/branding.cy.ts
+++ frontend/tests/overview/branding.cy.ts
@@ -3,17 +3,14 @@ import { Branding } from "../../views/branding";
 import { guidedTour } from '../../upstream/views/guided-tour';
 
 describe("Branding check", () => {
-  before(() => {
-    cy.login(Cypress.env("LOGIN_IDP"),Cypress.env("LOGIN_USERNAME"),Cypress.env("LOGIN_PASSWORD"));
-    guidedTour.close();
-  });
-
   it("(OCP-48357,yanpzhan,UserInterface) Switch the OCP branding logo and title to Red Hat OpenShift logo and title", {tags: ['e2e','@rosa']}, () => {
+    cy.visit('/');
+    Branding.checkLoginPageLogo();
+    cy.uiLogin(Cypress.env("LOGIN_IDP"),Cypress.env("LOGIN_USERNAME"),Cypress.env("LOGIN_PASSWORD"));
+    guidedTour.close();
     Overview.goToDashboard();
     Overview.checkUpperLeftLogo();
     Overview.toggleAbout();
     Branding.checkAboutModalLogo();
-    cy.uiLogout();
-    Branding.checkLoginPageLogo();
   });
 });
