9c0f1085f685a07991bae7408710857eb5c2e751
Keenon Lee <jitli@redhat.com>
2024-06-21 16:01:43 +0800

    Operator SDK switch to downstream 4.16 (#16373)

--- test/extended/operatorsdk/operatorsdk.go
+++ test/extended/operatorsdk/operatorsdk.go
@@ -30,7 +30,7 @@ var _ = g.Describe("[sig-operators] Operator_SDK should", func() {
 	var oc = exutil.NewCLIWithoutNamespace("default")
 	var ocpversion = "4.16"
 	var ocppreversion = "4.15"
-	var upstream = true
+	var upstream = false
 
 	// author: jfan@redhat.com
 	g.It("VMonly-Author:jfan-High-37465-SDK olm improve olm related sub commands", func() {
