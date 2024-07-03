570ecfd357880e3abc9e88d5beb16d6d24a34c2c
Yanping Zhang <yanpzhan@redhat.com>
2024-07-03 12:30:31 +0800

    update ocp-69183 (#17718)

--- frontend/tests/workloads/console.cy.ts
+++ frontend/tests/workloads/console.cy.ts
@@ -4,9 +4,7 @@ describe("console related deployments test", () => {
   it("(OCP-69183,yanpzhan,UserInterface) Set readOnlyRootFilesystem field for both console and console operator related containers", {tags: ['e2e','admin','@osd-ccs','@rosa']}, () => {
     // check console operator deployment
     Deployment.checkDeploymentFilesystem('console-operator','openshift-console-operator',0,true);
-    Deployment.checkDeploymentFilesystem('console-conversion-webhook','openshift-console-operator',0,true);
     Deployment.checkPodStatus('openshift-console-operator','name=console-operator','Running');
-    Deployment.checkPodStatus('openshift-console-operator','name=console-conversion-webhook','Running');
     // check console deployment
     Deployment.checkDeploymentFilesystem('console','openshift-console',0,false);
     Deployment.checkPodStatus('openshift-console','component=ui','Running');
