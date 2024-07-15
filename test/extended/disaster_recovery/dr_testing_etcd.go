19744809b22f6b39b4b2c6ae27a60566f2c97147
Ke Wang <kewang@redhat.com>
2024-07-15 14:53:44 +0800

    Remove LEVLE0 tag from case OCP-23803 (#18036)

--- test/extended/disaster_recovery/dr_testing_etcd.go
+++ test/extended/disaster_recovery/dr_testing_etcd.go
@@ -76,7 +76,7 @@ var _ = g.Describe("[sig-disasterrecovery] DR_Testing", func() {
 	})
 
 	// author: yinzhou@redhat.com
-	g.It("Author:yinzhou-LEVEL0-Longduration-NonPreRelease-Critical-23803-Restoring back to a previous cluster state in ocp v4 [Disruptive][Slow]", func() {
+	g.It("Author:yinzhou-Longduration-NonPreRelease-Critical-23803-Restoring back to a previous cluster state in ocp v4 [Disruptive][Slow]", func() {
 		g.By("check the platform is supported or not")
 		supportedList := []string{"aws", "gcp", "azure"}
 		support := in(iaasPlatform, supportedList)
