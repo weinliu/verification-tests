0a8f3177068aa096b517540c74e882db4c4ad4bf
Qiong Wang <qiowang@redhat.com>
2024-07-23 11:48:20 +0800

    update alerts cases (#18229)

--- test/extended/networking/ovn_sd_alerts.go
+++ test/extended/networking/ovn_sd_alerts.go
@@ -32,13 +32,11 @@ var _ = g.Describe("[sig-networking] SDN alerts", func() {
 		e2e.Logf("alertSeverity is %v", alertSeverity)
 		o.Expect(alertSeverity).To(o.ContainSubstring("critical"))
 
-		// https://issues.redhat.com/browse/OCPBUGS-18340 is minor bug, not sure when it will be fixed, disable below steps and wait for fix
-		/*
-			alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NoRunningOvnControlPlane\")].annotations.runbook_url}").Output()
-			o.Expect(runbookErr).NotTo(o.HaveOccurred())
-			e2e.Logf("The alertRunbook is %v", alertRunbook)
-			o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/NoRunningOvnControlPlane.md"))
-		*/
+		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NoRunningOvnControlPlane\")].annotations.runbook_url}").Output()
+		o.Expect(runbookErr).NotTo(o.HaveOccurred())
+		e2e.Logf("The alertRunbook is %v", alertRunbook)
+		o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/NoRunningOvnControlPlane.md"))
+
 	})
 
 	g.It("NonHyperShiftHOST-Author:weliang-Medium-51439-Upgrade NoOvnClusterManagerLeader to critical severity and inclue runbook.", func() {
@@ -52,27 +50,24 @@ var _ = g.Describe("[sig-networking] SDN alerts", func() {
 		e2e.Logf("alertSeverity is %v", alertSeverity)
 		o.Expect(alertSeverity).To(o.ContainSubstring("critical"))
 
-		// https://issues.redhat.com/browse/OCPBUGS-18340 is minor bug, not sure when it will be fixed, disable below steps and wait for fix
-		/*
-			alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NoOvnClusterManagerLeader\")].annotations.runbook_url}").Output()
-			o.Expect(runbookErr).NotTo(o.HaveOccurred())
-			e2e.Logf("The alertRunbook is %v", alertRunbook)
-			o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/NoOvnClusterManagerLeader.md"))
-		*/
+		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NoOvnClusterManagerLeader\")].annotations.runbook_url}").Output()
+		o.Expect(runbookErr).NotTo(o.HaveOccurred())
+		e2e.Logf("The alertRunbook is %v", alertRunbook)
+		o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/NoOvnClusterManagerLeader.md"))
 	})
 
 	g.It("NonHyperShiftHOST-Author:weliang-Medium-51722-Create runbook and link SOP for SouthboundStale alert", func() {
-		alertName, NameErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[*].alert}").Output()
+		alertName, NameErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "networking-rules", "-o=jsonpath={.spec.groups[*].rules[*].alert}").Output()
 		o.Expect(NameErr).NotTo(o.HaveOccurred())
 		e2e.Logf("The alertName is %v", alertName)
 		o.Expect(alertName).To(o.ContainSubstring("SouthboundStale"))
 
-		alertSeverity, severityErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NoOvnClusterManagerLeader\")].labels.severity}").Output()
+		alertSeverity, severityErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "networking-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"SouthboundStale\")].labels.severity}").Output()
 		o.Expect(severityErr).NotTo(o.HaveOccurred())
 		e2e.Logf("alertSeverity is %v", alertSeverity)
-		o.Expect(alertSeverity).To(o.ContainSubstring("critical"))
+		o.Expect(alertSeverity).To(o.ContainSubstring("warning"))
 
-		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"SouthboundStale\")].annotations.runbook_url}").Output()
+		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "networking-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"SouthboundStale\")].annotations.runbook_url}").Output()
 		o.Expect(runbookErr).NotTo(o.HaveOccurred())
 		e2e.Logf("The alertRunbook is %v", alertRunbook)
 		o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/SouthboundStaleAlert.md"))
@@ -113,17 +108,17 @@ var _ = g.Describe("[sig-networking] SDN alerts", func() {
 	})
 
 	g.It("NonHyperShiftHOST-Author:weliang-Medium-51723-bug 2094068 Create runbook and link SOP for NorthboundStale alert", func() {
-		alertName, NameErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[*].alert}").Output()
+		alertName, NameErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "networking-rules", "-o=jsonpath={.spec.groups[*].rules[*].alert}").Output()
 		o.Expect(NameErr).NotTo(o.HaveOccurred())
 		e2e.Logf("The alertName is %v", alertName)
 		o.Expect(alertName).To(o.ContainSubstring("NorthboundStale"))
 
-		alertSeverity, severityErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NorthboundStale\")].labels.severity}").Output()
+		alertSeverity, severityErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "networking-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NorthboundStale\")].labels.severity}").Output()
 		o.Expect(severityErr).NotTo(o.HaveOccurred())
 		e2e.Logf("alertSeverity is %v", alertSeverity)
-		o.Expect(alertSeverity).To(o.ContainSubstring("critical"))
+		o.Expect(alertSeverity).To(o.ContainSubstring("warning"))
 
-		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "master-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NorthboundStale\")].annotations.runbook_url}").Output()
+		alertRunbook, runbookErr := oc.AsAdmin().Run("get").Args("prometheusrule", "-n", "openshift-ovn-kubernetes", "networking-rules", "-o=jsonpath={.spec.groups[*].rules[?(@.alert==\"NorthboundStale\")].annotations.runbook_url}").Output()
 		o.Expect(runbookErr).NotTo(o.HaveOccurred())
 		e2e.Logf("The alertRunbook is %v", alertRunbook)
 		o.Expect(alertRunbook).To(o.ContainSubstring("https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/NorthboundStaleAlert.md"))
@@ -165,12 +160,12 @@ var _ = g.Describe("[sig-networking] SDN alerts", func() {
 	})
 
 	g.It("NonHyperShiftHOST-Author:qiowang-Medium-72329-Verify alert OVNKubernetesNorthboundDatabaseCPUUsagehigh and OVNKubernetesSouthboundDatabaseCPUUsagehigh", func() {
-		alertSeverity1, alertExpr1, _ := getOVNAlertMasterRules(oc, "OVNKubernetesNorthboundDatabaseCPUUsageHigh")
+		alertSeverity1, alertExpr1, _ := getOVNAlertNetworkingRules(oc, "OVNKubernetesNorthboundDatabaseCPUUsageHigh")
 		o.Expect(alertSeverity1).To(o.ContainSubstring("info"))
 		o.Expect(alertExpr1).To(o.ContainSubstring(`(sum(rate(container_cpu_usage_seconds_total{container="nbdb"}[5m])) BY`))
 		o.Expect(alertExpr1).To(o.ContainSubstring(`(instance, name, namespace)) > 0.8`))
 
-		alertSeverity2, alertExpr2, _ := getOVNAlertMasterRules(oc, "OVNKubernetesSouthboundDatabaseCPUUsageHigh")
+		alertSeverity2, alertExpr2, _ := getOVNAlertNetworkingRules(oc, "OVNKubernetesSouthboundDatabaseCPUUsageHigh")
 		o.Expect(alertSeverity2).To(o.ContainSubstring("info"))
 		o.Expect(alertExpr2).To(o.ContainSubstring(`(sum(rate(container_cpu_usage_seconds_total{container="sbdb"}[5m])) BY`))
 		o.Expect(alertExpr2).To(o.ContainSubstring(`(instance, name, namespace)) > 0.8`))
@@ -181,5 +176,12 @@ var _ = g.Describe("[sig-networking] SDN alerts", func() {
 		o.Expect(alertSeverity).To(o.ContainSubstring("warning"))
 		o.Expect(alertExpr).To(o.ContainSubstring(`ovnkube_clustermanager_allocated_v6_host_subnets / ovnkube_clustermanager_num_v6_host_subnets`))
 		o.Expect(alertExpr).To(o.ContainSubstring(`> 0.8`))
+	})
+
+	g.It("Author:qiowang-NonHyperShiftHOST-Medium-53926-OVN-K alerts for ovn northd inactivity", func() {
+		alertSeverity, alertExpr, runBook := getOVNAlertNetworkingRules(oc, "OVNKubernetesNorthdInactive")
+		o.Expect(alertSeverity).To(o.ContainSubstring("warning"))
+		o.Expect(alertExpr).To(o.ContainSubstring(`count(ovn_northd_status != 1) BY (instance, name, namespace) > 0`))
+		o.Expect(runBook).To(o.ContainSubstring(`https://github.com/openshift/runbooks/blob/master/alerts/cluster-network-operator/OVNKubernetesNorthdInactive.md`))
 	})
 })
