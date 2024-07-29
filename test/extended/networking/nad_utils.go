0e32feb66248059c5750a652af79c968fc5404ac
Anurag Saxena <anusaxen@redhat.com>
2024-07-28 22:54:37 -0400

    improvements on PR 17923 (#18335)
    combined 80-83 lines in 1 as per suggestion
    
    isolated subnets in first case
    
    optimised ns1 logic
    
    add a new line in udp pod template
    
    Co-authored-by: Anurag Saxena <anusaxen@anusaxen-thinkpadp1gen3.boston.csb>

--- test/extended/networking/nad_utils.go
+++ test/extended/networking/nad_utils.go
@@ -2,25 +2,28 @@ package networking
 
 import (
 	"fmt"
+	"net"
 	"strconv"
 	"time"
 
+	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 	"k8s.io/apimachinery/pkg/util/wait"
 	e2e "k8s.io/kubernetes/test/e2e/framework"
+	e2eoutput "k8s.io/kubernetes/test/e2e/framework/pod/output"
 )
 
 type udnPodResource struct {
-	name       string
-	namespace  string
-	annotation string
-	label      string
-	template   string
+	name      string
+	namespace string
+	label     string
+	template  string
 }
 
 type udnNetDefResource struct {
 	nadname             string
 	namespace           string
+	nad_network_name    string
 	topology            string
 	subnet              string
 	mtu                 int32
@@ -30,7 +33,7 @@ type udnNetDefResource struct {
 
 func (pod *udnPodResource) createUdnPod(oc *exutil.CLI) {
 	err := wait.Poll(5*time.Second, 20*time.Second, func() (bool, error) {
-		err1 := applyResourceFromTemplateByAdmin(oc, "--ignore-unknown-parameters=true", "-f", pod.template, "-p", "NAME="+pod.name, "NAMESPACE="+pod.namespace, "ANNOTATION="+pod.annotation, "LABEL="+pod.label)
+		err1 := applyResourceFromTemplateByAdmin(oc, "--ignore-unknown-parameters=true", "-f", pod.template, "-p", "NAME="+pod.name, "NAMESPACE="+pod.namespace, "LABEL="+pod.label)
 		if err1 != nil {
 			e2e.Logf("the err:%v, and try next round", err1)
 			return false, nil
@@ -42,7 +45,7 @@ func (pod *udnPodResource) createUdnPod(oc *exutil.CLI) {
 
 func (nad *udnNetDefResource) createUdnNad(oc *exutil.CLI) {
 	err := wait.Poll(5*time.Second, 20*time.Second, func() (bool, error) {
-		err1 := applyResourceFromTemplateByAdmin(oc, "--ignore-unknown-parameters=true", "-f", nad.template, "-p", "NADNAME="+nad.nadname, "NAMESPACE="+nad.namespace, "TOPOLOGY="+nad.topology, "SUBNET="+nad.subnet, "MTU="+strconv.Itoa(int(nad.mtu)), "NET_ATTACH_DEF_NAME="+nad.net_attach_def_name)
+		err1 := applyResourceFromTemplateByAdmin(oc, "--ignore-unknown-parameters=true", "-f", nad.template, "-p", "NADNAME="+nad.nadname, "NAMESPACE="+nad.namespace, "NAD_NETWORK_NAME="+nad.nad_network_name, "TOPOLOGY="+nad.topology, "SUBNET="+nad.subnet, "MTU="+strconv.Itoa(int(nad.mtu)), "NET_ATTACH_DEF_NAME="+nad.net_attach_def_name)
 		if err1 != nil {
 			e2e.Logf("the err:%v, and try next round", err1)
 			return false, nil
@@ -52,10 +55,62 @@ func (nad *udnNetDefResource) createUdnNad(oc *exutil.CLI) {
 	exutil.AssertWaitPollNoErr(err, fmt.Sprintf("fail to create pod %v", nad.nadname))
 }
 
-func (pod *udnPodResource) deleteUdnPod(oc *exutil.CLI) {
-	removeResource(oc, false, true, "pod", pod.name, "-n", pod.namespace)
-}
-
 func (nad *udnNetDefResource) deleteUdnNetDef(oc *exutil.CLI) {
 	removeResource(oc, false, true, "net-attach-def", nad.nadname, "-n", nad.namespace)
+}
+
+// getPodIPUDN returns IPv6 and IPv4 in vars in order on dual stack respectively and main IP in case of single stack (v4 or v6) in 1st var, and nil in 2nd var
+func getPodIPUDN(oc *exutil.CLI, namespace string, podName string, netName string) (string, string) {
+	ipStack := checkIPStackType(oc)
+	cmdIPv4 := "ip a sho " + netName + " | awk 'NR==3{print $2}' |grep -Eo '((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])'"
+	cmdIPv6 := "ip -o -6 addr show dev " + netName + " | awk '$3 == \"inet6\" && $6 == \"global\" {print $4}' | cut -d'/' -f1"
+	if ipStack == "ipv4single" {
+		podIPv4, err := execCommandInSpecificPod(oc, namespace, podName, cmdIPv4)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		e2e.Logf("The UDN pod %s IPv4 in namespace %s is %q", podName, namespace, podIPv4)
+		return podIPv4, ""
+	} else if ipStack == "ipv6single" {
+		podIPv6, err := execCommandInSpecificPod(oc, namespace, podName, cmdIPv6)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		e2e.Logf("The UDN pod %s IPv6 in namespace %s is %q", podName, namespace, podIPv6)
+		return podIPv6, ""
+	} else {
+		podIPv4, err := execCommandInSpecificPod(oc, namespace, podName, cmdIPv4)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		podIPv6, err := execCommandInSpecificPod(oc, namespace, podName, cmdIPv6)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		e2e.Logf("The UDN pod's %s IPv6 and IPv4 IP in namespace %s is %q %q", podName, namespace, podIPv6, podIPv4)
+		return podIPv6, podIPv4
+	}
+	return "", ""
+}
+
+// CurlPod2PodPass checks connectivity across udn pods regardless of network addressing type on cluster
+func CurlPod2PodPassUDN(oc *exutil.CLI, namespaceSrc string, podNameSrc string, namespaceDst string, podNameDst string) {
+	// getPodIPUDN will returns IPv6 and IPv4 in vars in order on dual stack respectively and main IP in case of single stack (v4 or v6) in 1st var, and nil in 2nd var
+	podIP1, podIP2 := getPodIPUDN(oc, namespaceDst, podNameDst, "ovn-udn1")
+	if podIP2 != "" {
+		_, err := e2eoutput.RunHostCmd(namespaceSrc, podNameSrc, "curl --connect-timeout 5 -s "+net.JoinHostPort(podIP1, "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+		_, err = e2eoutput.RunHostCmd(namespaceSrc, podNameSrc, "curl --connect-timeout 5 -s "+net.JoinHostPort(podIP2, "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+	} else {
+		_, err := e2eoutput.RunHostCmd(namespaceSrc, podNameSrc, "curl --connect-timeout 5 -s "+net.JoinHostPort(podIP1, "8080"))
+		o.Expect(err).NotTo(o.HaveOccurred())
+	}
+}
+
+// CurlPod2PodFailUDN ensures no connectivity from a udn pod to pod regardless of network addressing type on cluster
+func CurlPod2PodFailUDN(oc *exutil.CLI, namespaceSrc string, podNameSrc string, namespaceDst string, podNameDst string) {
+	// getPodIPUDN will returns IPv6 and IPv4 in vars in order on dual stack respectively and main IP in case of single stack (v4 or v6) in 1st var, and nil in 2nd var
+	podIP1, podIP2 := getPodIPUDN(oc, namespaceDst, podNameDst, "ovn-udn1")
+	if podIP2 != "" {
+		_, err := e2eoutput.RunHostCmd(namespaceSrc, podNameSrc, "curl --connect-timeout 5 -s "+net.JoinHostPort(podIP1, "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+		_, err = e2eoutput.RunHostCmd(namespaceSrc, podNameSrc, "curl --connect-timeout 5 -s "+net.JoinHostPort(podIP2, "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+	} else {
+		_, err := e2eoutput.RunHostCmd(namespaceSrc, podNameSrc, "curl --connect-timeout 5 -s "+net.JoinHostPort(podIP1, "8080"))
+		o.Expect(err).To(o.HaveOccurred())
+	}
 }
--- test/extended/networking/pod_udn.go
+++ test/extended/networking/pod_udn.go
@@ -6,7 +6,6 @@ import (
 
 	g "github.com/onsi/ginkgo/v2"
 	o "github.com/onsi/gomega"
-
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 )
 
@@ -49,26 +48,20 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 
 		ipStackType := checkIPStackType(oc)
 		g.By("1. Create first namespace")
-		oc.SetupProject()
 		ns1 := oc.Namespace()
-
-		defer exutil.RecoverNamespaceRestricted(oc, ns1)
-		exutil.SetNamespacePrivileged(oc, ns1)
 
 		g.By("2. Create 2nd namespace")
 		oc.SetupProject()
 		ns2 := oc.Namespace()
-
-		defer exutil.RecoverNamespaceRestricted(oc, ns2)
-		exutil.SetNamespacePrivileged(oc, ns2)
 
 		if ipStackType == "ipv4single" {
 			g.By("create NAD for ns1")
 			nad1 := udnNetDefResource{
 				nadname:             "l3-network-" + ns1,
 				namespace:           ns1,
+				nad_network_name:    "l3-network-" + ns1,
 				topology:            "layer3",
-				subnet:              "10.128.0.0/16/24",
+				subnet:              "10.150.0.0/16/24",
 				mtu:                 mtu,
 				net_attach_def_name: ns1 + "/l3-network-" + ns1,
 				template:            udnNadtemplate,
@@ -79,8 +72,9 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 			nad2 := udnNetDefResource{
 				nadname:             "l3-network-" + ns2,
 				namespace:           ns2,
+				nad_network_name:    "l3-network-" + ns2,
 				topology:            "layer3",
-				subnet:              "10.128.0.0/16/24",
+				subnet:              "10.151.0.0/16/24",
 				mtu:                 mtu,
 				net_attach_def_name: ns2 + "/l3-network-" + ns2,
 				template:            udnNadtemplate,
@@ -93,6 +87,7 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 				nad1 := udnNetDefResource{
 					nadname:             "l3-network-" + ns1,
 					namespace:           ns1,
+					nad_network_name:    "l3-network-" + ns1,
 					topology:            "layer3",
 					subnet:              "2010:100:200::0/60",
 					mtu:                 mtu,
@@ -105,8 +100,9 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 				nad2 := udnNetDefResource{
 					nadname:             "l3-network-" + ns2,
 					namespace:           ns2,
+					nad_network_name:    "l3-network-" + ns2,
 					topology:            "layer3",
-					subnet:              "2010:100:200::0/60",
+					subnet:              "2011:100:200::0/60",
 					mtu:                 mtu,
 					net_attach_def_name: ns2 + "/l3-network-" + ns2,
 					template:            udnNadtemplate,
@@ -118,8 +114,9 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 				nad1 := udnNetDefResource{
 					nadname:             "l3-network-" + ns1,
 					namespace:           ns1,
+					nad_network_name:    "l3-network-" + ns1,
 					topology:            "layer3",
-					subnet:              "10.128.0.0/16/24,2010:100:200::0/60",
+					subnet:              "10.150.0.0/16/24,2010:100:200::0/60",
 					mtu:                 mtu,
 					net_attach_def_name: ns1 + "/l3-network-" + ns1,
 					template:            udnNadtemplate,
@@ -130,8 +127,9 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 				nad2 := udnNetDefResource{
 					nadname:             "l3-network-" + ns2,
 					namespace:           ns2,
+					nad_network_name:    "l3-network-" + ns2,
 					topology:            "layer3",
-					subnet:              "10.128.0.0/16/24,2010:100:200::0/60",
+					subnet:              "10.151.0.0/16/24,2011:100:200::0/60",
 					mtu:                 mtu,
 					net_attach_def_name: ns2 + "/l3-network-" + ns2,
 					template:            udnNadtemplate,
@@ -141,42 +139,154 @@ var _ = g.Describe("[sig-networking] SDN udn", func() {
 		}
 		g.By("create a udn hello pod in ns1")
 		pod1 := udnPodResource{
-			name:       "hello-pod",
-			namespace:  ns1,
-			annotation: "l3-network-" + ns1,
-			label:      "hello-pod-ns1",
-			template:   udnPodTemplate,
+			name:      "hello-pod-ns1",
+			namespace: ns1,
+			label:     "hello-pod",
+			template:  udnPodTemplate,
 		}
 		pod1.createUdnPod(oc)
 		waitPodReady(oc, pod1.namespace, pod1.name)
 
 		g.By("create a udn hello pod in ns2")
 		pod2 := udnPodResource{
-			name:       "hello-pod",
-			namespace:  ns2,
-			annotation: "l3-network-" + ns2,
-			label:      "hello-pod-ns2",
-			template:   udnPodTemplate,
+			name:      "hello-pod-ns2",
+			namespace: ns2,
+			label:     "hello-pod",
+			template:  udnPodTemplate,
 		}
 
 		pod2.createUdnPod(oc)
 		waitPodReady(oc, pod2.namespace, pod2.name)
 
-		pod2Net1IPv4, pod2Net1IPv6 := getPodMultiNetwork(oc, ns2, pod2.name)
-		//Re-using multus util here to check net1 connectivity which is created by udn. It should be isolated and hence fail to connect
-
-		if ipStackType == "ipv4single" {
-			CurlMultusPod2PodFail(oc, ns1, pod1.name, pod2Net1IPv4, "net1", pod2.name)
-		} else {
-			if ipStackType == "ipv6single" {
-				CurlMultusPod2PodFail(oc, ns1, pod1.name, pod2Net1IPv6, "net1", pod2.name)
-			} else {
-				CurlMultusPod2PodFail(oc, ns1, pod1.name, pod2Net1IPv4, "net1", pod2.name)
-				CurlMultusPod2PodFail(oc, ns1, pod1.name, pod2Net1IPv6, "net1", pod2.name)
-			}
-		}
+		//udn network connectivity should be isolated
+		CurlPod2PodFailUDN(oc, ns1, pod1.name, ns2, pod2.name)
 		//default network connectivity should also be isolated
 		CurlPod2PodFail(oc, ns1, pod1.name, ns2, pod2.name)
 
+	})
+
+	g.It("Author:anusaxen-Critical-75236-Check udn pods are not isolated if same nad network is shared across two namespaces", func() {
+		var (
+			udnNadtemplate       = filepath.Join(testDataDirUDN, "udn_nad_template.yaml")
+			udnPodTemplate       = filepath.Join(testDataDirUDN, "udn_test_pod_template.yaml")
+			mtu            int32 = 1300
+		)
+
+		ipStackType := checkIPStackType(oc)
+		g.By("1. Create first namespace")
+		ns1 := oc.Namespace()
+
+		g.By("2. Create 2nd namespace")
+		oc.SetupProject()
+		ns2 := oc.Namespace()
+
+		if ipStackType == "ipv4single" {
+			g.By("create NAD for ns1")
+			nad1 := udnNetDefResource{
+				nadname:             "l3-network-" + ns1,
+				namespace:           ns1,
+				nad_network_name:    "l3-network-" + ns1,
+				topology:            "layer3",
+				subnet:              "10.150.0.0/16/24",
+				mtu:                 mtu,
+				net_attach_def_name: ns1 + "/l3-network-" + ns1,
+				template:            udnNadtemplate,
+			}
+			nad1.createUdnNad(oc)
+
+			g.By("create NAD for ns2")
+			nad2 := udnNetDefResource{
+				nadname:             "l3-network-" + ns2,
+				namespace:           ns2,
+				nad_network_name:    "l3-network-" + ns1, //network name is same as in ns1
+				topology:            "layer3",
+				subnet:              "10.151.0.0/16/24",
+				mtu:                 mtu,
+				net_attach_def_name: ns2 + "/l3-network-" + ns2,
+				template:            udnNadtemplate,
+			}
+			nad2.createUdnNad(oc)
+
+		} else {
+			if ipStackType == "ipv6single" {
+				g.By("create NAD for ns1")
+				nad1 := udnNetDefResource{
+					nadname:             "l3-network-" + ns1,
+					namespace:           ns1,
+					nad_network_name:    "l3-network-" + ns1,
+					topology:            "layer3",
+					subnet:              "2010:100:200::0/60",
+					mtu:                 mtu,
+					net_attach_def_name: ns1 + "/l3-network-" + ns1,
+					template:            udnNadtemplate,
+				}
+				nad1.createUdnNad(oc)
+
+				g.By("create NAD for ns2")
+				nad2 := udnNetDefResource{
+					nadname:             "l3-network-" + ns2,
+					namespace:           ns2,
+					nad_network_name:    "l3-network-" + ns1, //network name is same as in ns1
+					topology:            "layer3",
+					subnet:              "2011:100:200::0/60",
+					mtu:                 mtu,
+					net_attach_def_name: ns2 + "/l3-network-" + ns2,
+					template:            udnNadtemplate,
+				}
+				nad2.createUdnNad(oc)
+
+			} else {
+				g.By("create NAD for ns1")
+				nad1 := udnNetDefResource{
+					nadname:             "l3-network-" + ns1,
+					namespace:           ns1,
+					nad_network_name:    "l3-network-" + ns1,
+					topology:            "layer3",
+					subnet:              "10.150.0.0/16/24,2010:100:200::0/60",
+					mtu:                 mtu,
+					net_attach_def_name: ns1 + "/l3-network-" + ns1,
+					template:            udnNadtemplate,
+				}
+				nad1.createUdnNad(oc)
+
+				g.By("create NAD for ns2")
+				nad2 := udnNetDefResource{
+					nadname:             "l3-network-" + ns2,
+					namespace:           ns2,
+					nad_network_name:    "l3-network-" + ns1, //network name is same as in ns1
+					topology:            "layer3",
+					subnet:              "10.151.0.0/16/24,2011:100:200::0/60",
+					mtu:                 mtu,
+					net_attach_def_name: ns2 + "/l3-network-" + ns2,
+					template:            udnNadtemplate,
+				}
+				nad2.createUdnNad(oc)
+			}
+		}
+		g.By("create a udn hello pod in ns1")
+		pod1 := udnPodResource{
+			name:      "hello-pod-ns1",
+			namespace: ns1,
+			label:     "hello-pod",
+			template:  udnPodTemplate,
+		}
+		pod1.createUdnPod(oc)
+		waitPodReady(oc, pod1.namespace, pod1.name)
+
+		g.By("create a udn hello pod in ns2")
+		pod2 := udnPodResource{
+			name:      "hello-pod-ns2",
+			namespace: ns2,
+			label:     "hello-pod",
+			template:  udnPodTemplate,
+		}
+
+		pod2.createUdnPod(oc)
+		waitPodReady(oc, pod2.namespace, pod2.name)
+
+		//udn network connectivity should NOT be isolated
+		CurlPod2PodPassUDN(oc, ns1, pod1.name, ns2, pod2.name)
+		//default network connectivity should be isolated
+		CurlPod2PodFail(oc, ns1, pod1.name, ns2, pod2.name)
 	})
 })
