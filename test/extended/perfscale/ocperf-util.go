6182e15d222e0207ae78e9f4173cfa7b3be68a71
Liquan Cui <84432528+liqcui@users.noreply.github.com>
2024-09-02 15:46:01 +0800

    update code to optimize to run perfscale oc cli in parallel (#19087)
    Co-authored-by: Penghao <pewang@redhat.com>

--- test/extended/perfscale/ocperf-util.go
+++ test/extended/perfscale/ocperf-util.go
@@ -2,7 +2,10 @@ package perfscale
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
+	"sync"
+	"time"
 
 	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
@@ -36,4 +39,74 @@ func getImagestreamImageName(oc *exutil.CLI, imagestreamName string) string {
 
 	}
 	return imageName
+}
+
+func createNSUsingOCCLI(oc *exutil.CLI, namespace string, wg *sync.WaitGroup) {
+
+	err := oc.AsAdmin().WithoutNamespace().Run("create").Args("ns", namespace).Execute()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	wg.Done()
+
+}
+
+func checkIfNSIsInExpectedState(oc *exutil.CLI, expectedNum int, nsPattern string) {
+
+	o.Eventually(func() bool {
+		stdOut, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("ns").Output()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		nsReg := regexp.MustCompile(nsPattern + ".*")
+		perfCliNsList := nsReg.FindAllString(stdOut, -1)
+		nsNum := len(perfCliNsList)
+		e2e.Logf("current ns is: %v", nsNum)
+		return nsNum == expectedNum
+	}, 120*time.Second, 5*time.Second).Should(o.BeTrue())
+}
+
+func createDeploymentServiceUsingOCCLI(oc *exutil.CLI, namespace string, ocpPerfAppService string, ocpPerfAppDeployment string, ocPerfAppImageName string, wg *sync.WaitGroup) {
+
+	exutil.ApplyNsResourceFromTemplate(oc, namespace, "--ignore-unknown-parameters=true", "-f", ocpPerfAppDeployment, "-p", "IMAGENAME="+ocPerfAppImageName)
+	err := oc.AsAdmin().WithoutNamespace().Run("apply").Args("-f", ocpPerfAppService, "-n", namespace).Execute()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	wg.Done()
+}
+
+func checkIfDeploymentIsInExpectedState(oc *exutil.CLI, namespace string, resName string) {
+	var (
+		isCreated  bool
+		desiredNum string
+		readyNum   string
+	)
+	o.Eventually(func() bool {
+		kindNames, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("deployment", resName, "-n", namespace, "-oname").Output()
+		if strings.Contains(kindNames, "NotFound") || strings.Contains(kindNames, "No resources") || len(kindNames) == 0 || err != nil {
+			isCreated = false
+		} else {
+			//deployment/statefulset has been created, but not running, need to compare .status.readyReplicas and  in .status.replicas
+			isCreated = true
+			readyNum, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(kindNames, "-n", namespace, "-o=jsonpath={.status.readyReplicas}").Output()
+			desiredNum, _ = oc.AsAdmin().WithoutNamespace().Run("get").Args(kindNames, "-n", namespace, "-o=jsonpath={.status.replicas}").Output()
+		}
+		return isCreated && desiredNum == readyNum
+	}, 120*time.Second, time.Second).Should(o.BeTrue())
+}
+
+func getResourceUsingOCCLI(oc *exutil.CLI, namespace string, wg *sync.WaitGroup) {
+
+	err := oc.AsAdmin().WithoutNamespace().Run("get").Args("deployment,sa,secret", "-n", namespace).Execute()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	wg.Done()
+}
+
+func scaleDownDeploymentUsingOCCLI(oc *exutil.CLI, namespace string, wg *sync.WaitGroup) {
+
+	err := oc.AsAdmin().WithoutNamespace().Run("scale").Args("deployment", "-n", namespace, "--replicas=0", "--all").Execute()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	wg.Done()
+}
+
+func deleteNSUsingOCCLI(oc *exutil.CLI, namespace string, wg *sync.WaitGroup) {
+
+	err := oc.AsAdmin().WithoutNamespace().Run("delete").Args("ns", namespace).Execute()
+	o.Expect(err).NotTo(o.HaveOccurred())
+	wg.Done()
 }
--- test/extended/perfscale/ocperf.go
+++ test/extended/perfscale/ocperf.go
@@ -2,6 +2,7 @@ package perfscale
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	g "github.com/onsi/ginkgo/v2"
@@ -36,7 +37,7 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 	})
 
 	// author: liqcui@redhat.com
-	g.It("Longduration-Author:liqcui-Medium-22140-Create multiple projects and time various oc commands durations[Slow][Serial]", func() {
+	g.It("Author:liqcui-Medium-22140-Create multiple projects and time various oc commands durations[Serial]", func() {
 
 		if isSNO {
 			g.Skip("Skip Testing on SNO ...")
@@ -70,7 +71,7 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 		if len(ocPerfAppImageName) == 0 {
 			ocPerfAppImageName = "quay.io/openshifttest/hello-openshift:multiarch"
 		}
-		//ocPerfAppImageName = "quay.io/openshifttest/hello-openshift:multiarch"
+
 		e2e.Logf("ocp perfscale test case ocp-22140 will use below image to test:\n[Image Name]:%s", ocPerfAppImageName)
 
 		if iaasPlatform == "ibmcloud" {
@@ -85,15 +86,31 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 
 		start := time.Now()
 		g.By("Try to create projects and deployments")
-		nsPattern := "e2e-oc-cli-perf-%d"
+		randStr := exutil.RandStrCustomize("perfscaleqeoclixyz", 8)
+		nsPattern := randStr + "-%d"
+
+		var wg sync.WaitGroup
+		for i := 0; i < projectCount; i++ {
+			namespace := fmt.Sprintf(nsPattern, i)
+			wg.Add(1)
+			defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("ns", namespace, "--ignore-not-found").Execute()
+			go createNSUsingOCCLI(oc, namespace, &wg)
+		}
+		wg.Wait() // Wait for all goroutines to finish
+
+		checkIfNSIsInExpectedState(oc, projectCount, randStr)
+
+		//var wg sync.WaitGroup
 		for i := 0; i < projectCount; i++ {
 			namespace = fmt.Sprintf(nsPattern, i)
-			defer oc.AsAdmin().WithoutNamespace().Run("delete").Args("project", namespace, "--ignore-not-found").Execute()
-			err := oc.AsAdmin().WithoutNamespace().Run("new-project").Args(namespace).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
-			exutil.ApplyNsResourceFromTemplate(oc, namespace, "--ignore-unknown-parameters=true", "-f", ocpPerfAppDeployment, "-p", "IMAGENAME="+ocPerfAppImageName)
-			err = oc.AsAdmin().WithoutNamespace().Run("apply").Args("-f", ocpPerfAppService, "-n", namespace).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
+			wg.Add(1)
+			go createDeploymentServiceUsingOCCLI(oc, namespace, ocpPerfAppService, ocpPerfAppDeployment, ocPerfAppImageName, &wg)
+		}
+		wg.Wait()
+
+		for i := 0; i < projectCount; i++ {
+			namespace = fmt.Sprintf(nsPattern, i)
+			checkIfDeploymentIsInExpectedState(oc, namespace, "ocp-perfapp")
 		}
 		createDuration := time.Since(start).Seconds()
 
@@ -103,13 +120,11 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 		g.By("Try to get deployment, sa, and secrets")
 		for i := 0; i < projectCount; i++ {
 			namespace = fmt.Sprintf(nsPattern, i)
-			err := oc.AsAdmin().WithoutNamespace().Run("get").Args("deployment", "-n", namespace).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
-			err = oc.AsAdmin().WithoutNamespace().Run("get").Args("sa", "-n", namespace).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
-			err = oc.AsAdmin().WithoutNamespace().Run("get").Args("secrets", "-n", namespace).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
+			wg.Add(1)
+			go getResourceUsingOCCLI(oc, namespace, &wg)
 		}
+		wg.Wait()
+
 		getDuration := time.Since(start).Seconds()
 		e2e.Logf("Duration for gettings deployment, sa, secrets in each of those is %.2f seconds", getDuration)
 
@@ -117,9 +132,16 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 		g.By("Try to scale the dc replicas to 0")
 		for i := 0; i < projectCount; i++ {
 			namespace = fmt.Sprintf(nsPattern, i)
-			err := oc.AsAdmin().WithoutNamespace().Run("scale").Args("deployment", "-n", namespace, "--replicas=0", "--all").Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
+			wg.Add(1)
+			go scaleDownDeploymentUsingOCCLI(oc, namespace, &wg)
 		}
+		wg.Wait()
+
+		for i := 0; i < projectCount; i++ {
+			namespace = fmt.Sprintf(nsPattern, i)
+			checkIfDeploymentIsInExpectedState(oc, namespace, "ocp-perfapp")
+		}
+
 		scaleDuration := time.Since(start).Seconds()
 		e2e.Logf("Duration for scale the dc replicas to 0 in each of those is %.2f seconds", scaleDuration)
 
@@ -127,9 +149,12 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 		g.By("Try to delete project")
 		for i := 0; i < projectCount; i++ {
 			namespace = fmt.Sprintf(nsPattern, i)
-			err := oc.AsAdmin().WithoutNamespace().Run("delete").Args("project", namespace).Execute()
-			o.Expect(err).NotTo(o.HaveOccurred())
+			wg.Add(1)
+			go deleteNSUsingOCCLI(oc, namespace, &wg)
 		}
+		wg.Wait()
+
+		checkIfNSIsInExpectedState(oc, 0, randStr)
 		deleteDuration := time.Since(start).Seconds()
 
 		for i := 0; i < len(masterNodeNames); i++ {
@@ -145,18 +170,18 @@ var _ = g.Describe("[sig-perfscale] PerfScale oc cli perf", func() {
 			o.Expect(metricValueAfter).To(o.BeNumerically("<=", 3221225472))
 		}
 
-		e2e.Logf("Duration for deleting %d projects and 1 deploymentConfig in each of those is %.2f seconds", projectCount, deleteDuration)
+		// e2e.Logf("Duration for deleting %d projects and 1 deploymentConfig in each of those is %.2f seconds", projectCount, deleteDuration)
 		// all values in BeNumerically are "Expected" and "Threshold" numbers
 		// Expected derived by running this program 5 times against 4.8.0-0.nightly-2021-10-20-155651 and taking median
 		// Threshold is set to lower than the expected value
-		e2e.Logf("createDuration is: %v Expected time is less than 720s.", createDuration)
-		e2e.Logf("getDuration is: %v Expected time is less than 300s.", getDuration)
-		e2e.Logf("scaleDuration is: %v Expected time is less than 240s.", scaleDuration)
-		e2e.Logf("deleteDuration is: %v Expected time is less than 900s.", deleteDuration)
+		e2e.Logf("createDuration is: %v Expected time is less than 360s.", createDuration)
+		e2e.Logf("getDuration is: %v Expected time is less than 120s.", getDuration)
+		e2e.Logf("scaleDuration is: %v Expected time is less than 120s.", scaleDuration)
+		e2e.Logf("deleteDuration is: %v Expected time is less than 300s.", deleteDuration)
 
-		o.Expect(createDuration).To(o.BeNumerically("<=", 720))
-		o.Expect(getDuration).To(o.BeNumerically("<=", 300))
-		o.Expect(scaleDuration).To(o.BeNumerically("<=", 240))
-		o.Expect(deleteDuration).To(o.BeNumerically("<=", 900))
+		o.Expect(createDuration).To(o.BeNumerically("<=", 360))
+		o.Expect(getDuration).To(o.BeNumerically("<=", 120))
+		o.Expect(scaleDuration).To(o.BeNumerically("<=", 120))
+		o.Expect(deleteDuration).To(o.BeNumerically("<=", 300))
 	})
 })
