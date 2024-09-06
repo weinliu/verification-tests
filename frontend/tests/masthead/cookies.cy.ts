81634bec6a15512e489a6f588a79db29bf58efcb
Yanping Zhang <yanpzhan@redhat.com>
2024-09-06 12:19:54 +0800

    auto ocp-75895 (#19331)

---
+++ frontend/tests/masthead/cookies.cy.ts
@@ -0,0 +1,12 @@
+import { guidedTour } from '../../upstream/views/guided-tour';
+describe('cookies related feature', () => {
+  before(() => {
+    cy.login(Cypress.env("LOGIN_IDP"),Cypress.env("LOGIN_USERNAME"),Cypress.env("LOGIN_PASSWORD"));
+    guidedTour.close();
+  });
+
+  it('(OCP-75895,yanpzhan,UserInterface) Console cookies csrf-token and openshift-session-token have SameSite option',{tags:['@userinterface','@e2e','@rosa','@osd-ccs']}, () => {
+    cy.getCookie('csrf-token').should('have.property','sameSite','strict');
+    cy.getCookie('openshift-session-token').should('have.property','sameSite','strict');
+  });
+});  
