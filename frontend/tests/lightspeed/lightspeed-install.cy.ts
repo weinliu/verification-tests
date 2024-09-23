4d69915058acdbd2e0cfb291d4d68be4abfac9f5
Andy Pickering <andy.nihon@gmail.com>
2024-09-23 16:33:15 +0900

    OpenShift Lightspeed: Operator install test improvements (#19353)
    * OpenShift Lightspeed: Create OLSConfig after installing operator
    
    After the OpenShift Lightspeed operator is installed, an OLSConfig also
    need to be created for the OpenShift Lightspeed UI to appear.
    
    * Fix OpenShfit Lightspeed install test
    
    Fixes for the OpenShift Lightspeed install checks and the removal to
    make these test more robust

--- frontend/tests/lightspeed/lightspeed-install.cy.ts
+++ frontend/tests/lightspeed/lightspeed-install.cy.ts
@@ -5,33 +5,54 @@ describe('Lightspeed related features', () => {
   const OLS = {
     namespace:   "openshift-lightspeed",
     packageName: "lightspeed-operator",
-    operatorName: "OpenShift Lightspeed Operator"
+    operatorName: "OpenShift Lightspeed Operator",
+    config: {
+      kind: 'OLSConfig',
+      name: 'cluster',
+    },
   };
 
   before(() => {
     cy.adminCLI(`oc adm policy add-cluster-role-to-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
     cy.login(Cypress.env('LOGIN_IDP'), Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
-    //Delete operator if already exists
-    Pages.gotoInstalledOperatorPage(OLS.namespace);
-    cy.exec(`oc get sub ${OLS.packageName} -n ${OLS.namespace} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, {failOnNonZeroExit: false}).then(result => {
-      if(!result.stderr.includes('NotFound')){
-      operatorHubPage.removeOperator(OLS.operatorName);
-      }
-    });
   });
 
   after(() => {
-    Pages.gotoInstalledOperatorPage(OLS.namespace);
-    operatorHubPage.checkOperatorStatus(OLS.operatorName, "Succeeded")
-    operatorHubPage.removeOperator(OLS.operatorName);
+    // Delete entire namespace to delete operator and ensure everything else is cleaned up
+    cy.adminCLI(`oc delete namespace ${OLS.namespace}`, { failOnNonZeroExit: false });
+
+    // Delete config
+    cy.exec(`oc delete ${OLS.config.kind} ${OLS.config.name} -n ${OLS.namespace} --kubeconfig ${Cypress.env('KUBECONFIG_PATH')}`, { failOnNonZeroExit: false });
+
     cy.adminCLI(`oc adm policy remove-cluster-role-from-user cluster-admin ${Cypress.env('LOGIN_USERNAME')}`);
   });
 
-  it('(OLS-427,jfula,Lightspeed) Deploy Openshift Lightspeed operator via web console', {tags: ['e2e','admin', '@smoke']}, () => {
+  it('(OLS-427,jfula,Lightspeed) Deploy OpenShift Lightspeed operator via web console', {tags: ['e2e','admin', '@smoke']}, () => {
     operatorHubPage.installOperator(OLS.packageName, "redhat-operators");
+    cy.get('.co-clusterserviceversion-install__heading', { timeout: 2 * 60 * 1000 }).should('include.text', 'ready for use');
 
-    //Install the Openshift Lightspeed catalog source
+    const config = `apiVersion: ols.openshift.io/v1alpha1
+kind: ${OLS.config.kind}
+metadata:
+  name: ${OLS.config.name}
+spec:
+  llm:
+    providers:
+      - type: openai
+        name: openai
+        credentialsSecretRef:
+          name: openai-api-keys
+        url: https://api.openai.com/v1
+        models:
+          - name: gpt-3.5-turbo
+  ols:
+    defaultModel: gpt-3.5-turbo
+    defaultProvider: openai
+    logLevel: INFO`;
+    cy.exec(`echo '${config}' | oc create -f -`);
+
+    //Install the OpenShift Lightspeed catalog source
     // lightUtils.installOperator(OLS.namespace, OLS.packageName, "redhat-operators", catalogSource.channel(OLS.packageName), catalogSource.version(OLS.packageName), false, OLS.operatorName);
-    //Install the Openshift Lightspeed Operator with console plungin
+    //Install the OpenShift Lightspeed Operator with console plugin
   });
 });
