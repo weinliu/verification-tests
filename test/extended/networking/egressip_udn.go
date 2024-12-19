d5205c1acf57734fa3b58b5f33e1b6974f53fe1a
Jean Chen <jechen@redhat.com>
2024-12-18 21:53:19 -0500

    Auto EIP UDN layer3 cases - OCP-78200 and OCP-78274  (#21868)
    * Add OCP-78200-78274
    
    * some updates
    
    * update on OCP-78274 for dualstach ovnk local client host restart
    
    * updates after Meina's review comment, one correction

--- test/extended/networking/egressip_udn.go
+++ test/extended/networking/egressip_udn.go
@@ -671,6 +671,118 @@ var _ = g.Describe("[sig-networking] SDN udn EgressIP", func() {
 		}
 	})
 
+	// author: jechen@redhat.com
+	g.It("Author:jechen-ConnectedOnly-NonPreRelease-High-78200-egressIP still works correctly after OVNK restarted on local and remote client host  (layer3 and IPv4 only) [Serial]", func() {
+		var (
+			buildPruningBaseDir = exutil.FixturePath("testdata", "networking")
+			egressIP2Template   = filepath.Join(buildPruningBaseDir, "egressip-config2-template.yaml")
+			pingPodNodeTemplate = filepath.Join(buildPruningBaseDir, "ping-for-pod-specific-node-template.yaml")
+		)
+
+		exutil.By("1 Get node list, apply EgressLabel Key to one node to make it egressNode")
+		nodeList, err := e2enode.GetReadySchedulableNodes(context.TODO(), oc.KubeFramework().ClientSet)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		ok, nodesToBeUsed := getTwoNodesSameSubnet(oc, nodeList)
+		if !ok || nodesToBeUsed == nil || len(nodesToBeUsed) < 2 {
+			g.Skip("The prerequirement was not fullfilled, skip the case!!")
+		}
+		egressNode := nodesToBeUsed[0]
+
+		defer e2enode.RemoveLabelOffNode(oc.KubeFramework().ClientSet, egressNode, egressNodeLabel)
+		e2enode.AddOrUpdateLabelOnNode(oc.KubeFramework().ClientSet, egressNode, egressNodeLabel, "true")
+
+		exutil.By("2 Create an egressip object, verify egressIP is assigned to egress node")
+		freeIPs := findFreeIPs(oc, egressNode, 1)
+		o.Expect(len(freeIPs)).Should(o.Equal(1))
+		egressip1 := egressIPResource1{
+			name:          "egressip-78200",
+			template:      egressIP2Template,
+			egressIP1:     freeIPs[0],
+			nsLabelKey:    "org",
+			nsLabelValue:  "qe",
+			podLabelKey:   "color",
+			podLabelValue: "pink",
+		}
+		defer egressip1.deleteEgressIPObject1(oc)
+		egressip1.createEgressIPObject2(oc)
+		egressIPMaps1 := getAssignedEIPInEIPObject(oc, egressip1.name)
+		o.Expect(len(egressIPMaps1)).Should(o.Equal(1))
+		o.Expect(egressIPMaps1[0]["node"]).Should(o.ContainSubstring(egressNode))
+
+		exutil.By("3 Obtain a namespace, apply a label to the namespace that matches namespaceSelector definied in egressIP object")
+		ns1 := oc.Namespace()
+
+		defer oc.AsAdmin().WithoutNamespace().Run("label").Args("ns", ns1, "org-").Execute()
+		err = oc.AsAdmin().WithoutNamespace().Run("label").Args("ns", ns1, "org=qe").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		exutil.By("4. Create an UDN in ns1")
+		err = applyL3UDNtoNamespace(oc, ns1, 0)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		exutil.By("5.1 In the namespace, create two test pods, the first one on egressNode, the second one on nonEgressNode ")
+		exutil.By("5.2 Apply to all pods with label that matches podSelector definied in egressIP object")
+		testpods := make([]pingPodResourceNode, 2)
+		for i := 0; i < 2; i++ {
+			testpods[i] = pingPodResourceNode{
+				name:      "hello-pod" + strconv.Itoa(i) + "-" + ns1,
+				namespace: ns1,
+				nodename:  nodesToBeUsed[i],
+				template:  pingPodNodeTemplate,
+			}
+			testpods[i].createPingPodNode(oc)
+			waitPodReady(oc, ns1, testpods[i].name)
+			defer exutil.LabelPod(oc, ns1, testpods[i].name, "color-")
+			err = exutil.LabelPod(oc, ns1, testpods[i].name, "color=pink")
+			o.Expect(err).NotTo(o.HaveOccurred())
+		}
+
+		exutil.By("6. Verify egress traffic from these local or remote egressIP pods should use egressIP as their sourceIP")
+		var dstHost, primaryInf, tcpdumpCmd, cmdOnPod string
+		var infErr error
+		exutil.By("6.1 Use tcpdump to verify egressIP.")
+		e2e.Logf("Trying to get physical interface on the egressNode %s", egressNode)
+		primaryInf, infErr = getSnifPhyInf(oc, egressNode)
+		o.Expect(infErr).NotTo(o.HaveOccurred())
+		dstHost = nslookDomainName("ifconfig.me")
+		tcpdumpCmd = fmt.Sprintf("timeout 60s tcpdump -c 4 -nni %s host %s", primaryInf, dstHost)
+		_, cmdOnPod = getRequestURL(dstHost)
+
+		exutil.By("6.2 Use tcpdump captured on egressNode to verify egressIP from local pods and remote pods")
+		for i := 0; i < 2; i++ {
+			tcpdumOutput := getTcpdumpOnNodeCmdFromPod(oc, egressNode, tcpdumpCmd, ns1, testpods[i].name, cmdOnPod)
+			o.Expect(strings.Contains(tcpdumOutput, freeIPs[0])).To(o.BeTrue())
+		}
+
+		exutil.By("7. Restart ovnkube-node pod of client host that local egressIP pod is on.\n")
+		// Since local egressIP pod is on egress node, restart ovnkube-pod of egress node
+		ovnkPod := ovnkubeNodePod(oc, egressNode)
+		err = oc.AsAdmin().WithoutNamespace().Run("delete").Args("pods", ovnkPod, "-n", "openshift-ovn-kubernetes").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		waitForPodWithLabelReady(oc, "openshift-ovn-kubernetes", "app=ovnkube-node")
+
+		exutil.By("8. Validate egressIP again after restarting ovnkude-node pod of client host that local egressIP pod is on \n")
+		exutil.By("Use tcpdump captured on egressNode to verify egressIP from local pods and remote pods again after UDN recreation")
+		for i := 0; i < 2; i++ {
+			tcpdumOutput := getTcpdumpOnNodeCmdFromPod(oc, egressNode, tcpdumpCmd, ns1, testpods[i].name, cmdOnPod)
+			o.Expect(strings.Contains(tcpdumOutput, freeIPs[0])).To(o.BeTrue())
+		}
+
+		exutil.By("9. Restart ovnkube-node pod of client host that remote egressIP pod is on.\n")
+		// Since remote egressIP pod is on non-egress node, restart ovnkube-pod of the non-egress node nodesToBeUsed[1]
+		ovnkPod = ovnkubeNodePod(oc, nodesToBeUsed[1])
+		err = oc.AsAdmin().WithoutNamespace().Run("delete").Args("pods", ovnkPod, "-n", "openshift-ovn-kubernetes").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		waitForPodWithLabelReady(oc, "openshift-ovn-kubernetes", "app=ovnkube-node")
+
+		exutil.By("10. Validate egressIP again after restarting ovnkude-node pod of client host that remote egressIP pod is on \n")
+		exutil.By("Use tcpdump captured on egressNode to verify egressIP from local pods and remote pods again after UDN recreation")
+		for i := 0; i < 2; i++ {
+			tcpdumOutput := getTcpdumpOnNodeCmdFromPod(oc, egressNode, tcpdumpCmd, ns1, testpods[i].name, cmdOnPod)
+			o.Expect(strings.Contains(tcpdumOutput, freeIPs[0])).To(o.BeTrue())
+		}
+	})
+
 })
 
 var _ = g.Describe("[sig-networking] SDN udn EgressIP IPv6", func() {
@@ -1440,13 +1552,6 @@ var _ = g.Describe("[sig-networking] SDN udn EgressIP IPv6", func() {
 		waitPodReady(oc, ns1, testpod3.name)
 		testpods = append(testpods, testpod3)
 
-		exutil.By("5.2 Apply to all pods with label that matches podSelector definied in egressIP object")
-		for i := 0; i < len(testpods); i++ {
-			defer exutil.LabelPod(oc, ns1, testpods[i].name, "color-")
-			err = exutil.LabelPod(oc, ns1, testpods[i].name, "color=pink")
-			o.Expect(err).NotTo(o.HaveOccurred())
-		}
-
 		exutil.By("6. Use tcpdump captured on egressNode to verify egressIP from local pods and remote pods")
 		primaryInf, infErr := getSnifPhyInf(oc, egressNode1)
 		o.Expect(infErr).NotTo(o.HaveOccurred())
@@ -1494,9 +1599,6 @@ var _ = g.Describe("[sig-networking] SDN udn EgressIP IPv6", func() {
 		for i := 0; i < len(testpods); i++ {
 			testpods[i].createPingPodNode(oc)
 			waitPodReady(oc, ns1, testpods[i].name)
-			defer exutil.LabelPod(oc, ns1, testpods[i].name, "color-")
-			err = exutil.LabelPod(oc, ns1, testpods[i].name, "color=pink")
-			o.Expect(err).NotTo(o.HaveOccurred())
 		}
 		err = waitUDNCRDApplied(oc, ns1, udncrd.crdname)
 		o.Expect(err).NotTo(o.HaveOccurred())
@@ -1521,4 +1623,215 @@ var _ = g.Describe("[sig-networking] SDN udn EgressIP IPv6", func() {
 		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
 	})
 
+	// author: jechen@redhat.com
+	g.It("Author:jechen-ConnectedOnly-NonPreRelease-High-78274-egressIP still works correctly after OVNK restarted on local and remote client host (layer3 + v6 or dualstack) [Serial]", func() {
+		var (
+			buildPruningBaseDir = exutil.FixturePath("testdata", "networking")
+			egressIP1Template   = filepath.Join(buildPruningBaseDir, "egressip-config1-template.yaml")
+			pingPodNodeTemplate = filepath.Join(buildPruningBaseDir, "ping-for-pod-specific-node-template.yaml")
+		)
+
+		exutil.By("1. Get node list, apply EgressLabel Key to one node to make it egressNode, for dualstack, need to label two nodes to be egressNodes")
+		nodeList, err := e2enode.GetReadySchedulableNodes(context.TODO(), oc.KubeFramework().ClientSet)
+		o.Expect(err).NotTo(o.HaveOccurred())
+		var egressNode1, egressNode2, nonEgressNode string
+		var freeIPs []string
+		if ipStackType == "dualstack" && len(nodeList.Items) < 3 {
+			g.Skip("Need 3 nodes for the test on dualstack cluster, the prerequirement was not fullfilled, skip the case!!")
+		}
+		if ipStackType == "ipv6single" && len(nodeList.Items) < 2 {
+			g.Skip("Need 2 nodes for the test on singlev6 cluster, the prerequirement was not fullfilled, skip the case!!")
+		}
+
+		if ipStackType == "dualstack" {
+			egressNode1 = nodeList.Items[0].Name
+			egressNode2 = nodeList.Items[1].Name
+			nonEgressNode = nodeList.Items[2].Name
+			freeIPs = findFreeIPs(oc, egressNode1, 1)
+			o.Expect(len(freeIPs)).Should(o.Equal(1))
+			freeIPv6s := findFreeIPv6s(oc, egressNode2, 1)
+			o.Expect(len(freeIPs)).Should(o.Equal(1))
+			freeIPs = append(freeIPs, freeIPv6s[0])
+			defer e2enode.RemoveLabelOffNode(oc.KubeFramework().ClientSet, egressNode2, egressNodeLabel)
+			e2enode.AddOrUpdateLabelOnNode(oc.KubeFramework().ClientSet, egressNode2, egressNodeLabel, "true")
+		} else if ipStackType == "ipv6single" {
+			egressNode1 = nodeList.Items[0].Name
+			nonEgressNode = nodeList.Items[1].Name
+			freeIPs = findFreeIPv6s(oc, egressNode1, 2)
+			o.Expect(len(freeIPs)).Should(o.Equal(2))
+		}
+		e2e.Logf("egressIPs to use: %s", freeIPs)
+
+		defer e2enode.RemoveLabelOffNode(oc.KubeFramework().ClientSet, egressNode1, egressNodeLabel)
+		e2enode.AddOrUpdateLabelOnNode(oc.KubeFramework().ClientSet, egressNode1, egressNodeLabel, "true")
+
+		exutil.By("2. Create an egressip object")
+		egressip1 := egressIPResource1{
+			name:      "egressip-78274",
+			template:  egressIP1Template,
+			egressIP1: freeIPs[0],
+			egressIP2: freeIPs[1],
+		}
+		defer egressip1.deleteEgressIPObject1(oc)
+		egressip1.createEgressIPObject1(oc)
+		egressIPMaps1 := getAssignedEIPInEIPObject(oc, egressip1.name)
+
+		// For dualstack, need to find out the actual nodes where v4 and v6 egressIP address are assigned
+		var assignedEIPNodev4, assignedEIPNodev6, assignedEIPv6Addr string
+		if ipStackType == "dualstack" {
+			o.Expect(len(egressIPMaps1) == 2).Should(o.BeTrue())
+			for _, eipMap := range egressIPMaps1 {
+				if netutils.IsIPv4String(eipMap["egressIP"]) {
+					assignedEIPNodev4 = eipMap["node"]
+				}
+				if netutils.IsIPv6String(eipMap["egressIP"]) {
+					assignedEIPNodev6 = eipMap["node"]
+					assignedEIPv6Addr = eipMap["egressIP"]
+				}
+			}
+			o.Expect(assignedEIPNodev4).NotTo(o.Equal(""))
+			o.Expect(assignedEIPNodev6).NotTo(o.Equal(""))
+			e2e.Logf("For the dualstack EIP,  v4 EIP is currently assigned to node: %s, v6 EIP is currently assigned to node: %s", assignedEIPNodev4, assignedEIPNodev6)
+		} else {
+			o.Expect(len(egressIPMaps1) == 1).Should(o.BeTrue())
+			assignedEIPNodev6 = egressNode1
+			assignedEIPv6Addr = egressIPMaps1[0]["egressIP"]
+		}
+
+		exutil.By("3. Obtain a namespace, apply a label to the namespace that matches namespaceSelector definied in egressIP object")
+		ns1 := oc.Namespace()
+
+		defer oc.AsAdmin().WithoutNamespace().Run("label").Args("ns", ns1, "name-").Execute()
+		err = oc.AsAdmin().WithoutNamespace().Run("label").Args("ns", ns1, "name=test").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		exutil.By("4. Create an UDN in ns1")
+		err = applyL3UDNtoNamespace(oc, ns1, 0)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		exutil.By("5.1 In the namespace, create local test pod on egressNode, create remote test pod on nonEgressNode ")
+		var testpod1, testpod2, testpod3 pingPodResourceNode
+		// var testpods []pingPodResourceNode
+		if ipStackType == "dualstack" {
+			// testpod1 is local pod on assignedEIPNodev4 for dualstack
+			testpod1 = pingPodResourceNode{
+				name:      "hello-pod1-" + ns1,
+				namespace: ns1,
+				nodename:  assignedEIPNodev4,
+				template:  pingPodNodeTemplate,
+			}
+			testpod1.createPingPodNode(oc)
+			waitPodReady(oc, ns1, testpod1.name)
+			// testpods = append(testpods, testpod1)
+		}
+
+		// testpod2 is local pod on assignedEIPNodev6 for dualstack
+		testpod2 = pingPodResourceNode{
+			name:      "hello-pod2-" + ns1,
+			namespace: ns1,
+			nodename:  assignedEIPNodev6,
+			template:  pingPodNodeTemplate,
+		}
+		testpod2.createPingPodNode(oc)
+		waitPodReady(oc, ns1, testpod2.name)
+		// testpods = append(testpods, testpod2)
+
+		// testpod3 is remote pod on the other non-egress node
+		testpod3 = pingPodResourceNode{
+			name:      "hello-pod3-" + ns1,
+			namespace: ns1,
+			nodename:  nonEgressNode,
+			template:  pingPodNodeTemplate,
+		}
+		testpod3.createPingPodNode(oc)
+		waitPodReady(oc, ns1, testpod3.name)
+		// testpods = append(testpods, testpod3)
+
+		exutil.By("6. Use tcpdump captured on egressNode to verify egressIP from local pods and remote pods")
+		primaryInf, infErr := getSnifPhyInf(oc, egressNode1)
+		o.Expect(infErr).NotTo(o.HaveOccurred())
+
+		var dstHostv4, tcpdumpCmdv4, cmdOnPodv4, tcpdumpCmdv6, cmdOnPodv6 string
+		if ipStackType == "dualstack" {
+			exutil.By("Verify egressIP from IPv4 perspective")
+			dstHostv4 = nslookDomainName("ifconfig.me")
+			exutil.SetNamespacePrivileged(oc, oc.Namespace())
+			tcpdumpCmdv4 = fmt.Sprintf("timeout 60s tcpdump -c 4 -nni %s host %s", primaryInf, dstHostv4)
+			_, cmdOnPodv4 = getRequestURL(dstHostv4)
+			exutil.By("6.1 Verify v4 egressIP from test pods local to egress node")
+			tcpdumOutputv4 := getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev4, tcpdumpCmdv4, ns1, testpod1.name, cmdOnPodv4)
+			o.Expect(strings.Contains(tcpdumOutputv4, freeIPs[0])).To(o.BeTrue())
+			exutil.By("6.2 Verify v4 egressIP from test pods remote to egress node")
+			tcpdumOutputv4 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev4, tcpdumpCmdv4, ns1, testpod3.name, cmdOnPodv4)
+			o.Expect(strings.Contains(tcpdumOutputv4, freeIPs[0])).To(o.BeTrue())
+		}
+
+		exutil.By("Verify egressIP from IPv6 perspective")
+		tcpdumpCmdv6 = fmt.Sprintf("timeout 60s tcpdump -c 3 -nni %s ip6 and host %s", primaryInf, dstHostv6)
+		_, cmdOnPodv6 = getRequestURL("[" + dstHostv6 + "]")
+		exutil.By("6.3 Verify v6 egressIP from test pods local to egress node")
+		tcpdumOutputv6 := getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev6, tcpdumpCmdv6, ns1, testpod2.name, cmdOnPodv6)
+		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
+		exutil.By("6.4 Verify v6 egressIP from test pods remote to egress node")
+		tcpdumOutputv6 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev6, tcpdumpCmdv6, ns1, testpod3.name, cmdOnPodv6)
+		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
+
+		exutil.By("7. Restart ovnkube-node pod of client host that local egressIP pod is on.\n")
+		// Since local egressIP pod is on egress node, so just to restart ovnkube-pod of egress node
+		ovnkPod := ovnkubeNodePod(oc, egressNode1)
+		err = oc.AsAdmin().WithoutNamespace().Run("delete").Args("pods", ovnkPod, "-n", "openshift-ovn-kubernetes").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		waitForPodWithLabelReady(oc, "openshift-ovn-kubernetes", "app=ovnkube-node")
+		if ipStackType == "dualstack" {
+			ovnkPod := ovnkubeNodePod(oc, egressNode2)
+			err = oc.AsAdmin().WithoutNamespace().Run("delete").Args("pods", ovnkPod, "-n", "openshift-ovn-kubernetes").Execute()
+			o.Expect(err).NotTo(o.HaveOccurred())
+			waitForPodWithLabelReady(oc, "openshift-ovn-kubernetes", "app=ovnkube-node")
+		}
+
+		exutil.By("8. Validate egressIP again from local and remote pods after recreating UDN \n")
+		if ipStackType == "dualstack" {
+			exutil.By("Verify egressIP from IPv4 perspective")
+			exutil.By("8.1 Verify v4 egressIP from test pods local to egress node")
+			tcpdumOutputv4 := getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev4, tcpdumpCmdv4, ns1, testpod1.name, cmdOnPodv4)
+			o.Expect(strings.Contains(tcpdumOutputv4, freeIPs[0])).To(o.BeTrue())
+			exutil.By("8.2 Verify v4 egressIP from test pods remote to egress node")
+			tcpdumOutputv4 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev4, tcpdumpCmdv4, ns1, testpod3.name, cmdOnPodv4)
+			o.Expect(strings.Contains(tcpdumOutputv4, freeIPs[0])).To(o.BeTrue())
+		}
+
+		exutil.By("Verify egressIP from IPv6 perspective")
+		exutil.By("8.3 Verify v6 egressIP from test pods local to egress node")
+		tcpdumOutputv6 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev6, tcpdumpCmdv6, ns1, testpod2.name, cmdOnPodv6)
+		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
+		exutil.By("8.4 Verify v6 egressIP from test pods remote to egress node")
+		tcpdumOutputv6 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev6, tcpdumpCmdv6, ns1, testpod3.name, cmdOnPodv6)
+		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
+
+		exutil.By("9. Restart ovnkube-node pod of client host that remote egressIP pod is on.\n")
+		// Since local egressIP pod is on egress node, so just to restart ovnkube-pod of egress node
+		ovnkPod = ovnkubeNodePod(oc, nonEgressNode)
+		err = oc.AsAdmin().WithoutNamespace().Run("delete").Args("pods", ovnkPod, "-n", "openshift-ovn-kubernetes").Execute()
+		o.Expect(err).NotTo(o.HaveOccurred())
+		waitForPodWithLabelReady(oc, "openshift-ovn-kubernetes", "app=ovnkube-node")
+
+		exutil.By("10. Validate egressIP again from local and remote pods after recreating UDN \n")
+		if ipStackType == "dualstack" {
+			exutil.By("Verify egressIP from IPv4 perspective")
+			exutil.By("10.1 Verify v4 egressIP from test pods local to egress node")
+			tcpdumOutputv4 := getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev4, tcpdumpCmdv4, ns1, testpod1.name, cmdOnPodv4)
+			o.Expect(strings.Contains(tcpdumOutputv4, freeIPs[0])).To(o.BeTrue())
+			exutil.By("10.2 Verify v4 egressIP from test pods remote to egress node")
+			tcpdumOutputv4 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev4, tcpdumpCmdv4, ns1, testpod3.name, cmdOnPodv4)
+			o.Expect(strings.Contains(tcpdumOutputv4, freeIPs[0])).To(o.BeTrue())
+		}
+
+		exutil.By("Verify egressIP from IPv6 perspective")
+		exutil.By("10.3 Verify v6 egressIP from test pods local to egress node")
+		tcpdumOutputv6 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev6, tcpdumpCmdv6, ns1, testpod2.name, cmdOnPodv6)
+		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
+		exutil.By("10.4 Verify v6 egressIP from test pods remote to egress node")
+		tcpdumOutputv6 = getTcpdumpOnNodeCmdFromPod(oc, assignedEIPNodev6, tcpdumpCmdv6, ns1, testpod3.name, cmdOnPodv6)
+		o.Expect(strings.Contains(tcpdumOutputv6, assignedEIPv6Addr)).To(o.BeTrue())
+	})
 })
