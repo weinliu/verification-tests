6102962b452cfdae60e0a90fa52b158b934ceed4
Prachiti Talgulkar <160736205+ptalgulk01@users.noreply.github.com>
2024-12-19 11:24:57 +0530

    check for wrong extension (#21852)
    Co-authored-by: Prachiti Talgulkar <ptalgulk@ptalgulk-thinkpadp16vgen1.punetw6.csb>

--- test/extended/mco/mco_ocb.go
+++ test/extended/mco/mco_ocb.go
@@ -658,6 +658,48 @@ var _ = g.Describe("[sig-mco] MCO ocb", func() {
 		ValidateMOSCIsGarbageCollected(mosc, mcp)
 		logger.Infof("OK!\n")
 	})
+
+	g.It("Author:ptalgulk-ConnectedOnly-Longduration-NonPreRelease-Medium-78197-Install wrong extension after OCB is enabled[Disruptive]", func() {
+
+		var (
+			moscName = "test-" + GetCurrentTestPolarionIDNumber()
+			mcp      = GetCompactCompatiblePool(oc.AsAdmin())
+			mcName   = "test-install-extenstion-" + GetCurrentTestPolarionIDNumber()
+		)
+
+		exutil.By("Configure OCB functionality for the new worker MCP")
+		mosc, err := CreateMachineOSConfigUsingInternalRegistry(oc.AsAdmin(), MachineConfigNamespace, moscName, mcp.GetName(), nil)
+		defer DisableOCL(mosc)
+		o.Expect(err).NotTo(o.HaveOccurred(), "Error creating the MachineOSConfig resource")
+		logger.Infof("OK!\n")
+
+		ValidateSuccessfulMOSC(mosc, nil)
+
+		exutil.By("Create a MC")
+		mc := NewMachineConfig(oc.AsAdmin(), mcName, MachineConfigPoolWorker)
+		mc.SetParams(`EXTENSIONS=["zsh"]`)
+		mc.skipWaitForMcp = true
+		defer mc.delete()
+		mc.create()
+
+		exutil.AssertAllPodsToBeReady(oc.AsAdmin(), MachineConfigNamespace)
+		logger.Infof("OK!\n")
+		mOSBuilder := NewNamespacedResource(oc.AsAdmin(), "deployment", MachineConfigNamespace, "machine-os-builder")
+
+		exutil.By("Verify the  wrong extension is detected in machine-os-builder")
+		o.Expect(mOSBuilder.Logs()).To(o.ContainSubstring("invalid extensions found: [zsh]"))
+
+		exutil.By("Delete a MC.")
+		mc.delete()
+		logger.Infof("OK!\n")
+
+		exutil.By("Remove the MachineOSConfig resource")
+		o.Expect(DisableOCL(mosc)).To(o.Succeed(), "Error cleaning up %s", mosc)
+		logger.Infof("OK!\n")
+
+		ValidateMOSCIsGarbageCollected(mosc, mcp)
+
+	})
 })
 
 func testContainerFile(containerFiles []ContainerFile, imageNamespace string, mcp *MachineConfigPool, checkers []Checker) {
