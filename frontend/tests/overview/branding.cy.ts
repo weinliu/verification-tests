d709bb12af633eee08eb3fde540f6ca3eaa84246
Yanping Zhang <yanpzhan@redhat.com>
2025-01-23 00:24:56 +0800

    remove rosa tag for ocp-48357 (#22715)

--- frontend/tests/overview/branding.cy.ts
+++ frontend/tests/overview/branding.cy.ts
@@ -3,7 +3,7 @@ import { Branding } from "../../views/branding";
 import { guidedTour } from '../../upstream/views/guided-tour';
 
 describe("Branding check", () => {
-  it("(OCP-48357,yanpzhan,UserInterface) Switch the OCP branding logo and title to Red Hat OpenShift logo and title",{tags:['@userinterface','@e2e','@rosa','@smoke','@hypershift-hosted']}, () => {
+  it("(OCP-48357,yanpzhan,UserInterface) Switch the OCP branding logo and title to Red Hat OpenShift logo and title",{tags:['@userinterface','@e2e','@smoke','@hypershift-hosted']}, () => {
     cy.visit('/');
     Branding.checkLoginPageLogo();
     cy.uiLogin(Cypress.env("LOGIN_IDP"),Cypress.env("LOGIN_USERNAME"),Cypress.env("LOGIN_PASSWORD"));
