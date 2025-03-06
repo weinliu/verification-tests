43302fe2b9bfa0812a8d162f357044431a7fc2ad
Yanping Zhang <yanpzhan@redhat.com>
2025-03-06 12:55:18 +0800

    fix ocp-48357 (#23615)

--- frontend/views/branding.ts
+++ frontend/views/branding.ts
@@ -1,10 +1,10 @@
 export const Branding = {
   checkAboutModalLogo: () => {
-    cy.get("div[class$='about-modal-box__brand']").find("img").should("have.attr", "src").and("contain", "openshift-logo.svg");
+    cy.get("div[class$='about-modal-box__brand']").find("img").should("have.attr", "src").and("contain", "red-hat-fedora.svg");
     cy.get('[aria-label="Close Dialog"]').click();
   },
   checkLoginPageLogo: () => {
-    cy.get("img").should("have.attr", "alt", "Red Hat OpenShift logo");
+    cy.get("svg").should("have.attr", "title", "Red Hat OpenShift logo");
   },
   closeModal: () => {
     cy.get('body').then(($body) => {
--- frontend/views/overview.ts
+++ frontend/views/overview.ts
@@ -15,7 +15,7 @@ export const Overview = {
     cy.get("button").contains("About").click();
   },
   checkUpperLeftLogo: () => {
-    cy.get("img").should('have.attr', 'src').and('contain', 'openshift-logo.svg');
+    cy.get("svg.injected-svg").should('have.attr', 'data-src').and('contain', 'openshift-logo.svg');
   },
   navToOverviewPage: () => {
     cy.get('[data-quickstart-id="qs-nav-home"]').click();
