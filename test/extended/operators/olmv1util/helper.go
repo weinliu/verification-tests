5f140147d6f8d9e4b32a9966b75dee10fd0d2c6a
Kui Wang <kuiwang@redhat.com>
2025-03-10 19:17:56 +0800

    add helper method to check if there is feature gate enabled (#23687)

--- test/extended/operators/olmv1util/helper.go
+++ test/extended/operators/olmv1util/helper.go
@@ -1,8 +1,13 @@
 package olmv1util
 
 import (
+	"context"
 	"time"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	o "github.com/onsi/gomega"
 	exutil "github.com/openshift/openshift-tests-private/test/extended/util"
 )
 
@@ -28,4 +33,29 @@ func Cleanup(oc *exutil.CLI, parameters ...string) {
 func Appearance(oc *exutil.CLI, appear bool, parameters ...string) bool {
 	return exutil.CheckAppearance(oc, 4*time.Second, 200*time.Second, exutil.NotImmediately,
 		exutil.AsAdmin, exutil.WithoutNamespace, appear, parameters...)
+}
+
+// isFeaturegateEnabled checks if a cluster enable feature gate
+func isFeaturegateEnabled(oc *exutil.CLI, featuregate string) bool {
+	featureGate, err := oc.AdminConfigClient().ConfigV1().FeatureGates().Get(context.Background(), "cluster", metav1.GetOptions{})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return false
+		}
+		o.Expect(err).NotTo(o.HaveOccurred(), "could not retrieve feature-gate: %v", err)
+	}
+
+	isEnabled := false
+	for _, featureGate := range featureGate.Status.FeatureGates {
+		for _, enabled := range featureGate.Enabled {
+			if string(enabled.Name) == featuregate {
+				isEnabled = true
+				break
+			}
+		}
+		if isEnabled {
+			break
+		}
+	}
+	return isEnabled
 }
